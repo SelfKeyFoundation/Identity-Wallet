fa61919e4568f0433d8259d83cf76c0d543b4d8a
Gio <gmerabishvili>
2017-12-23 23:40:29 +0400

    termsss

--- wallet-web-app/src/angular/controllers/commons/dialogs/terms-controller.js
+++ wallet-web-app/src/angular/controllers/commons/dialogs/terms-controller.js
@@ -7,14 +7,22 @@ function TermsDialogController($rootScope, $scope, $log, $q, $mdDialog) {
         $mdDialog.cancel();
     };
 
-    $scope.scrolledBottom = false;
 
-    var elem = angular.element(document.querySelector(".textual"));
-    elem.on("scroll", function (ev) {
+
+    setTimeout(function () {
         $scope.scrolledBottom = true;
-    });
+        var elem = angular.element(document.querySelector(".textual"));
+        var elemChild = angular.element(document.querySelector(".text"));
 
+        var wholeHeight = elemChild[0].scrollHeight;
+        var visibleHeight = elem[0].offsetHeight;
+        elem.on("scroll", function (ev) {
+            if(elem[0].scrollTop >= (wholeHeight - visibleHeight -50)){
+                $scope.scrolledBottom = false;
+            }
 
+        });
+    }, 1000)
 };
 
 export default TermsDialogController;
