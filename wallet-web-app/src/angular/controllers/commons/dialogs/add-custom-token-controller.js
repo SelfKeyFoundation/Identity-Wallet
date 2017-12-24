4476691a69fd197912601f172a61a01aca959bbf
tatulika <tato.mchedlishvili@gmail.com>
2017-12-24 18:47:17 +0400

    WAL-100 dev done

--- wallet-web-app/src/angular/controllers/commons/dialogs/add-custom-token-controller.js
+++ wallet-web-app/src/angular/controllers/commons/dialogs/add-custom-token-controller.js
@@ -1,11 +1,34 @@
+import { setTimeout } from "timers";
+
 function AddCustomTokenDialogController($rootScope, $scope, $log, $q, $mdDialog) {
     'ngInject'
 
     $log.info('AddCustomTokenDialogController');
-
+    $scope.showTooltip = false;
     $scope.cancel = (event) => {
         $mdDialog.cancel();
     }
+
+    const MAX_TIMESTAMP = 8640000000000000;
+    let tooltipDisipareDate = new Date();
+    const tooltipDisipareLastTime = new Date(new Date().getTime() + 1990);
+
+    $scope.setShowTooltip = function (value) {
+        if (value) {
+            $scope.showTooltip = true;
+            tooltipDisipareDate = new Date(MAX_TIMESTAMP);
+            return;
+        }
+
+        tooltipDisipareDate = tooltipDisipareLastTime;
+        setTimeout(function () {
+            if (tooltipDisipareDate < new Date()) {
+                $scope.showTooltip = false;
+            }
+        }, 2000);
+    }
+
+
 
 };
 
