09b637e729a6454bbc525e3698d3f91ef5cff648
Giorgi Maisuradze <g.maisuradze88@gmail.com>
2017-12-25 18:44:47 +0400

    fix

--- wallet-web-app/src/angular/controllers/commons/dialogs/receive-token-controller.js
+++ wallet-web-app/src/angular/controllers/commons/dialogs/receive-token-controller.js
@@ -1,7 +1,7 @@
 function ReceiveTokenDialogController($rootScope, $scope, $log, $q, $mdDialog, args) {
     'ngInject'
 
-    $scope.symbol = args.selectedToken.symbol;      // key
+    $scope.symbol = args.symbol;                    // key
     $scope.publicKeyHex = args.publicKeyHex;        // 0x4ac0d9ebd28118cab68a64ad8eb8c07c0120ebf8
 
     $scope.cancel = (event) => {
