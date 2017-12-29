e1b7e7193cc774d59ab375fd1e70432c99db19df
Joonas Ruotsalainen <joonas@selfkey.org>
2017-12-29 17:04:46 +0700

    Move i18n

--- app/main.js
+++ app/main.js
@@ -1,16 +1,16 @@
 'use strict';
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
-}
-
 const path = require('path');
 const url = require('url');
 const electron = require('electron');
 const {autoUpdater} = require('electron-updater');
 const {dialog} = require('electron');
 const deskmetrics = require('deskmetrics');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+  electron.app.quit();
+}
 
 
 const app = {
@@ -34,7 +34,7 @@ const i18n = [
 const AsyncRequestHandler = require('./controllers/async-request-handler')(app);
 
 for(let i in i18n){
-	app.translations[i18n[i]] = require('../i18n/' + i18n[i] + ".js");
+	app.translations[i18n[i]] = require('./i18n/' + i18n[i] + '.js');
 }
 
 function createWindow(app) {
@@ -81,42 +81,40 @@ function createWindow(app) {
 		});
 
 		// self updater
-		// TODO ignore linux because not supported
-		// TODO ignore when in dev mode
+		if (!app.config.app.dev) {
+			autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+				console.log('update-downloaded');
+				const dialogOpts = {
+					type: 'info',
+					buttons: ['Restart', 'Later'],
+					title: 'Application Update',
+					message: process.platform === 'win32' ? releaseNotes : releaseName,
+					detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+				};
 
-		autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-			console.log('update-downloaded');
-			const dialogOpts = {
-				type: 'info',
-				buttons: ['Restart', 'Later'],
-				title: 'Application Update',
-				message: process.platform === 'win32' ? releaseNotes : releaseName,
-				detail: 'A new version has been downloaded. Restart the application to apply the updates.'
-			};
-
-			dialog.showMessageBox(dialogOpts, (response) => {
-				if (response === 0) { autoUpdater.quitAndInstall(); }
+				dialog.showMessageBox(dialogOpts, (response) => {
+					if (response === 0) { autoUpdater.quitAndInstall(); }
+				});
 			});
-		});
 
-		autoUpdater.on('error', (error) => {
-			console.error('There was a problem updating the application', error);
-		});
+			autoUpdater.on('error', (error) => {
+				console.error('There was a problem updating the application', error);
+			});
 
-		autoUpdater.on('update-not-available', () => {
-			console.log('update-not-available');
-		});
+			autoUpdater.on('update-not-available', () => {
+				console.log('update-not-available');
+			});
 
-		autoUpdater.on('update-available', () => {
-			console.log('update-available');
-		});
+			autoUpdater.on('update-available', () => {
+				console.log('update-available');
+			});
 
-		autoUpdater.on('checking-for-update', () => {
-			console.log('checking-for-update');
-		});
+			autoUpdater.on('checking-for-update', () => {
+				console.log('checking-for-update');
+			});
 
-		// TODO
-		autoUpdater.checkForUpdatesAndNotify();
+			autoUpdater.checkForUpdatesAndNotify();
+		}
 
 		app.asyncRequestHandler = new AsyncRequestHandler();
 
