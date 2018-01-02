a87137ea3163bfc402a86e3c24f676fd25a6453b
Joonas Ruotsalainen <joonas@selfkey.org>
2018-01-02 23:44:35 +0700

    Move web app correctly

--- wallet-web-app/src/angular/controllers/member/setup/wallet-setup-controller.js
+++
@@ -1,8 +0,0 @@
-function MemberSetupWalletSetupController($rootScope, $scope, $log, $q, $timeout, $state, ConfigFileService, WalletService, CommonService) {
-    'ngInject'
-
-    $log.info('MemberSetupWalletSetupController');
-
-};
-
-export default MemberSetupWalletSetupController;
