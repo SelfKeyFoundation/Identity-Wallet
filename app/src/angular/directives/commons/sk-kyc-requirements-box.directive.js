381a0091fd0d55a61072a4233c6db3e04c52560b
Giorgi Maisuradze <g.maisuradze88@gmail.com>
2018-01-12 23:04:40 +0400

    req fix

--- app/src/angular/directives/commons/sk-kyc-requirements-box.directive.js
+++ app/src/angular/directives/commons/sk-kyc-requirements-box.directive.js
@@ -18,6 +18,8 @@ function SkKycRequirementsBoxDirective($rootScope, $log, $window, SelfkeyService
              * get kyc requirements
              */
             function loadRequirements() {
+                const COLUMNS = 2;
+
                 scope.requirementsListPromise = SelfkeyService.retrieveKycTemplate(
                     scope.kycInfo.apiEndpoint,
                     scope.kycInfo.organisation,
@@ -33,20 +35,29 @@ function SkKycRequirementsBoxDirective($rootScope, $log, $window, SelfkeyService
                     let questions = requirements.questions;
                     let uploads = requirements.uploads;
 
+                    // Temporary - name is one of default id attribute that is missing in template
+                    let idAttributeType = ConfigFileService.getIdAttributeType("name");
+                    requirementsList.push({ key: "name", idAttributeType: idAttributeType, req: null });
+
                     for (let i in questions) {
                         let req = questions[i];
+                        
+                        if(!req.attributeType) continue;
+
                         let idAttributeType = ConfigFileService.getIdAttributeType(req.attributeType);
                         requirementsList.push({ key: req.attributeType, idAttributeType: idAttributeType, req: req });
                     }
 
                     for (let i in uploads) {
                         let req = uploads[i];
+                        
+                        if(!req.attributeType) continue;
 
                         let idAttributeType = ConfigFileService.getIdAttributeType(req.attributeType);
                         requirementsList.push({ key: req.attributeType, idAttributeType: idAttributeType, req: req });
                     }
 
-                    scope.sections = CommonService.chunkArray(requirementsList, 3);
+                    scope.sections = CommonService.chunkArray(requirementsList, COLUMNS);
                 }).catch((error) => {
                     $log.error("requirementsListPromise data:", error);
                 });
