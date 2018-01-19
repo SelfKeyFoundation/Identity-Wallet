406bdd45b782ce7170d0f8d85f5bda9491375078
anri <anri_morchiladze@yahoo.com>
2018-01-19 22:12:04 +0400

    IDWAL00K-202

--- app/src/angular/controllers/commons/dialogs/receive-token-controller.js
+++ app/src/angular/controllers/commons/dialogs/receive-token-controller.js
@@ -1,8 +1,8 @@
 function ReceiveTokenDialogController($rootScope, $scope, $log, $q, $mdDialog, $window, args) {
     'ngInject'
 
-    $scope.symbol = args.symbol;                            // key
-    $scope.publicKeyHex = "0x" + args.publicKeyHex;         // 0x4ac0d9ebd28118cab68a64ad8eb8c07c0120ebf8
+    $scope.symbol = args.symbol; // key
+    $scope.publicKeyHex = "0x" + args.publicKeyHex; // 0x4ac0d9ebd28118cab68a64ad8eb8c07c0120ebf8
 
     $scope.print = (event) => {
         $window.print();
@@ -11,6 +11,7 @@ function ReceiveTokenDialogController($rootScope, $scope, $log, $q, $mdDialog, $
     $scope.cancel = (event) => {
         $mdDialog.cancel();
     }
+
 };
 
 export default ReceiveTokenDialogController;
