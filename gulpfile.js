546f5725729dbebe32f62c36a2946a955705f4b0
Dito Ogiashvili <ogiashvili@gmail.com>
2018-01-24 12:08:59 +0400

    add auto start and make

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const gulp = require('gulp');
+const exec = require('child_process').exec;
 const runSequence = require('run-sequence');
 const watch = require('gulp-watch');
 const pug = require('gulp-pug');
@@ -13,6 +14,7 @@ const tmplDest = path.resolve(__dirname, './app/src/angular/');
 
 const scssSrc = path.resolve(__dirname, './app/src/stylesheets/scss/main.scss');
 const scssDest = path.resolve(__dirname, './app/src/stylesheets/css');
+const isWin =  /^win/.test(process.platform);
 
 
 gulp.task('templates', (cb) => {
@@ -46,6 +48,30 @@ gulp.task('watch', (cb) => {
 	watch([scssSrc, tmplSrc], () => {
 		runSequence(['templates', 'stylesheets'], cb);
 	});
+});
+
+gulp.task('start', (cb)=>{
+	let command = '';
+	if(isWin){
+		command = 'node_modules\\.bin\\electron-forge start -- dev';
+	} else {
+		command = './node_modules/.bin/electron-forge start -- dev';
+	}
+	exec(command, (err)=>{
+		cb(err);
+	})
+});
+
+gulp.task('make', (cb)=>{
+	let command = '';
+	if(isWin){
+		command = 'node_modules\\.bin\\electron-forge make';
+	} else {
+		command = './node_modules/.bin/electron-forge make';
+	}
+	exec(command, (err)=>{
+		cb(err);
+	})
 });
 
 gulp.task('default', (cb) => {
