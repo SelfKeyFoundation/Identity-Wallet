1e57acd4ed3107cf8470b8f96a973342a3622317
burdula <burduli13@gmail.com>
2018-01-26 15:55:12 +0400

    address bug

--- app/src/angular/controllers/commons/dialogs/receive-token-controller.js
+++ app/src/angular/controllers/commons/dialogs/receive-token-controller.js
@@ -2,7 +2,7 @@ function ReceiveTokenDialogController($rootScope, $scope, $log, $q, $mdDialog, $
     'ngInject'
 
     $scope.symbol = args.symbol; // key
-    $scope.publicKeyHex = "0x" + args.publicKeyHex; // 0x4ac0d9ebd28118cab68a64ad8eb8c07c0120ebf8
+    $scope.publicKeyHex = args.publicKeyHex; // 0x4ac0d9ebd28118cab68a64ad8eb8c07c0120ebf8
 
     $scope.print = (event) => {
         $window.print();
