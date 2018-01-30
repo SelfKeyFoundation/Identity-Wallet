dd5bee193464ed1fde763a972a0d34f0147e66c1
altninja <benny.gervais@gmail.com>
2018-01-30 17:34:56 +0700

    updates to test blocked at kyc upload auth issue

--- test/quick.sh
+++ test/quick.sh
@@ -1,4 +1,12 @@
 #!bin/bash
-rm -rf /Users/altninja/Library/Application\ Support/Electron
-rm -rf /Users/altninja/Library/Application\ Support/ID\ Wallet
-node /Users/altninja/code/selfkey/idw2/test/test.js 1
+rm -rf /Users/altninja/Library/Application\ Support/Electron/documents
+rm -rf /Users/altninja/Library/Application\ Support/Electron/wallets
+rm -rf /Users/altninja/Library/Application\ Support/ID\ Wallet/documents
+rm -rf /Users/altninja/Library/Application\ Support/ID\ Wallet/wallets
+rm /Users/altninja/Library/Application\ Support/ID\ Wallet/main-store.json
+rm /Users/altninja/Library/Application\ Support/Electron/main-store.json
+rm -rf /Users/altninja/code/selfkey/wallets/Identity-Wallet/test/caps/source
+rm -rf /Users/altninja/code/selfkey/wallets/Identity-Wallet/test/caps/screens
+mkdir /Users/altninja/code/selfkey/wallets/Identity-Wallet/test/caps/screens
+mkdir /Users/altninja/code/selfkey/wallets/Identity-Wallet/test/caps/source
+node /Users/altninja/code/selfkey/wallets/Identity-Wallet/test/test.js 2
