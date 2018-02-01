3495be0f7c0cff4ae49888664d85fe43a46306e3
altninja <benny.gervais@gmail.com>
2018-02-01 19:19:05 +0700

    pw flow everything up to upload done

--- test/quick.sh
+++ test/quick.sh
@@ -1,12 +1,16 @@
 #!bin/bash
 rm -rf /Users/altninja/Library/Application\ Support/Electron/documents
 rm -rf /Users/altninja/Library/Application\ Support/Electron/wallets
-rm -rf /Users/altninja/Library/Application\ Support/ID\ Wallet/documents
-rm -rf /Users/altninja/Library/Application\ Support/ID\ Wallet/wallets
-rm /Users/altninja/Library/Application\ Support/ID\ Wallet/main-store.json
 rm /Users/altninja/Library/Application\ Support/Electron/main-store.json
+
+rm -rf /Users/altninja/Library/Application\ Support/id-wallet/documents
+rm -rf /Users/altninja/Library/Application\ Support/id-wallet/wallets
+rm /Users/altninja/Library/Application\ Support/id-wallet/main-store.json
+
 rm -rf /Users/altninja/code/selfkey/wallets/Identity-Wallet/test/caps/source
 rm -rf /Users/altninja/code/selfkey/wallets/Identity-Wallet/test/caps/screens
+
 mkdir /Users/altninja/code/selfkey/wallets/Identity-Wallet/test/caps/screens
 mkdir /Users/altninja/code/selfkey/wallets/Identity-Wallet/test/caps/source
+
 node /Users/altninja/code/selfkey/wallets/Identity-Wallet/test/test.js 2
--- test/test.js
+++ test/test.js
@@ -37,31 +37,38 @@ app.start().then(() => {
 		w = app.webContents
 		
 	c.waitUntilWindowLoaded()
+		.then(() => console.log(chalk.green('Start Create New Wallet From Password Flow')))
 		.then(() => w.savePage('test/caps/source/index.html', 'HTMLComplete'))
-		.then(() => console.log(chalk.green('Source Code Saved Done')))
 
 // TOC Confirm
 		.then(() => c.waitForVisible('#agree', 10000))
-		.then(() => console.log(chalk.blue('TOC Agree Loaded')))
-		.then(() => c.click("#agree"))
-		.then(() => console.log(chalk.green('TOC Agree Test Done')))
+		.then(() => c.click('#agree'))
+
+// Setup Wallet
+		.then(() => c.waitForExist('#setupWallet', 10000))
+		.then(() => c.click("#setupWallet"))
 
 // Click Create New Wallet
-		.then(() => c.click("#createWallet"))
+		.then(() => delay(1000).then(() => c.waitForVisible('#createWallet', 10000)))
+		.then(() => c.click('#createWallet'))
 
 // 2x Password
+		.then(() => delay(1000).then(() => c.waitForVisible('#pw1', 10000)))
 		.then(() => c.setValue('#pw1', password))
 		.then(() => c.setValue('#pw2', password))
-		.then(() => c.click('#confirm'))
+		.then(() => c.click('#next'))
 
 // Confirm Password
+		.then(() => delay(1000).then(() => c.waitForVisible('#generateWallet', 10000)))
 		.then(() => c.setValue('#pw3', password))
-		.then(() => c.click('#confirm'))
+		.then(() => c.click('#generateWallet'))
 
 // Check PubKey
 // get text
 // assert vs ?
 // click download
+
+		.then(() => delay(1000).then(() => c.waitForVisible('#dl', 10000)))
 		.then(() => c.click('#dl'))
 		//click next
 		.then(() => c.click('#next'))
@@ -70,16 +77,22 @@ app.start().then(() => {
 // get text
 // assert vs ?
 		// click next
-		.then(() => c.click('#next'))
+		.then(() => delay(1000).then(() => c.waitForVisible('#next2', 10000)))
+		.then(() => c.click('#next2'))
 
 // Upload KYC Docs
 	// post anything to localhost:3333/ get 200 go to dashboard
-	          name: store.wallets[key].name,
-          keystoreFilePath: store.wallets[key].keystoreFilePath,
-          publicKey: key
-
-// Dashboard
-
+	    // local storage checks file
+	    // make new zip file from public key and other test data
+	    // name: store.wallets[key].name,
+     //    keystoreFilePath: store.wallets[key].keystoreFilePath,
+     //    publicKey: key
+ 		.then(() => console.log(chalk.green('Done')))
+		.then(() => app.stop())
+		.catch((err) => {
+			console.log(chalk.red('Test Failed With Error: '), err)
+			app.stop()
+		})
 })
 
 
@@ -114,109 +127,109 @@ app.start().then(() => {
 // Private Key
 // ***********
 
-app.start().then(() => {
-	const
-		c = app.client
-		e = app.electron
-		b = app.browserWindow
-		w = app.webContents
+// app.start().then(() => {
+// 	const
+// 		c = app.client
+// 		e = app.electron
+// 		b = app.browserWindow
+// 		w = app.webContents
 		
-	c.waitUntilWindowLoaded()
-		.then(() => w.savePage('test/caps/source/index.html', 'HTMLComplete'))
-		.then(() => console.log(chalk.green('Source Code Saved Done')))
+// 	c.waitUntilWindowLoaded()
+// 		.then(() => w.savePage('test/caps/source/index.html', 'HTMLComplete'))
+// 		.then(() => console.log(chalk.green('Source Code Saved Done')))
 
-// TOC Agree -> Import Key
-// TOC Disagree -> Quit / Go Back & Agree
-// Help -> External Window
-// Quit -> Exit
+// // TOC Agree -> Import Key
+// // TOC Disagree -> Quit / Go Back & Agree
+// // Help -> External Window
+// // Quit -> Exit
 
-	 	// Agree TOC
-		.then(() => c.waitForExist('#agree', 10000))
-		.then(() => console.log(chalk.blue('TOC Agree Loaded')))
-		//.then(() => c.execute("window.scrollTo(0, document.body.scrollHeight)"))
-		// .then(() => c.execute('arguments[0].scrollIntoView()', 'document.getElementById("#bottom")'))
-		// .then(() => c.execute('document.getElementById("#agree").removeAttribute("disabled");'))
-		// ** temp removed this from button #agree ** (ng-disabled="!scrolledBottom || storeSavePromise.$$state.status")
-		.then(() => c.click("#agree"))
-		.then(() => console.log(chalk.green('TOC Agree Test Done')))
+// 	 	// Agree TOC
+// 		.then(() => c.waitForExist('#agree', 10000))
+// 		.then(() => console.log(chalk.blue('TOC Agree Loaded')))
+// 		//.then(() => c.execute("window.scrollTo(0, document.body.scrollHeight)"))
+// 		// .then(() => c.execute('arguments[0].scrollIntoView()', 'document.getElementById("#bottom")'))
+// 		// .then(() => c.execute('document.getElementById("#agree").removeAttribute("disabled");'))
+// 		// ** temp removed this from button #agree ** (ng-disabled="!scrolledBottom || storeSavePromise.$$state.status")
+// 		.then(() => c.click("#agree"))
+// 		.then(() => console.log(chalk.green('TOC Agree Test Done')))
 		
-		// Setup Wallet Confirm
-		.then(() => c.waitForExist('#setupWallet', 10000))
-		.then(() => c.click("#setupWallet"))
-		.then(() => console.log(chalk.green('Setup Wallet Test Done')))
+// 		// Setup Wallet Confirm
+// 		.then(() => c.waitForExist('#setupWallet', 10000))
+// 		.then(() => c.click("#setupWallet"))
+// 		.then(() => console.log(chalk.green('Setup Wallet Test Done')))
 
-// Import Key -> Choose Import Method
+// // Import Key -> Choose Import Method
 
-		// Import Wallet
-		.then(() => c.waitForVisible('#import', 10000))
-		.then(() => delay(1000).then(() => console.log(chalk.green('Import Button Exists'))))
-		.then(() => b.capturePage()
-			.then(() => console.log(chalk.green('Screencap Start')))
-			.then(img => fs.writeFile(pwd + '/test/caps/screen/1.png', img))
-			.then(() => console.log(chalk.green('Screencap Done'))))
-		.then(() => c.click('#import'))
-		.then(() => console.log(chalk.green('Import Button Test Done')))
+// 		// Import Wallet
+// 		.then(() => c.waitForVisible('#import', 10000))
+// 		.then(() => delay(1000).then(() => console.log(chalk.green('Import Button Exists'))))
+// 		.then(() => b.capturePage()
+// 			.then(() => console.log(chalk.green('Screencap Start')))
+// 			.then(img => fs.writeFile(pwd + '/test/caps/screen/1.png', img))
+// 			.then(() => console.log(chalk.green('Screencap Done'))))
+// 		.then(() => c.click('#import'))
+// 		.then(() => console.log(chalk.green('Import Button Test Done')))
 
-// Choose Import -> Private Key (Ignore other options for now, lack test data)
-// Select Private Key Radio Button
-// Select Private Key Text Area
-// Add Value For Private Key Text Area
-// Submit Private Key
+// // Choose Import -> Private Key (Ignore other options for now, lack test data)
+// // Select Private Key Radio Button
+// // Select Private Key Text Area
+// // Add Value For Private Key Text Area
+// // Submit Private Key
 
-		// Select Private Key And Submit
-		.then(() => c.waitForExist('#pkey', 10000))
-		.then(() => b.capturePage()
-			.then(img => fs.writeFile(pwd + '/test/caps/screen/2.png', img)))
-		.then(() => c.click("#pkey"))
-		.then(() => c.click('#keyarea'))
-		.then(() => c.setValue('#keyarea', data[usr].privKey))
-		.then(() => c.click('#pkyunlock'))
-		.then(() => console.log(chalk.green('Select And Add Private Key Test Done')))
+// 		// Select Private Key And Submit
+// 		.then(() => c.waitForExist('#pkey', 10000))
+// 		.then(() => b.capturePage()
+// 			.then(img => fs.writeFile(pwd + '/test/caps/screen/2.png', img)))
+// 		.then(() => c.click("#pkey"))
+// 		.then(() => c.click('#keyarea'))
+// 		.then(() => c.setValue('#keyarea', data[usr].privKey))
+// 		.then(() => c.click('#pkyunlock'))
+// 		.then(() => console.log(chalk.green('Select And Add Private Key Test Done')))
 
-// If no KYC upload KYC Docs
-// Confirm Screen with Docs Info -> Goto Dashboard or Selfkey ICO view
+// // If no KYC upload KYC Docs
+// // Confirm Screen with Docs Info -> Goto Dashboard or Selfkey ICO view
 
-		// Upload KYC Docs
-		.then(() => c.waitForExist('#uploadKyc', 10000))
-		.then(() => c.click('#uploadKyc', 10000))
-		.then(() => c.element('#uploadKyc').keys(pwd + '/test/data/' + usr + '.zip'))
-		// .then(() => c.chooseFile('#uploadKyc', pwd + '/test/data/' + usr + '.zip'))
+// 		// Upload KYC Docs
+// 		.then(() => c.waitForExist('#uploadKyc', 10000))
+// 		.then(() => c.click('#uploadKyc', 10000))
+// 		.then(() => c.element('#uploadKyc').keys(pwd + '/test/data/' + usr + '.zip'))
+// 		// .then(() => c.chooseFile('#uploadKyc', pwd + '/test/data/' + usr + '.zip'))
 			
-// If KYC confirm private key -> Goto Dashboard
+// // If KYC confirm private key -> Goto Dashboard
 
-		// Confirm Private Key
-		// .then(() => c.waitForExist('#pkycontinue', 10000))
-		// .then(() => b.capturePage()
-		// 	.then(img => fs.writeFile(pwd + '/test/caps/screen/3.png', img))
-		// ).then(() => c.click('#pkycontinue'))
-		// .then(() => console.log(chalk.green('Confirm Private Key Test Done')))
+// 		// Confirm Private Key
+// 		// .then(() => c.waitForExist('#pkycontinue', 10000))
+// 		// .then(() => b.capturePage()
+// 		// 	.then(img => fs.writeFile(pwd + '/test/caps/screen/3.png', img))
+// 		// ).then(() => c.click('#pkycontinue'))
+// 		// .then(() => console.log(chalk.green('Confirm Private Key Test Done')))
 			
-// Check ETH Address from Copy button
-// Check KEY Address from Copy button
-// Check Navbar works
-// Check Navbar Links Work
-// Check Transaction History for a correct value
-// Send ETH
-// Send KEY
-// Airdrop Test
+// // Check ETH Address from Copy button
+// // Check KEY Address from Copy button
+// // Check Navbar works
+// // Check Navbar Links Work
+// // Check Transaction History for a correct value
+// // Send ETH
+// // Send KEY
+// // Airdrop Test
 
-		// Dashboard
-		.then(() => c.waitForExist('#eth-copy', 10000))
-		.then(() => c.click('#eth-copy'))
-		.then(() => e.clipboard.readText()
-			.then(cbt => assert.equal(cbt, data[usr].pubKey))
-			.then(() => console.log(chalk.green('ETH Address Copy Test Done'))))
-		.then(() => c.click('#qey-copy'))
-		.then(() => e.clipboard.readText()
-			.then(cbt => assert.equal(cbt, data[usr].pubKey))
-			.then(() => console.log(chalk.green('KEY Address Copy Test Done'))))
-		.then(() => c.click('#aboutLink'))
-		.then(() => c.click('#helpLink'))
-		.then(() => console.log(chalk.green('Testing Completed Successfully'))
-		.then(() => app.stop())
-		).catch((err) => {
-			console.log(chalk.red('Tests Failed With Error: '), err)
-			app.stop()
-		})
+// 		// Dashboard
+// 		.then(() => c.waitForExist('#eth-copy', 10000))
+// 		.then(() => c.click('#eth-copy'))
+// 		.then(() => e.clipboard.readText()
+// 			.then(cbt => assert.equal(cbt, data[usr].pubKey))
+// 			.then(() => console.log(chalk.green('ETH Address Copy Test Done'))))
+// 		.then(() => c.click('#qey-copy'))
+// 		.then(() => e.clipboard.readText()
+// 			.then(cbt => assert.equal(cbt, data[usr].pubKey))
+// 			.then(() => console.log(chalk.green('KEY Address Copy Test Done'))))
+// 		.then(() => c.click('#aboutLink'))
+// 		.then(() => c.click('#helpLink'))
+// 		.then(() => console.log(chalk.green('Testing Completed Successfully')))
+// 		.then(() => app.stop())
+// 		.catch((err) => {
+// 			console.log(chalk.red('Tests Failed With Error: '), err)
+// 			app.stop()
+// 		})
 	
-})
+// })
