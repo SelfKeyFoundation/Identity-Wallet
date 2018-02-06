f7dc9daca3ddc001f0512502e21d1feba753f7aa
altninja <benny.gervais@gmail.com>
2018-02-06 22:16:56 +0700

    feat(f): f

--- test/test.js
+++ test/test.js
@@ -1,35 +1,35 @@
-const Application = require('spectron').Application
-const fs = require('fs')
-const chalk = require('chalk')
-const delay = require('delay')
-const pwd = process.cwd()
-const data = require(pwd + '/test/data/data.json')
-const usr = process.argv[2] || 0
-const OSENV = process.env.OSENV
-const APP_TITLE = require('../package.json').config.forge.electronPackagerConfig
-  .name
+const Application = require("spectron").Application;
+const fs = require("fs");
+const chalk = require("chalk");
+const delay = require("delay");
+const pwd = process.cwd();
+const data = require(pwd + "/test/data/data.json");
+const usr = process.argv[2] || 0;
+const OSENV = process.env.OSENV;
+const APP_TITLE = require("../package.json").config.forge.electronPackagerConfig
+  .name;
 // const password = 'Y@88@D00!'
 
-var appPath
+var appPath;
 
 appPath =
   pwd +
-  '/out/' +
+  "/out/" +
   APP_TITLE +
-  '-darwin-x64/' +
+  "-darwin-x64/" +
   APP_TITLE +
-  '.app/Contents/MacOS/' +
-  APP_TITLE
+  ".app/Contents/MacOS/" +
+  APP_TITLE;
 
-if (OSENV === 'win') {
-  appPath = pwd + '\\out\\id-wallet-win32-ia32\\' + APP_TITLE + '.exe'
-} else if (OSENV === 'lin') {
-  appPath = pwd + '/out/' + APP_TITLE + '-linux-x64/' + APP_TITLE
+if (OSENV === "win") {
+  appPath = pwd + "\\out\\id-wallet-win32-ia32\\" + APP_TITLE + ".exe";
+} else if (OSENV === "lin") {
+  appPath = pwd + "/out/" + APP_TITLE + "-linux-x64/" + APP_TITLE;
 }
 
 const app = new Application({
   path: appPath
-})
+});
 
 function init() {
   return new Promise((resolve, reject) => {
@@ -37,81 +37,81 @@ function init() {
       app.client
         .waitUntilWindowLoaded()
         .then(() =>
-          console.log(chalk.green('Start: Import Wallet From Private Key'))
+          console.log(chalk.green("Start: Import Wallet From Private Key"))
         )
         .then(() =>
           app.webContents.savePage(
-            'test/caps/source/index.html',
-            'HTMLComplete'
+            "test/caps/source/index.html",
+            "HTMLComplete"
           )
         )
-        .then(() => resolve('Init Done'))
+        .then(() => resolve("Init Done"))
         .catch(err => {
-          console.log(chalk.red('Tests Failed With Error: '), err)
-          app.stop()
-          reject(err)
-        })
-    })
-  })
+          console.log(chalk.red("Tests Failed With Error: "), err);
+          app.stop();
+          reject(err);
+        });
+    });
+  });
 }
 
 function regStep(selector) {
   return new Promise((resolve, reject) => {
     delay(1000)
-      .then(() => console.log(chalk.green('Start Step')))
+      .then(() => console.log(chalk.green("Start Step")))
       .then(() => app.client.waitForVisible(selector, 10000))
       .then(() => app.client.click(selector))
-      .then(() => resolve('Step Done'))
+      .then(() => resolve("Step Done"))
       .catch(err => {
-        console.log(chalk.red('Tests Failed With Error: '), err)
-        app.stop()
-        reject(err)
-      })
-  })
+        console.log(chalk.red("Tests Failed With Error: "), err);
+        app.stop();
+        reject(err);
+      });
+  });
 }
 
 function dashboardTest() {
   return new Promise((resolve, reject) => {
-    const c = app.client
-    const e = app.electron
+    const c = app.client;
+    const e = app.electron;
 
     // Dashboard
     c
-      .waitForExist('#goToDashboard', 10000)
-      .then(() => c.click('#goToDashboard', 10000))
-      .then(() => c.waitForExist('#eth-copy', 10000))
-      .then(() => c.click('#eth-copy'))
+      .waitForExist("#goToDashboard", 10000)
+      .then(() => c.click("#goToDashboard", 10000))
+      .then(() => c.waitForExist("#eth-copy", 10000))
+      .then(() => c.click("#eth-copy"))
       .then(() =>
         e.clipboard
           .readText()
           // Check ETH Address from Copy button
           // .then(cbt => assert.equal(cbt, data[usr].pubKey))
-          .then(() => console.log(chalk.green('ETH Address Copy Test Done')))
+          .then(() => console.log(chalk.green("ETH Address Copy Test Done")))
       )
-      .then(() => c.click('#key-copy'))
+      .then(() => c.click("#key-copy"))
       .then(() =>
         e.clipboard
           .readText()
           // Check KEY Address from Copy button
           // .then(cbt => assert.equal(cbt, data[usr].pubKey))
-          .then(() => console.log(chalk.green('KEY Address Copy Test Done')))
+          .then(() => console.log(chalk.green("KEY Address Copy Test Done")))
       )
       // Check Navbar works
-      .then(() => c.click('#navLink'))
+      .then(() => c.click("#navLink"))
       // Check Navbar Links Work
-      .then(() => c.click('#aboutLink'))
-      .then(() => c.click('#helpLink'))
+      .then(() => c.click("#aboutLink"))
+      .then(() => c.click("#helpLink"))
       // Check Transaction History for a correct value
       // Send ETH
       // Send KEY
       // Airdrop Test
-      .then(() => resolve('done'))
+      .then(() => resolve("done"))
       .catch(err => {
-        console.log(chalk.red('Test Failed With Error: '), err)
-        app.stop()
-        reject(err)
-      })
-  })
+        console.log(chalk.red("Test Failed With Error: "), err);
+        app.stop();
+        reject(err);
+      });
+  });
 }
 
 // function passwordTest(password) {
@@ -180,54 +180,54 @@ function dashboardTest() {
 
 function uploadKycTest() {
   return new Promise((resolve, reject) => {
-    const c = app.client
+    const c = app.client;
     c
-      .waitForExist('#uploadKyc', 10000)
+      .waitForExist("#uploadKyc", 10000)
       // .then(() => c.element('#uploadKyc').keys(pwd + '/test/data/' + usr + '.zip'))
       // .then(() => c.chooseFile('#uploadKyc', pwd + '/test/data/' + usr + '.zip'))
-      .then(() => c.click('#uploadKyc', 10000))
+      .then(() => c.click("#uploadKyc", 10000))
       // post anything to localhost:3333/ get 200 go to dashboard
       // local storage checks file
       // make new zip file from public key and other test data
       // name: store.wallets[key].name,
       // keystoreFilePath: store.wallets[key].keystoreFilePath,
       // publicKey: key
-      .then(() => console.log(chalk.blue('Upload KYC All Good')))
-      .then(() => resolve('done'))
+      .then(() => console.log(chalk.blue("Upload KYC All Good")))
+      .then(() => resolve("done"))
       .catch(err => {
-        console.log(chalk.red('Test Failed With Error: '), err)
-        app.stop()
-        reject(err)
-      })
-  })
+        console.log(chalk.red("Test Failed With Error: "), err);
+        app.stop();
+        reject(err);
+      });
+  });
 }
 
 function importWalletTest() {
   return new Promise((resolve, reject) => {
-    const c = app.client
-    const b = app.browserWindow
+    const c = app.client;
+    const b = app.browserWindow;
     delay(1000)
       // Import Wallet
-      .then(() => c.waitForVisible('#import', 10000))
+      .then(() => c.waitForVisible("#import", 10000))
       .then(() =>
-        delay(1000).then(() => console.log(chalk.green('Import Button Exists')))
+        delay(1000).then(() => console.log(chalk.green("Import Button Exists")))
       )
       .then(() =>
         b
           .capturePage()
-          .then(() => console.log(chalk.green('Screencap Start')))
-          .then(img => fs.writeFile(pwd + '/test/caps/screen/1.png', img))
-          .then(() => console.log(chalk.green('Screencap Done')))
+          .then(() => console.log(chalk.green("Screencap Start")))
+          .then(img => fs.writeFile(pwd + "/test/caps/screen/1.png", img))
+          .then(() => console.log(chalk.green("Screencap Done")))
       )
-      .then(() => c.click('#import'))
-      .then(() => console.log(chalk.green('Import Button Test Done')))
-      .then(() => resolve('done'))
+      .then(() => c.click("#import"))
+      .then(() => console.log(chalk.green("Import Button Test Done")))
+      .then(() => resolve("done"))
       .catch(err => {
-        console.log(chalk.red('Test Failed With Error: '), err)
-        app.stop()
-        reject(err)
-      })
-  })
+        console.log(chalk.red("Test Failed With Error: "), err);
+        app.stop();
+        reject(err);
+      });
+  });
 }
 
 function selectPrivateKey() {
@@ -237,30 +237,30 @@ function selectPrivateKey() {
     // Select Private Key Text Area
     // Add Value For Private Key Text Area
     // Submit Private Key
-    const c = app.client
-    const b = app.browserWindow
+    const c = app.client;
+    const b = app.browserWindow;
     delay(1000)
-      .then(() => c.waitForExist('#pkey', 10000))
+      .then(() => c.waitForExist("#pkey", 10000))
       .then(() =>
         b
           .capturePage()
-          .then(img => fs.writeFile(pwd + '/test/caps/screen/2.png', img))
+          .then(img => fs.writeFile(pwd + "/test/caps/screen/2.png", img))
       )
-      .then(() => c.click('#pkey'))
-      .then(() => c.click('#keyarea'))
-      .then(() => c.setValue('#keyarea', data[usr].privKey))
-      .then(() => c.click('#pkyunlock'))
+      .then(() => c.click("#pkey"))
+      .then(() => c.click("#keyarea"))
+      .then(() => c.setValue("#keyarea", data[usr].privKey))
+      .then(() => c.click("#pkyunlock"))
       .then(() =>
-        console.log(chalk.green('Select And Add Private Key Test Done'))
+        console.log(chalk.green("Select And Add Private Key Test Done"))
       )
-      .then(() => console.log(chalk.green('Import Button Test Done')))
-      .then(() => resolve('done'))
+      .then(() => console.log(chalk.green("Import Button Test Done")))
+      .then(() => resolve("done"))
       .catch(err => {
-        console.log(chalk.red('Test Failed With Error: '), err)
-        app.stop()
-        reject(err)
-      })
-  })
+        console.log(chalk.red("Test Failed With Error: "), err);
+        app.stop();
+        reject(err);
+      });
+  });
 }
 
 // Create New Wallet
@@ -319,10 +319,10 @@ function selectPrivateKey() {
 function privateKeyImportTest() {
   init()
     // TOC Agre
-    .then(() => regStep('#agree'))
+    .then(() => regStep("#agree"))
 
     // Setup Wallet
-    .then(() => regStep('#setupWallet'))
+    .then(() => regStep("#setupWallet"))
 
     // Import Wallet
     .then(() => importWalletTest())
@@ -337,12 +337,12 @@ function privateKeyImportTest() {
     .then(() => dashboardTest())
 
     // Complete
-    .then(() => console.log(chalk.green('Done')))
+    .then(() => console.log(chalk.green("Done")))
     .then(() => app.stop())
     .catch(err => {
-      console.log(chalk.red('Test Failed With Error: '), err)
-      app.stop()
-    })
+      console.log(chalk.red("Test Failed With Error: "), err);
+      app.stop();
+    });
 }
 
 // TOC Agree -> Import Key
@@ -371,4 +371,4 @@ function privateKeyImportTest() {
 
 // createNewWalletTest()
 // keystoreImportTest()
-privateKeyImportTest()
+privateKeyImportTest();
