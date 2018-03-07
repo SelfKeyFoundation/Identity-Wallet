2603322064328543cc474ed0707e1fb8c206c536
Giorgi Maisuradze <g.maisuradze88@gmail.com>
2018-03-07 12:13:13 +0400

    f

--- app/controllers/rpc-handler.js
+++ app/controllers/rpc-handler.js
@@ -350,6 +350,7 @@ module.exports = function (app) {
                 try{
                     fsm.appendFileSync(documentsDirectoryPath + "/" + data.name, new Buffer(data.buffer));
                 }catch(e){
+                    app.log.warn(e);
                     console.log(e);
                     return app.win.webContents.send(RPC_METHOD, actionId, actionName, e, null);
                 }
@@ -360,12 +361,15 @@ module.exports = function (app) {
                     width: 800,
                     height: 600,
                     protocol: 'file:',
-                    slashes: true
+                    slashes: true,
+
+                    //allowDisplayingInsecureContent: true,
+                    //allowRunningInsecureContent: true
                 };
 
                 if(data.mimeType === 'application/pdf'){
                     win = new PDFWindow(config);
-                    win.addSupport(app.win);
+                    PDFWindow.addSupport(app.win)
                 }else{
                     win = new electron.BrowserWindow(config);
                 }
@@ -382,11 +386,13 @@ module.exports = function (app) {
 
                 app.win.webContents.send(RPC_METHOD, actionId, actionName, null, null);
             }).catch((error) => {
+                app.log.warn(error);
                 console.log(error);
                 app.win.webContents.send(RPC_METHOD, actionId, actionName, error, null);
             });
 
         } catch (e) {
+            app.log.warn(e);
             console.log(e);
             app.win.webContents.send(RPC_METHOD, actionId, actionName, e, null);
         }
