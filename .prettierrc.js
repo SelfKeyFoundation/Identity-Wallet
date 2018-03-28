3fdf3ebb9cbbec7068fa0baff515a8037cacadce
altninja <benny.gervais@gmail.com>
2018-03-28 14:54:45 +0700

    chore(prettier): add prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+// .prettierrc.js
+module.exports = {
+	requirePragma: true, // Temp Bypass
+	printWidth: 100,
+	tabWidth: 4,
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+	trailingComma: 'none',
+	bracketSpacing: true,
+	arrowParens: 'avoid',
+	parser: 'flow',
+	proseWrap: 'never'
+}
