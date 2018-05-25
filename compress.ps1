61480be7c0c8ba9c889e36cba460e838b3feec99
altninja <ben@grvs.io>
2018-05-25 07:05:04 +0700

    ci(appveyor): update appveyor config for build umbers

--- compress.ps1
+++ compress.ps1
@@ -1,4 +1,4 @@
+param([string]$output)
 $source = "C:\projects\identity-wallet\out"
-$output = "C:\projects\identity-wallet\sk-windows.zip"
 Add-Type -assembly "system.io.compression.filesystem"
 [io.compression.zipfile]::CreateFromDirectory($source, $output)
