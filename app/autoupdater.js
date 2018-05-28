d7a334e3c05c540405ba3fa15e5f025456beebe3
altninja <ben@grvs.io>
2018-05-28 22:18:03 +0700

    update for electron-builder

--- app/autoupdater.js
+++ app/autoupdater.js
@@ -1,35 +1,29 @@
-'use strict';
+'use strict'
 
-const os = require('os');
-const { app, autoUpdater, dialog } = require('electron');
-const version = app.getVersion();
-const platform = os.platform() + '/' + os.arch();  // usually returns darwin_64
-
-const updaterFeedURL = 'https://selfkey-releases.storage.googleapis.com/SelfKey-Desktop-Identity-Wallet/ea788838ccc9eb63676dd2da87772850/' + platform + '/RELEASES.json?method=JSON&version=' + app.getVersion();
+const { app, autoUpdater, dialog } = require('electron')
+const os = require('os')
+const updaterFeedURL = 'https://selfkey-releases.storage.googleapis.com/SelfKey-Desktop-Identity-Wallet/ea788838ccc9eb63676dd2da87772850/' + os.platform() + '/' + os.arch() + '/RELEASES.json?method=JSON&version=' + app.getVersion()
 
 function appUpdater() {
-    autoUpdater.setFeedURL({
-      url: updaterFeedURL,
-      serverType: 'json'
-    });	
+    autoUpdater.setFeedURL(updaterFeedURL)	
 
     /* Log whats happening
 	TODO send autoUpdater events to renderer so that we could console log it in developer tools
 	You could alsoe use nslog or other logging to see what's happening */
-    autoUpdater.on('error', err => console.log(err));
-    autoUpdater.on('checking-for-update', () => console.log('checking-for-update'));
-    autoUpdater.on('update-available', () => console.log('update-available'));
-    autoUpdater.on('update-not-available', () => console.log('update-not-available'));
+    autoUpdater.on('error', err => console.log(err))
+    autoUpdater.on('checking-for-update', () => console.log('checking-for-update'))
+    autoUpdater.on('update-available', () => console.log('update-available'))
+    autoUpdater.on('update-not-available', () => console.log('update-not-available'))
 
     // Ask the user if update is available
     autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-        let message = app.getName() + ' ' + releaseName + ' is now available. It will be installed the next time you restart the application.';
+        let message = app.getName() + ' ' + releaseName + ' is now available. It will be installed the next time you restart the application.'
         if (releaseNotes) {
-            const splitNotes = releaseNotes.split(/[^\r]\n/);
-            message += '\n\nRelease notes:\n';
+            const splitNotes = releaseNotes.split(/[^\r]\n/)
+            message += '\n\nRelease notes:\n'
             splitNotes.forEach(notes => {
-                message += notes + '\n\n';
-            });
+                message += notes + '\n\n'
+            })
         }
         // Ask user to update the app
         dialog.showMessageBox({
@@ -40,14 +34,14 @@ function appUpdater() {
             detail: message
         }, response => {
             if (response === 0) {
-                setTimeout(() => autoUpdater.quitAndInstall(), 1);
+                setTimeout(() => autoUpdater.quitAndInstall(), 1)
             }
-        });
-    });
+        })
+    })
     // init for updates
-    autoUpdater.checkForUpdates();
+    autoUpdater.checkForUpdates()
 }
 
 module.exports = {
     appUpdater
-};
+}
