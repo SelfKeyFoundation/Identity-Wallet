37f325ba0fad38d0aabd333dce3c9e54aabc4996
altninja <ben@grvs.io>
2018-06-06 19:33:59 +0700

    remove console log

--- src/renderer/angular/controllers/guest/import/keystore-controller.js
+++ src/renderer/angular/controllers/guest/import/keystore-controller.js
@@ -98,7 +98,6 @@ function GuestImportKeystoreController($rootScope, $scope, $log, $q, $timeout, $
         });
 
         unlockExistingWalletPromise.then((data) => {
-            console.log('YES')
             $rootScope.wallet = new Wallet(data.id, data.privateKey, data.publicKey, data.keystoreFilePath, data.profile);
 
             let initialPromises = [];
