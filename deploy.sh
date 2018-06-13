6d465ab7c59cb935eaf88dea5e8544acf05fbf2a
rodrigopavezi <rodrigopavezi@gmail.com>
2018-06-12 21:48:59 -0300

    Fix deployment

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,12 @@ VERSION=$1
 echo "staging $VERSION"
 openssl aes-256-cbc -K $encrypted_40ef2f4394e3_key -iv $encrypted_40ef2f4394e3_iv -in client-secret.json.enc -out client-secret.json -d
 gcloud auth activate-service-account --key-file client-secret.json
-gsutil cp dist/*.zip gs://selfkey-builds/$(date +%Y.%m.%d)-$VERSION/
-gsutil cp dist/*.AppImage gs://selfkey-builds/$(date +%Y.%m.%d)-$VERSION/
+
+if [ "$OSENV" == "CIRCLE" ]
+then
+    gsutil cp dist/*.AppImage gs://selfkey-builds/$(date +%Y.%m.%d)-$VERSION/
+else
+    gsutil cp dist/*.zip gs://selfkey-builds/$(date +%Y.%m.%d)-$VERSION/
+fi
+
 
