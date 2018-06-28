9377b02254437511d5feb9a288fb9a83e4751f20
rodrigopavezi <rodrigopavezi@gmail.com>
2018-06-28 20:49:43 -0300

    revert: only allow crash report to production

--- src/main/controllers/crash-report-service.js
+++ src/main/controllers/crash-report-service.js
@@ -6,19 +6,19 @@ const sentry = require('../assets/data/sentry');
 
 module.exports = {
 	startCrashReport: () => {
-		//if (process.env.NODE_ENV !== 'development' && process.env.MODE !== 'test') {
-		log.info('Starting Crash Report');
-		init({
-			dsn: sentry.dsn,
-			shouldSend: data => {
-				log.info(
-					'guideSettings.hasAgreedToCrashReport()',
-					guideSettings.hasAgreedToCrashReport()
-				);
-				return guideSettings.hasAgreedToCrashReport();
-			}
-		});
-		guideSettings.loadCrashReportAgreement();
-		//}
+		if (process.env.NODE_ENV !== 'development' && process.env.MODE !== 'test') {
+			log.info('Starting Crash Report');
+			init({
+				dsn: sentry.dsn,
+				shouldSend: data => {
+					log.info(
+						'guideSettings.hasAgreedToCrashReport()',
+						guideSettings.hasAgreedToCrashReport()
+					);
+					return guideSettings.hasAgreedToCrashReport();
+				}
+			});
+			guideSettings.loadCrashReportAgreement();
+		}
 	}
 };
