97e35b49947069450de231bf40a03b30d74734f3
rodrigopavezi <rodrigopavezi@gmail.com>
2018-06-28 21:03:44 -0300

    fix: migration to ask for guide again

--- src/main/migrations/20180628203700_guide_settings_add_crash_report.js
+++ src/main/migrations/20180628203700_guide_settings_add_crash_report.js
@@ -2,6 +2,12 @@ exports.up = async (knex, Promise) => {
 	await knex.schema.table('guide_settings', t => {
 		t.integer('crashReportAgreement');
 	});
+	const records = await knex('guide_settings').select();
+	await knex('guide_settings')
+		.update({ guideShown: 0, termsAccepted: 0 })
+		.where({
+			id: records[0].id
+		});
 };
 
 exports.down = async (knex, Promise) => {};
