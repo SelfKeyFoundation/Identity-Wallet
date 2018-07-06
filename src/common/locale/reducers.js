5b818fc85de86ad589836bc4a7602d7acd236221
rodrigopavezi <rodrigopavezi@gmail.com>
2018-07-05 22:44:35 -0300

    fix: remove debug logs

--- src/common/locale/reducers.js
+++ src/common/locale/reducers.js
@@ -6,7 +6,6 @@ const initialState = {
 };
 
 const localeReducer = (state = initialState, action) => {
-	console.log('REDUCER', state, action);
 	switch (action.type) {
 		case types.LOCALE_UPDATE:
 			return {
