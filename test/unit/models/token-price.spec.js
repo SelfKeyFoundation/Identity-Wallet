2343ba694212ba107077c5d3e2942fba36c12609
Maxim Kovalov <max.kovalov@gmail.com>
2018-07-10 15:59:11 +0300

    feat(model-tests): add token-price tests

--- test/unit/models/token-price.spec.js
+++ test/unit/models/token-price.spec.js
@@ -1,21 +1,69 @@
-// const { expect } = require('chai');
-// const TokenPrice = require('../../../src/main/models/token-price');
+const { expect } = require('chai');
+const TokenPrice = require('../../../src/main/models/token-price');
 const db = require('../../utils/db');
 
 describe('TokenPrice model', () => {
+	const testItem = {
+		name: 'test',
+		symbol: 'TST'
+	};
+	const testItem2 = { name: 'test2', symbol: 'TST2' };
 	beforeEach(async () => {
 		await db.reset();
 	});
 
-	xit('create', () => {});
+	it('create', async () => {
+		let itm = await TokenPrice.create(testItem);
+		expect(itm.id).to.be.gt(0);
+		expect(itm.createdAt).to.be.gt(0);
+		expect(itm.updatedAt).to.be.gt(0);
+	});
 
-	xit('findAll', () => {});
+	it('findAll', async () => {
+		let all = await TokenPrice.findAll();
+		expect(all.length).to.eq(0);
+		await TokenPrice.create(testItem);
+		await TokenPrice.create(testItem2);
+		all = await TokenPrice.findAll();
+		expect(all.length).to.eq(2);
+	});
 
-	xit('findAllBySymbol', () => {});
+	it('findAllBySymbol', async () => {
+		let all = await TokenPrice.findAll();
+		expect(all.length).to.eq(0);
+		await TokenPrice.create(testItem);
+		await TokenPrice.create(testItem2);
+		let itm = await TokenPrice.findBySymbol(testItem.symbol);
+		expect(itm.symbol).to.eq(testItem.symbol);
+	});
 
-	xit('updateById', () => {});
+	it('updateById', async () => {
+		let itm = await TokenPrice.create(testItem);
+		let updated = await TokenPrice.updateById(itm.id, { source: 'source˝' });
+		let found = await TokenPrice.query().findById(itm.id);
+		expect(updated).to.deep.eq(found);
+		expect(updated).to.not.deep.eq(itm);
+	});
 
-	xit('bulkEdit', () => {});
+	it('bulkEdit', async () => {
+		let itm1 = await TokenPrice.create(testItem);
+		let itm2 = await TokenPrice.create(testItem2);
+		let all = await TokenPrice.query();
+		expect(all.length).to.eq(2);
+		itm1.source = 'modified';
+		itm2.source = 'modified';
+		await TokenPrice.bulkEdit([itm1, itm2]);
+		all = await TokenPrice.query();
+		expect(all.length).to.eq(2);
+		expect(all[0].source).to.eq('modified');
+		expect(all[1].source).to.eq('modified');
+	});
 
-	xit('bulkAdd', () => {});
+	it('bulkAdd', async () => {
+		let all = await TokenPrice.query();
+		expect(all.length).to.eq(0);
+		await TokenPrice.bulkAdd([testItem, testItem2]);
+		all = await TokenPrice.query();
+		expect(all.length).to.eq(2);
+	});
 });
