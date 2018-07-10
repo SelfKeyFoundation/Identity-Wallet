2ee333ecb07c976874f74884fdefa8264ab769c0
rodrigopavezi <rodrigopavezi@gmail.com>
2018-07-09 23:33:23 -0300

    test: remove import of installation of react extensions

--- src/main/main.js
+++ src/main/main.js
@@ -3,11 +3,6 @@
 import configureStore from 'common/configure-store';
 import { localeUpdate } from 'common/locale/actions';
 
-import installExtension, {
-	REACT_DEVELOPER_TOOLS,
-	REDUX_DEVTOOLS
-} from 'electron-devtools-installer';
-
 const path = require('path');
 const fs = require('fs');
 const electron = require('electron');
