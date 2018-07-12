db80a9a9af2a1566a5c057aae31be3ca498dcef6
Maxim Kovalov <max.kovalov@gmail.com>
2018-07-12 17:37:45 +0300

    fix(tx-history): data.publicKey -> args.publicKey

--- src/main/controllers/rpc-handler.js
+++ src/main/controllers/rpc-handler.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const electron = require('electron');
 const { dialog, Notification, shell, autoUpdater } = require('electron');
 const { isSyncing } = require('./tx-history-service');
@@ -1214,7 +1213,7 @@ module.exports = function(app) {
 	) {
 		TxHistory.findByPublicKeyAndTokenSymbol(args.publicKey, args.tokenSymbol, args.pager)
 			.then(data => {
-				data = { ...data, isSyncing: isSyncing(data.publicKey) };
+				data = { ...data, isSyncing: isSyncing(args.publicKey) };
 				app.win.webContents.send(RPC_METHOD, actionId, actionName, null, data);
 			})
 			.catch(error => {
@@ -1234,7 +1233,7 @@ module.exports = function(app) {
 			args.pager
 		)
 			.then(data => {
-				data = { ...data, isSyncing: isSyncing(data.publicKey) };
+				data = { ...data, isSyncing: isSyncing(args.publicKey) };
 				app.win.webContents.send(RPC_METHOD, actionId, actionName, null, data);
 			})
 			.catch(error => {
@@ -1266,7 +1265,7 @@ module.exports = function(app) {
 	controller.prototype.getTxHistoryByPublicKey = function(event, actionId, actionName, args) {
 		TxHistory.findByPublicKey(args.publicKey, args.pager)
 			.then(data => {
-				data = { ...data, isSyncing: isSyncing(data.publicKey) };
+				data = { ...data, isSyncing: isSyncing(args.publicKey) };
 				app.win.webContents.send(RPC_METHOD, actionId, actionName, null, data);
 			})
 			.catch(error => {
