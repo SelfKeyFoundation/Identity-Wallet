c45be75c5aa0e3ebd84994003e6b30e140ccb96d
tato mchedlishvili <tato.mchedlishvili@gmail.com>
2018-07-12 12:05:00 +0400

    fix: remove async variable from window obj

--- src/main/preload.js
+++ src/main/preload.js
@@ -5,8 +5,6 @@ const config = require('./config');
 const defaultWindowOpen = window.open;
 const async = require('async');
 
-window.async = async;
-
 window.appName = appPackage.productName;
 window.appVersion = appPackage.version;
 
