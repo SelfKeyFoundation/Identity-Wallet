a2c847a3311c4bb43224ff59371a6f0aa5147448
rodrigopavezi <rodrigopavezi@gmail.com>
2018-07-31 14:08:44 -0300

    feat: mac build on circle ci

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,13 @@ echo "staging $VERSION"
 openssl aes-256-cbc -K $encrypted_40ef2f4394e3_key -iv $encrypted_40ef2f4394e3_iv -in client-secret.json.enc -out client-secret.json -d
 gcloud auth activate-service-account --key-file client-secret.json
 
-if [ "$OSENV" == "circle" ]
+if [ "$OSENV" == "linux" ]
 then
-    gsutil cp dist/*.AppImage gs://selfkey-builds/$VERSION/
+    gsutil cp linux/dist/*.AppImage gs://selfkey-builds/$VERSION/
 else
-    gsutil cp dist/*.zip gs://selfkey-builds/$VERSION/
+    gsutil cp mac/dist/*.zip gs://selfkey-builds/$VERSION/
 #    if [ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "dev"  ]
 #    then
-    curl -i -X POST -H "Content-Type: application/json" -d "{\"text\": \"Mac build has been deployed for \n$TRAVIS_COMMIT_MESSAGE \n[(see artifacts)](https://console.cloud.google.com/storage/browser/selfkey-builds/$VERSION/?project=kycchain-master)\", \"channel\": \"id-wallet-builds\" }" $MM_URL
+#    curl -i -X POST -H "Content-Type: application/json" -d "{\"text\": \"Mac build has been deployed for \n$TRAVIS_COMMIT_MESSAGE \n[(see artifacts)](https://console.cloud.google.com/storage/browser/selfkey-builds/$VERSION/?project=kycchain-master)\", \"channel\": \"id-wallet-builds\" }" $MM_URL
 #    fi
 fi
