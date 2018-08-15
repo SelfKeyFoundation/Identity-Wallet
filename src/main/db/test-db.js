e69b6bdd632415a927fc1115ebbc73faa5e3a902
Maxim Kovalov <max.kovalov@gmail.com>
2018-08-15 20:06:22 +0300

    fix(test): remove unneded rollback

--- src/main/db/test-db.js
+++ src/main/db/test-db.js
@@ -1,30 +1,16 @@
 import { db as config } from 'common/config';
-import db from './db';
 import { Model } from 'objection';
 import Knex from 'knex';
 
 export class TestDb {
 	async init() {
-		this.knex = Knex(db.config);
+		this.knex = Knex(config);
 		Model.knex(this.knex);
 		await this.knex.migrate.latest();
 		await this.knex.seed.run();
 	}
 	async reset() {
-		// try {
-		// 	await this.rollbackAll();
-		// } catch (error) {
-		// 	console.error('rollback failed', error);
-		// }
 		await this.init();
-	}
-
-	async rollbackAll() {
-		await this.knex.migrate.forceFreeMigrationsLock();
-		let migration = await this.knex.migrate.currentVersion(config.migrations);
-		if (migration === 'none') return;
-		await this.knex.migrate.rollback(config.migrations);
-		return this.rollbackAll();
 	}
 }
 
