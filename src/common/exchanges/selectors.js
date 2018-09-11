131212b48435860c84c4b906842edce6866df8c6
rodrigopavezi <rodrigopavezi@gmail.com>
2018-09-11 18:11:12 +0200

    feat: normalize the exchange data

--- src/common/exchanges/selectors.js
+++ src/common/exchanges/selectors.js
@@ -1,38 +1,36 @@
 import { getTokens } from 'common/wallet-tokens/selectors';
 
 export const getExchanges = ({ exchanges }) => {
-	return exchanges.list.map(item => {
+	return exchanges.allIds.map(item => {
 		return {
-			name: item.name,
-			status: item.data.status,
-			description: item.data.description,
-			logoUrl: item.data.logo[0].url
+			name: exchanges.byId[item].data.name,
+			status: exchanges.byId[item].data.status,
+			description: exchanges.byId[item].data.description,
+			logoUrl: exchanges.byId[item].data.logo[0].url
 		};
 	});
 };
 
 export const getExchangeLinks = ({ exchanges }) => {
-	return exchanges.list.map(item => {
+	return exchanges.allIds.map(item => {
 		return {
-			name: item.name,
-			url: item.data.URL
+			name: exchanges.byId[item].data.name,
+			url: exchanges.byId[item].data.URL
 		};
 	});
 };
 
 export const getItemDetails = ({ exchanges }, name) => {
-	return exchanges.list.find(item => {
-		return item.name === name;
-	});
+	return exchanges.byId[name];
 };
 
 export const hasBalance = (state, name) => {
-	const exchange = state.exchanges.list.find(item => {
-		return item.name === name;
-	});
+	const exchange = getItemDetails(state, name);
+
 	const keyToken = getTokens(state).find(token => {
 		return token.symbol === 'KEY';
 	});
+
 	const requiredBalance = exchange.data.requiredBalance;
 
 	return keyToken.balance >= requiredBalance;
