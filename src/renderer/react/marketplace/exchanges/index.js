a408c975f3c0e9a4fa6e38a0b4009bb92d6cb3f5
rodrigopavezi <rodrigopavezi@gmail.com>
2018-09-11 18:11:12 +0200

    feat: normalize the exchange data

--- src/renderer/react/marketplace/exchanges/index.js
+++ src/renderer/react/marketplace/exchanges/index.js
@@ -4,8 +4,6 @@ import store from '../../common/store';
 import Exchanges from './containers/exchanges';
 
 export const ExchangesWrapper = props => {
-	console.log('Props', props);
-
 	return (
 		<Provider store={store}>
 			<Exchanges {...props} />
