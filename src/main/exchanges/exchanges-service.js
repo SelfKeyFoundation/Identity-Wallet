52f0878fbeb7ac84e119ce8b5ce71d7069af0084
rodrigopavezi <rodrigopavezi@gmail.com>
2018-09-13 19:10:17 +0200

    feat: add more unit tests

--- src/main/exchanges/exchanges-service.js
+++ src/main/exchanges/exchanges-service.js
@@ -1,8 +1,6 @@
 'use strict';
 import fetch from 'node-fetch';
 import Exchange from './exchange';
-import { Logger } from 'common/logger';
-const log = new Logger('ExchangesService');
 
 const airtableBaseUrl =
 	'https://us-central1-kycchain-master.cloudfunctions.net/airtable?tableName=';
@@ -12,7 +10,6 @@ export class ExchangesService {
 		const response = await fetch(`${airtableBaseUrl}Exchanges`);
 
 		const responseBody = await response.json();
-		log.info('loadExchangeData %j', responseBody);
 
 		const exchanges = responseBody.entities
 			.filter(row => row.data && row.data.name)
@@ -20,8 +17,6 @@ export class ExchangesService {
 				name: row.data.name,
 				data: row.data
 			}));
-
-		log.info('exchanges %j', exchanges);
 
 		return Exchange.import(exchanges);
 	}
