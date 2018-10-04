9b2d290a73a1eb808bf85042a4eb05ac3ecafc9d
rodrigopavezi <rodrigopavezi@gmail.com>
2018-10-04 10:00:57 +0100

    feat: more tests and fix reference to selfkey-ui

--- src/common/transaction/operations.spec.js
+++ src/common/transaction/operations.spec.js
@@ -85,4 +85,104 @@ describe('operations', () => {
 		await store.dispatch(operations.setAddress('0xd0d99d16dbeae0be07e0bbaa5d715bf17ac4f0c8'));
 		expect(store.getActions()).toEqual(expectedActions);
 	});
+
+	it('should call setAddress with error if address is not a address', async () => {
+		const ctx = {
+			web3Service: {
+				web3: {
+					utils: {
+						toChecksumAddress: jest.fn(),
+						isAddress: () => {
+							return false;
+						},
+						isHex: () => {
+							return true;
+						}
+					}
+				}
+			}
+		};
+		setGlobalContext(ctx);
+
+		const store = mockStore({
+			transaction: {
+				address: '',
+				amount: 0,
+				ethFee: 0,
+				usdFee: 0,
+				gasPrice: 0,
+				gasLimit: 0,
+				nouce: 0,
+				signedHex: '',
+				transactionHash: '',
+				addressError: false,
+				sending: false,
+				cryptoCurrency: undefined
+			},
+			wallet: {
+				publicKey: 'dasdsa'
+			},
+			ethGasStationInfo: {
+				ethGasStationInfo: {
+					average: 123
+				}
+			}
+		});
+
+		const address = 'dadsa';
+		const expectedActions = [operations.setAddressError(true)];
+
+		await store.dispatch(operations.setAddress(address));
+		expect(store.getActions()).toEqual(expectedActions);
+	});
+
+	it('should call setAddress with error if address is not a hex', async () => {
+		const ctx = {
+			web3Service: {
+				web3: {
+					utils: {
+						toChecksumAddress: jest.fn(),
+						isAddress: () => {
+							return true;
+						},
+						isHex: () => {
+							return false;
+						}
+					}
+				}
+			}
+		};
+		setGlobalContext(ctx);
+
+		const store = mockStore({
+			transaction: {
+				address: '',
+				amount: 0,
+				ethFee: 0,
+				usdFee: 0,
+				gasPrice: 0,
+				gasLimit: 0,
+				nouce: 0,
+				signedHex: '',
+				transactionHash: '',
+				addressError: false,
+				sending: false,
+				cryptoCurrency: undefined
+			},
+			wallet: {
+				publicKey: 'dasdsa'
+			},
+			ethGasStationInfo: {
+				ethGasStationInfo: {
+					average: 123
+				}
+			}
+		});
+
+		const address = 'dadsa';
+		const expectedActions = [operations.setAddressError(true)];
+
+		await store.dispatch(operations.setAddress(address));
+		expect(store.getActions()).toEqual(expectedActions);
+	});
 });
