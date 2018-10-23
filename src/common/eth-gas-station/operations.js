aa45586a56ae8b991300660a799b8cd6f6f9c915
tato mchedlishvili <tato.mchedlishvili@gmail.com>
2018-10-23 10:10:29 +0400

    fix: remove commented lines

--- src/common/eth-gas-station/operations.js
+++ src/common/eth-gas-station/operations.js
@@ -1,8 +1,6 @@
 import * as actions from './actions';
 import { getGlobalContext } from 'common/context';
-// import EthGasStationService from 'main/blockchain/eth-gas-station-service';
 import { transactionOperations } from 'common/transaction';
-// const ethGasStationService = new EthGasStationService();
 
 const loadData = () => async (dispatch, getState) => {
 	const data = await getGlobalContext().ethGasStationService.getInfo();
