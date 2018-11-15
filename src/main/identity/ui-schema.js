d1a385c58aed65f783b94da56b65139e219fe0e3
Maxim Kovalov <max.kovalov@gmail.com>
2018-11-15 21:39:21 +0200

    feat(jsonschema-attributes): load remote ui-schema

--- src/main/identity/ui-schema.js
+++ src/main/identity/ui-schema.js
@@ -1,5 +1,12 @@
-import { Model } from 'objection';
+import { Model, transaction } from 'objection';
 import BaseModel from '../common/base-model';
+import fetch from 'node-fetch';
+import { Logger } from 'common/logger';
+
+const log = new Logger('ui-schema-model');
+
+const UI_SCHEMA_EXPIRES = 86400000; // 1 day
+
 const TABLE_NAME = 'ui_schema';
 
 export class UiSchema extends BaseModel {
@@ -45,8 +52,8 @@ export class UiSchema extends BaseModel {
 		return this.query(tx).findOne({ url, repositoryId });
 	}
 
-	static async findById(id) {
-		return this.query().findById(id);
+	static async findById(id, tx) {
+		return this.query(tx).findById(id);
 	}
 
 	static create(itm, tx) {
@@ -55,6 +62,50 @@ export class UiSchema extends BaseModel {
 
 	static delete(id, tx) {
 		return this.query(tx).deleteById(id);
+	}
+
+	static async loadRemote(url) {
+		let res = await fetch(url);
+		if (res.statusCode >= 400) {
+			throw new Error('Failed to fetch ui-schema from remote');
+		}
+		let remote = await res.json();
+
+		let remoteSchema = {
+			url,
+			content: remote,
+			expires: Date.now() + (remote.expires || UI_SCHEMA_EXPIRES)
+		};
+
+		return remoteSchema;
+	}
+
+	static async addRemote(url, repositoryId, attributeTypeId) {
+		let [remote, attrType] = await Promise.all([
+			this.loadRemote(url),
+			this.findByUrl(url, repositoryId)
+		]);
+		if (!remote) {
+			log.error('could not load ui schema %s from remote', url);
+			return;
+		}
+		remote.repositoryId = repositoryId;
+		remote.attributeTypeId = attributeTypeId;
+		const tx = await transaction.start(this.knex());
+		try {
+			if (!attrType) {
+				attrType = await this.create(remote, tx);
+			} else {
+				attrType = await attrType.$query(tx).patchAndFetch(remote);
+			}
+
+			await tx.commit();
+			return attrType;
+		} catch (error) {
+			log.error(error);
+			await tx.rollback(error);
+			throw error;
+		}
 	}
 }
 
