92d17c35f52e52913ded478fbefd5f61bb3452dd
Maxim Kovalov <max.kovalov@gmail.com>
2018-11-16 17:10:13 +0200

    feat(redux): define operations for wallet, identity and tokens

---
+++ src/common/identity/identity-duck.spec.js
@@ -0,0 +1,32 @@
+describe('Identity Duck', () => {
+	it('', () => {});
+	// Operations:
+	// - LOAD Repositories
+	// - LOAD IdAttribute Types
+	// - LOAD UiSchemas
+	// - LOAD document binary -- with binary
+	// - EDIT id attribute
+	// - REMOVE id-attribute
+	// - CREATE id-attribute
+	// - LOCK IDENTITY
+	//   - DELETE id attributres
+	//   - DELETE documents
+	// - UNLOCK IDENTITY
+	//   - LOAD idAttributes
+	//   - LOAD documents  - without binary
+	// Actions:
+	// - SET Repositories loading
+	// - Set id attribute types loading
+	// - set ui schema is loading
+	// - set documents are loading
+	// - set document is loadin
+	// - set id-attribute update
+	// - set id-attribute-types
+	// - set id-attributes
+	// - update id-attribute
+	// - add id-attribute
+	// - delete id-attribute
+	// - set documents
+	// - delete document
+	// - update document
+});
---
+++ src/common/wallet/wallet-duck.spec.js
@@ -0,0 +1,27 @@
+describe('wallet duck', () => {
+	it('', () => {});
+	// OPERAIONTS
+	// - LOAD wallets
+	// - LOAD wallet tokens
+	// - CREATE wallet token
+	// - EDIT wallet token
+	// - LOAD wallet settings
+	// - LOAD wallet action  log
+	// - CREATE action log item
+	// - CREATE wallet
+	// - UNLOCK wallet
+	//   - --> UNLOCK identity
+	// - LOCK wallet
+	//   - --> LOCK identity
+	// ACTIONS
+	// - SET wallet action log
+	// - ADD wallet action log item
+	// - SET wallets
+	// - SET wallet tokens
+	// - ADD wallet token
+	// - UPDATE wallet token
+	// - SET unlocked wallet
+	// - DELETE action logs for wallet
+	// - DELETE wallet tokens for wallet
+	// - DELETE wallet settings
+});
