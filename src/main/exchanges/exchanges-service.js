3af5e13b0350e1728c3da01c70563f35ee5d63e0
Maxim Kovalov <max.kovalov@gmail.com>
2019-01-15 15:30:46 +0200

    feat(exchange-config): add relying party config

--- src/main/exchanges/exchanges-service.js
+++ src/main/exchanges/exchanges-service.js
@@ -1,22 +1,36 @@
 'use strict';
 import fetch from 'node-fetch';
 import Exchange from './exchange';
+import { isDevMode } from 'common/utils/common';
+import { Logger } from 'common/logger';
+
+const log = new Logger('ExchangesService');
 
 const airtableBaseUrl =
 	'https://us-central1-kycchain-master.cloudfunctions.net/airtable?tableName=';
 
 export class ExchangesService {
 	async loadExchangeData() {
-		const response = await fetch(`${airtableBaseUrl}Exchanges`);
+		log.info(`Fetching exchanges from ${airtableBaseUrl}Exchanges${isDevMode() ? 'Dev' : ''}`);
+		const response = await fetch(`${airtableBaseUrl}Exchanges${isDevMode() ? 'Dev' : ''}`);
 
 		const responseBody = await response.json();
-
 		const exchanges = responseBody.entities
 			.filter(row => row.data && row.data.name)
-			.map(row => ({
-				name: row.data.name,
-				data: row.data
-			}));
+			.map(row => {
+				const { data } = row;
+				if (data.relying_party_config) {
+					try {
+						data.relying_party_config = JSON.parse(data.relying_party_config);
+					} catch (error) {
+						log.error(error);
+					}
+				}
+				return {
+					name: data.name,
+					data: data
+				};
+			});
 
 		return Exchange.import(exchanges);
 	}
