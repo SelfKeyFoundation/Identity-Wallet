754fb62aafbcbcc3e965edc234cae4ef5479f8e3
designhorf <designhorf@gmail.com>
2019-01-28 09:14:46 +0100

    feat: added basic companies component but hide from tabs

--- src/renderer/selfkey-id/main/components/selfkey-id.jsx
+++ src/renderer/selfkey-id/main/components/selfkey-id.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Grid, Typography, Tabs, Tab } from '@material-ui/core';
 import SelfkeyIdOverview from './selfkey-id-overview';
 import SelfkeyIdApplications from './selfkey-id-applications';
+// import SelfkeyIdCompanies from './selfkey-id-companies';
 import SelfkeyIdHistory from './selfkey-id-history';
 
 class SelfkeyId extends Component {
@@ -17,6 +18,8 @@ class SelfkeyId extends Component {
 		let component = <SelfkeyIdOverview {...this.props} />;
 		if (this.state.tabValue === 1) {
 			component = <SelfkeyIdApplications {...this.props} />;
+			// } else if (this.state.tabValue === 2) {
+			// 	component = <SelfkeyIdCompanies {...this.props} />;
 		} else if (this.state.tabValue === 3) {
 			component = <SelfkeyIdHistory {...this.props} />;
 		}
@@ -30,7 +33,7 @@ class SelfkeyId extends Component {
 					<Tabs value={this.state.tabValue} onChange={this.handleChange}>
 						<Tab label="Overview" />
 						<Tab label="Marketplace Applications" />
-						<Tab label="Companies" />
+						{/* <Tab label="Companies" /> */}
 						<Tab label="History" />
 					</Tabs>
 				</Grid>
