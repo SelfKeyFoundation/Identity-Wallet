a3296849f065da9283f98beb4db7b6cd2c7a54a9
Eduardo Santiago Moura <eduardosmoura@gmail.com>
2019-02-05 15:01:39 -0300

    feat(address-book): create add, edit, and delete features

--- src/renderer/address-book/edit/index.jsx
+++ src/renderer/address-book/edit/index.jsx
@@ -1,33 +1,158 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { AddressBookEdit, ModalBox } from 'selfkey-ui';
+import { StyledButton, ModalBox } from 'selfkey-ui';
 import { addressBookSelectors, addressBookOperations } from 'common/address-book';
+import { Grid } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+	errorText: {
+		height: '19px',
+		width: '242px',
+		color: '#FE4B61',
+		fontFamily: 'Lato',
+		fontSize: '13px',
+		lineHeight: '19px'
+	},
+
+	errorColor: {
+		color: '#FE4B61 !important',
+		border: '2px solid #FE4B61 !important',
+		backgroundColor: 'rgba(255,46,99,0.09) !important'
+	},
+
+	input: {
+		boxSizing: 'border-box',
+		height: '46px',
+		width: '722px',
+		border: '1px solid #384656',
+		borderRadius: '4px',
+		backgroundColor: '#1E262E',
+		color: '#a9c5d6',
+		fontSize: '14px',
+		boxShadow:
+			'inset -1px 0 0 0 rgba(0,0,0,0.24), 1px 0 0 0 rgba(118,128,147,0.2), 2px 0 2px 0 rgba(0,0,0,0.2)',
+		paddingLeft: '10px',
+		'&::-webkit-input-placeholder': {
+			fontSize: '14px',
+			color: '#93B0C1'
+		}
+	},
+
+	inputError: {
+		borderBottom: '2px solid #FE4B61 !important'
+	},
+
+	label: {
+		color: '#93A4AF',
+		fontSize: '12px',
+		fontWeight: 'bold',
+		lineHeight: '15px'
+	}
+});
 
 class AddressBookEditContainer extends Component {
+	state = {
+		id: undefined,
+		label: ''
+	};
+
 	componentDidMount() {
+		const id = this.props.id;
+		const label = this.props.label;
+		this.setState({
+			...this.state,
+			id: id,
+			label: label
+		});
 		this.props.dispatch(addressBookOperations.resetEdit());
 	}
 
-	handleSave = async label => {
-		const id = this.props.id;
-		await this.props.dispatch(addressBookOperations.editAddressBookEntry({ id, label }));
-		this.props.closeAction();
-	};
-
-	handleLabelChange = label => {
+	handleLabelChange = event => {
+		event.preventDefault();
+		const label = event.target.value;
+		this.setState({
+			...this.state,
+			label
+		});
 		this.props.dispatch(addressBookOperations.validateLabel(label));
 	};
 
+	handleSubmit = event => {
+		event.preventDefault();
+		return this.handleEdit(this.state.label);
+	};
+
+	handleEdit = async label => {
+		const id = this.props.id;
+		await this.props.dispatch(addressBookOperations.editAddressBookEntry({ id, label }));
+		this.props.confirmAction({ id, label });
+		this.props.closeAction();
+	};
+
 	render() {
+		const { classes, labelError } = this.props;
+		const hasLabelError = labelError !== '' && labelError !== undefined;
+		const labelInputClass = `${classes.input} ${hasLabelError ? classes.errorColor : ''}`;
+
 		return (
 			<ModalBox closeAction={this.props.closeAction} headerText="Edit Label">
-				<AddressBookEdit
-					onCancel={this.props.onCancel}
-					onSave={this.handleSave}
-					onLabelChange={this.handleLabelChange}
-					labelError={this.props.labelError}
-					label={this.props.label}
-				/>
+				<form
+					className={classes.container}
+					noValidate
+					autoComplete="off"
+					onSubmit={this.handleSubmit}
+				>
+					<Grid container direction="column" spacing={32}>
+						<Grid item>
+							<Grid container direction="column" spacing={8}>
+								<Grid item>
+									<label className={classes.label}>LABEL</label>
+								</Grid>
+								<Grid item>
+									<input
+										type="text"
+										id="labelInput"
+										onChange={this.handleLabelChange}
+										value={this.state.label}
+										className={labelInputClass}
+										placeholder="Address label"
+									/>
+									{hasLabelError && (
+										<span id="labelError" className={classes.errorText}>
+											{labelError}
+										</span>
+									)}
+								</Grid>
+							</Grid>
+						</Grid>
+						<Grid item>
+							<Grid container direction="row" spacing={24}>
+								<Grid item>
+									<StyledButton
+										id="saveButton"
+										variant="contained"
+										size="medium"
+										type="submit"
+										disabled={!this.state.label || hasLabelError}
+									>
+										Save
+									</StyledButton>
+								</Grid>
+								<Grid item>
+									<StyledButton
+										id="cancelButton"
+										variant="outlined"
+										size="medium"
+										onClick={this.props.closeAction}
+									>
+										Cancel
+									</StyledButton>
+								</Grid>
+							</Grid>
+						</Grid>
+					</Grid>
+				</form>
 			</ModalBox>
 		);
 	}
@@ -40,4 +165,5 @@ const mapStateToProps = (state, props) => {
 	};
 };
 
-export default connect(mapStateToProps)(AddressBookEditContainer);
+const styledComponent = withStyles(styles)(AddressBookEditContainer);
+export default connect(mapStateToProps)(styledComponent);
--- src/renderer/address-book/main/index.jsx
+++ src/renderer/address-book/main/index.jsx
@@ -1,19 +1,276 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { AddressBook } from 'selfkey-ui';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { addressBookSelectors, addressBookOperations } from 'common/address-book';
+import AddressBookAddContainer from 'renderer/address-book/add';
+import AddressBookEditContainer from 'renderer/address-book/edit';
+import AddressBookIcon from 'renderer/address-book/icon';
+import { H2, CopyIcon, DeleteIcon, EditTransparentIcon } from 'selfkey-ui';
+import { withStyles } from '@material-ui/core/styles';
+import {
+	Grid,
+	Typography,
+	Button,
+	Table,
+	TableHead,
+	TableRow,
+	TableCell,
+	TableBody,
+	IconButton
+} from '@material-ui/core';
+
+const styles = theme => ({
+	addressBook: {
+		width: '1140px'
+	},
+
+	button: {
+		height: '44px',
+		width: '180px',
+		border: '2px solid #1CA9BA',
+		borderRadius: '3px',
+		boxShadow: 'inset 3px 3px 10px 0 rgba(0,0,0,0.1)',
+		color: '#1CA9BA',
+		fontSize: '16px',
+		fontWeight: 'bold',
+		letterSpacing: '0.67px',
+		lineHeight: '19px',
+		textAlign: 'center'
+	},
+
+	descriptionText: {
+		color: '#93B0C1',
+		fontSize: '18px',
+		lineHeight: '28px',
+		textAlign: 'center'
+	},
+
+	table: {
+		borderSpacing: '0px',
+		minWidth: '900px',
+		'& tbody tr:nth-child(odd)': {
+			background: '#262f39'
+		}
+	},
+
+	headerTableRow: {
+		height: '38px',
+		'& th': {
+			fontSize: '12px',
+			fontWeight: 600,
+			textAlign: 'left',
+			color: '#7f8fa4',
+			textTransform: 'uppercase',
+			borderBottom: '0px',
+			paddingLeft: '0px'
+		},
+		'& th:first-child': {
+			paddingLeft: '24px !important'
+		}
+	},
+
+	bodyTableRow: {
+		height: '74px',
+		cursor: 'pointer',
+		'& td': {
+			padding: '0px',
+			fontSize: '15px',
+			textAlign: 'left',
+			color: '#ffffff',
+			borderBottom: '0px'
+		},
+		'& td:first-child': {
+			paddingLeft: '24px !important'
+		}
+	}
+});
 
 class AddressBookContainer extends Component {
+	state = {
+		addresses: [],
+		showModalAdd: false,
+		showModalEdit: false,
+		id: undefined,
+		label: ''
+	};
+
 	componentDidMount() {
 		this.props.dispatch(addressBookOperations.loadAddressBook());
 	}
+
+	componentDidUpdate(prevProps) {
+		if (prevProps.addresses !== this.props.addresses) {
+			this.setState({
+				...this.state,
+				addresses: this.props.addresses
+			});
+		}
+	}
+
+	openModalAdd = () => {
+		this.setState({
+			...this.state,
+			showModalAdd: true,
+			showModalEdit: false
+		});
+	};
+
+	closeModalAdd = () => {
+		this.setState({
+			...this.state,
+			showModalAdd: false
+		});
+	};
+
+	openModalEdit = () => {
+		this.setState({
+			...this.state,
+			showModalAdd: false,
+			showModalEdit: true
+		});
+	};
+
+	confirmModalEdit = ({ id, label }) => {
+		const addresses = this.props.addresses.slice();
+		const address = addresses.find(address => {
+			return address.id === id;
+		});
+		address.label = label;
+		this.setState({
+			...this.state,
+			addresses: addresses
+		});
+	};
+
+	closeModalEdit = () => {
+		this.setState({
+			...this.state,
+			showModalEdit: false
+		});
+	};
+
+	handleEdit = id => {
+		const address = this.props.addresses.find(address => {
+			return address.id === id;
+		});
+		this.setState(
+			{
+				...this.state,
+				id: id,
+				label: address.label
+			},
+			function() {
+				return this.openModalEdit();
+			}
+		);
+	};
 
 	handleDelete = id => {
 		this.props.dispatch(addressBookOperations.deleteAddressBookEntry(id));
 	};
 
 	render() {
-		return <AddressBook onDelete={this.handleDelete} {...this.props} />;
+		const { classes } = this.props;
+		const { showModalAdd, showModalEdit, addresses } = this.state;
+		return (
+			<Grid
+				container
+				direction="column"
+				justify="center"
+				alignItems="center"
+				className={classes.addressBook}
+				spacing={32}
+			>
+				<Grid item>
+					<AddressBookIcon />
+				</Grid>
+				<Grid item>
+					<H2>Address Book</H2>
+				</Grid>
+				<Grid item>
+					<Typography variant="body1" className={classes.descriptionText}>
+						Create and assign labels to save commonly used Ethereum addresses when
+						sending assets from the SelfKey Identity Wallet.
+					</Typography>
+				</Grid>
+				<Grid item>
+					<Button
+						id="addAddressButton"
+						className={classes.button}
+						onClick={this.openModalAdd}
+					>
+						ADD ADDRESS
+					</Button>
+				</Grid>
+				<Grid item>
+					<Table className={classes.table}>
+						<TableHead>
+							<TableRow className={classes.headerTableRow}>
+								<TableCell>LABEL</TableCell>
+								<TableCell>ETH ADDRESS</TableCell>
+								<TableCell>ACTIONS</TableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{addresses &&
+								addresses.map(address => {
+									return (
+										<TableRow key={address.id} className={classes.bodyTableRow}>
+											<TableCell id={address.id}>{address.label}</TableCell>
+											<TableCell>{address.address}</TableCell>
+											<TableCell>
+												<Grid
+													container
+													direction="row"
+													justify="flex-start"
+													alignItems="center"
+												>
+													<Grid item>
+														<IconButton>
+															<CopyToClipboard text={address.address}>
+																<CopyIcon />
+															</CopyToClipboard>
+														</IconButton>
+													</Grid>
+													<Grid item>
+														<IconButton
+															id="editButton"
+															onClick={() =>
+																this.handleEdit(address.id)
+															}
+														>
+															<EditTransparentIcon />
+														</IconButton>
+													</Grid>
+													<Grid item>
+														<IconButton
+															id="deleteButton"
+															onClick={() =>
+																this.handleDelete(address.id)
+															}
+														>
+															<DeleteIcon />
+														</IconButton>
+													</Grid>
+												</Grid>
+											</TableCell>
+										</TableRow>
+									);
+								})}
+						</TableBody>
+					</Table>
+				</Grid>
+				{showModalAdd && <AddressBookAddContainer closeAction={this.closeModalAdd} />}
+				{showModalEdit && (
+					<AddressBookEditContainer
+						closeAction={this.closeModalEdit}
+						confirmAction={this.confirmModalEdit}
+						id={this.state.id}
+						label={this.state.label}
+					/>
+				)}
+			</Grid>
+		);
 	}
 }
 
@@ -23,4 +280,5 @@ const mapStateToProps = (state, props) => {
 	};
 };
 
-export default connect(mapStateToProps)(AddressBookContainer);
+const styledComponent = withStyles(styles)(AddressBookContainer);
+export default connect(mapStateToProps)(styledComponent);
