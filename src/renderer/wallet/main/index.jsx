d9225b8bc3c72b95d17ee74dd7d957828cd8df7e
Andre Goncalves <andre@andregoncalves.com>
2019-02-07 21:09:49 +0000

    feat: add receive transaction screen with qr code for #606

--- src/renderer/wallet/main/index.jsx
+++ src/renderer/wallet/main/index.jsx
@@ -14,6 +14,7 @@ import {
 
 import { SelfkeyIdContainer } from '../../selfkey-id/main';
 import Transfer from '../../transaction/send';
+import ReceiveTransfer from '../../transaction/receive';
 import { walletTokensOperations } from 'common/wallet-tokens';
 
 import { Grid, withStyles } from '@material-ui/core';
@@ -86,6 +87,14 @@ class Main extends Component {
 					<Route
 						path={`${match.path}/transfer/custom`}
 						render={props => <Transfer cryptoCurrency="" />}
+					/>
+					<Route
+						path={`${match.path}/transfer/receive/:crypto`}
+						render={props => (
+							<ReceiveTransfer
+								cryptoCurrency={props.match.params.crypto.toUpperCase()}
+							/>
+						)}
 					/>
 				</Grid>
 			</Grid>
