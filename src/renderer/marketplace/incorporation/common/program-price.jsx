fc0becfe38512eab5be69c11b58103fc4dffdb3e
Andre Goncalves <andre@andregoncalves.com>
2019-02-12 22:24:34 +0000

    feat: format KEY price values

--- src/renderer/marketplace/incorporation/common/program-price.jsx
+++ src/renderer/marketplace/incorporation/common/program-price.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
+const formatNumber = (number) => {
+	let splitNum = number.toString().split('.');
+	// Format number with thousands separator
+	splitNum[0] = splitNum[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+	return splitNum.join('.');
+}
+
 const ProgramPrice = props => {
 	const { price, rate, label } = props;
 
@@ -8,7 +15,7 @@ const ProgramPrice = props => {
 	}
 
 	const numeric = parseInt(price.replace(/\$/, '').replace(/,/, ''));
-	const key = Math.round(numeric / rate);
+	const key = formatNumber(numeric / rate);
 
 	return (
 		<div className="price">
