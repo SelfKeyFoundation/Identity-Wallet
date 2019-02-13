bbe827ce3dbff868da75effc405436aff7caa7da
Andre Goncalves <andre@andregoncalves.com>
2019-02-13 09:49:45 +0000

    fix: token KEY number formatting

--- src/renderer/marketplace/incorporation/common/program-price.jsx
+++ src/renderer/marketplace/incorporation/common/program-price.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const formatNumber = (number) => {
-	let splitNum = number.toString().split('.');
+const formatNumber = number => {
+	// 4 decimal places for KEY values ?
+	const numeric = Math.abs(number).toFixed(4);
+	let splitNum = numeric.split('.');
 	// Format number with thousands separator
 	splitNum[0] = splitNum[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 	return splitNum.join('.');
-}
+};
 
 const ProgramPrice = props => {
 	const { price, rate, label } = props;
