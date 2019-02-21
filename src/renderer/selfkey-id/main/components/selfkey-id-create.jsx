c85b0fbb89833f624583959cb72cd66a0b5a3663
designhorf <designhorf@gmail.com>
2019-02-21 17:39:56 +0100

    fix: style and position of Back button

--- src/renderer/selfkey-id/main/components/selfkey-id-create.jsx
+++ src/renderer/selfkey-id/main/components/selfkey-id-create.jsx
@@ -12,9 +12,14 @@ const styles = theme => ({
 		border: 'none',
 		backgroundColor: 'transparent'
 	},
-	back: {
+	backButtonContainer: {
+		left: '40px',
 		position: 'absolute',
-		left: '-200px'
+		top: '40px',
+		zIndex: '1301'
+	},
+	bold: {
+		fontWeight: 600
 	},
 	hr: {
 		backgroundColor: '#303C49',
@@ -41,147 +46,158 @@ class SelfKeyIdCreateComponent extends Component {
 	render() {
 		const { classes } = this.props;
 		return (
-			<Modal open={true}>
-				<ModalWrap className={classes.modalWrap}>
+			<>
+				<div className={classes.backButtonContainer}>
 					<Button
 						variant="outlined"
 						color="secondary"
 						size="small"
 						onClick={this.handleBackClick}
-						className={classes.back}
 					>
-						‹ Back
+						<Typography variant="subtitle2" color="secondary" className={classes.bold}>
+							‹ Back
+						</Typography>
 					</Button>
-					<Grid
-						container
-						direction="column"
-						justify="flex-start"
-						alignItems="center"
-						spacing={32}
-					>
-						<Grid item>
-							<IdCardIcon />
-						</Grid>
-						<Grid item>
-							<Typography variant="h1">Setup your Selfkey Identity</Typography>
-						</Grid>
-						<Grid item>
-							<Typography variant="body1" color="secondary" gutterBottom>
-								Create a new Selfkey ID to start building your identity profile.
-							</Typography>
-						</Grid>
-						<Grid item>
-							<Card>
-								<CardContent>
-									<Grid
-										container
-										direction="column"
-										justify="center"
-										alignItems="center"
-										spacing={32}
-									>
-										<Grid container item spacing={0} justify="space-between">
+				</div>
+				<Modal open={true}>
+					<ModalWrap className={classes.modalWrap}>
+						<Grid
+							container
+							direction="column"
+							justify="flex-start"
+							alignItems="center"
+							spacing={32}
+						>
+							<Grid item>
+								<IdCardIcon />
+							</Grid>
+							<Grid item>
+								<Typography variant="h1">Setup your Selfkey Identity</Typography>
+							</Grid>
+							<Grid item>
+								<Typography variant="body1" color="secondary" gutterBottom>
+									Create a new Selfkey ID to start building your identity profile.
+								</Typography>
+							</Grid>
+							<Grid item>
+								<Card>
+									<CardContent>
+										<Grid
+											container
+											direction="column"
+											justify="center"
+											alignItems="center"
+											spacing={32}
+										>
 											<Grid
 												container
-												xs={3}
-												justify="end"
-												alignItems="center"
-												direction="column"
-												wrap="nowrap"
-												spacing={32}
-												className={classes.info}
+												item
+												spacing={0}
+												justify="space-between"
 											>
-												<Grid item>
-													<SKIDIcon />
+												<Grid
+													container
+													xs={3}
+													justify="end"
+													alignItems="center"
+													direction="column"
+													wrap="nowrap"
+													spacing={32}
+													className={classes.info}
+												>
+													<Grid item>
+														<SKIDIcon />
+													</Grid>
 												</Grid>
-											</Grid>
-											<Grid item xs={9}>
-												<Grid item>
-													<Typography variant="body1" gutterBottom>
-														I am creating a new Selfkey Identity Profile
-													</Typography>
-													<Typography
-														variant="subtitle2"
-														color="secondary"
-														gutterBottom
-													>
-														Setup the Selfkey ID. This will form the{' '}
-														basis of your locally managed and stored{' '}
-														identity profile.
-													</Typography>
-												</Grid>
-												<Grid item>
-													<Button
-														variant="contained"
-														size="large"
-														className={classes.create}
-														component={selfkeyIdCreateAbout}
-													>
-														Create Selfkey ID
-													</Button>
+												<Grid item xs={9}>
+													<Grid item>
+														<Typography variant="body1" gutterBottom>
+															I am creating a new Selfkey Identity
+															Profile
+														</Typography>
+														<Typography
+															variant="subtitle2"
+															color="secondary"
+															gutterBottom
+														>
+															Setup the Selfkey ID. This will form the{' '}
+															basis of your locally managed and stored{' '}
+															identity profile.
+														</Typography>
+													</Grid>
+													<Grid item>
+														<Button
+															variant="contained"
+															size="large"
+															className={classes.create}
+															component={selfkeyIdCreateAbout}
+														>
+															Create Selfkey ID
+														</Button>
+													</Grid>
 												</Grid>
 											</Grid>
 										</Grid>
-									</Grid>
-								</CardContent>
-								{/* <hr className={classes.hr} />
-								<CardContent>
-									<Grid
-										container
-										direction="column"
-										justify="center"
-										alignItems="center"
-										spacing={32}
-									>
-										<Grid container item spacing={0} justify="space-between">
-											<Grid
-												container
-												xs={3}
-												justify="end"
-												alignItems="center"
-												direction="column"
-												wrap="nowrap"
-												spacing={32}
-												className={classes.info}
-											>
-												<Grid item>
-													<BookIcon />
+									</CardContent>
+									{/* <hr className={classes.hr} />
+									<CardContent>
+										<Grid
+											container
+											direction="column"
+											justify="center"
+											alignItems="center"
+											spacing={32}
+										>
+											<Grid container item spacing={0} justify="space-between">
+												<Grid
+													container
+													xs={3}
+													justify="end"
+													alignItems="center"
+													direction="column"
+													wrap="nowrap"
+													spacing={32}
+													className={classes.info}
+												>
+													<Grid item>
+														<BookIcon />
+													</Grid>
 												</Grid>
-											</Grid>
-											<Grid item xs={9}>
-												<Grid item>
-													<Typography variant="body1" gutterBottom>
-														I was approved in the Selfkey token sale KYC
-													</Typography>
-													<Typography
-														variant="subtitle2"
-														color="secondary"
-														gutterBottom
-													>
-														If you were whitelisted for the Selfkey token sale,{' '}
-														you can download a zip file and import to complete{' '}
-														your Selfkey ID.
-													</Typography>
-												</Grid>
-												<Grid item>
-													<Button
-														variant="outlined"
-														size="large"
-														className={classes.create}
-														onClick={this.handleAddAttribute}
-													>
-														Import KYC File
-													</Button>
+												<Grid item xs={9}>
+													<Grid item>
+														<Typography variant="body1" gutterBottom>
+															I was approved in the Selfkey token sale KYC
+														</Typography>
+														<Typography
+															variant="subtitle2"
+															color="secondary"
+															gutterBottom
+														>
+															If you were whitelisted for the Selfkey token sale,{' '}
+															you can download a zip file and import to complete{' '}
+															your Selfkey ID.
+														</Typography>
+													</Grid>
+													<Grid item>
+														<Button
+															variant="outlined"
+															size="large"
+															className={classes.create}
+															onClick={this.handleAddAttribute}
+														>
+															Import KYC File
+														</Button>
+													</Grid>
 												</Grid>
 											</Grid>
 										</Grid>
-									</Grid>
-								</CardContent> */}
-							</Card>
+									</CardContent> */}
+								</Card>
+							</Grid>
 						</Grid>
-					</Grid>
-				</ModalWrap>
-			</Modal>
+					</ModalWrap>
+				</Modal>
+			</>
 		);
 	}
 }
