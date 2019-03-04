b9ea7bbee03df9028f385f66a7d677b2e12c5fd4
Maxim Kovalov <max.kovalov@gmail.com>
2019-03-04 17:38:16 +0200

    fix(id-attributes): fix error message for required string

--- src/renderer/selfkey-id/main/components/transform-errors.js
+++ src/renderer/selfkey-id/main/components/transform-errors.js
@@ -1,7 +1,11 @@
 export default errors => {
+	console.log('XXX errors', errors);
 	return errors.map(error => {
 		if (error.name === 'pattern') {
 			error.message = '';
+		}
+		if (error.message === 'should be string') {
+			error.message = 'This field is required';
 		}
 		return error;
 	});
