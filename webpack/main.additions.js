1ef05d4adcb420499449db21462ff9dfd97d4416
Maxim Kovalov <max.kovalov@gmail.com>
2019-03-04 17:18:44 +0200

    feat(hardsource-plugin): fix hardsource integration

--- webpack/main.additions.js
+++ webpack/main.additions.js
@@ -1,5 +1,5 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -19,6 +19,8 @@ module.exports = {
 		]
 	},
 	plugins: [
+		new HardSourceWebpackPlugin(),
+
 		new CopyWebpackPlugin([
 			{
 				from: path.join(__dirname, '/../src/main/migrations'),
