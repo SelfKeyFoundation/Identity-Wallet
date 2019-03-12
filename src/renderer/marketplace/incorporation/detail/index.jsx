cfd130b2a11c6df172a70f28d277e1b100722175
Maxim Kovalov <max.kovalov@gmail.com>
2019-03-12 13:43:35 +0200

    feat(incorporations): fix cancel payment

--- src/renderer/marketplace/incorporation/detail/index.jsx
+++ src/renderer/marketplace/incorporation/detail/index.jsx
@@ -287,7 +287,7 @@ class IncorporationsDetailView extends Component {
 
 	userHasPaid = () => {
 		const application = this.getLastApplication();
-		if (!application) {
+		if (!application || !application.payments) {
 			return false;
 		}
 		return !!application.payments.length;
