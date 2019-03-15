68a6322721e059708563ed7dbb7e03545948eb72
Eduardo Santiago Moura <eduardosmoura@gmail.com>
2019-03-15 07:26:12 -0300

    fix(custom-token): add logger on error message

--- src/common/tokens/operations.js
+++ src/common/tokens/operations.js
@@ -2,6 +2,9 @@ import * as actions from './actions';
 import * as types from './types';
 import { getGlobalContext } from 'common/context';
 import { createAliasedAction } from 'electron-redux';
+import { Logger } from 'common/logger';
+
+const log = new Logger('tokens-duck');
 
 const loadTokens = () => async (dispatch, getState) => {
 	const tokenService = getGlobalContext().tokenService;
@@ -14,10 +17,11 @@ const addToken = contractAddress => async dispatch => {
 	try {
 		const tokenInfo = await tokenService.getTokenInfo(contractAddress);
 		await tokenService.addToken(tokenInfo);
-	} catch (error) {
+	} catch (e) {
+		log.error(e);
 		await dispatch(
 			actions.setTokenError(
-				`Sorry, an error occurred while adding the current token: ${error}`
+				`Sorry, an error occurred while adding the provided token. Please try again later.`
 			)
 		);
 	}
