fd78b433537fd7b6d2b2a762ff3aa7455669f126
rodrigopavezi <rodrigopavezi@gmail.com>
2019-03-27 10:13:20 -0300

    fix(freeze-issue): implement matomo consent

--- src/renderer/app.jsx
+++ src/renderer/app.jsx
@@ -5,8 +5,7 @@ import { Route, HashRouter } from 'react-router-dom';
 import { ConnectedRouter, push } from 'connected-react-router';
 import ReactPiwik from 'react-piwik';
 import { SelfkeyDarkTheme } from 'selfkey-ui';
-import { appOperations, appSelectors } from 'common/app';
-import { isDevMode, isTestMode, isDebugMode } from 'common/utils/common';
+import { appOperations } from 'common/app';
 import config from 'common/config';
 
 import { GlobalError } from './global-error';
@@ -41,15 +40,14 @@ const piwik = new ReactPiwik({
 	siteId: config.matomoSite | 1,
 	trackErrors: true
 });
+ReactPiwik.push(['requireConsent']);
+ReactPiwik.push(['trackPageView']);
 
 class AppContainerComponent extends Component {
 	state = { hasError: false };
 	handleRefresh = async () => {
 		await this.props.dispatch(push('/'));
 		this.setState({ hasError: false });
-	};
-	includeTracking = () => {
-		return this.props.hasAcceptedTracking && !isDevMode() && !isTestMode() && !isDebugMode();
 	};
 	static getDerivedStateFromError() {
 		return { hasError: true };
@@ -67,13 +65,7 @@ class AppContainerComponent extends Component {
 			return <GlobalError onRefresh={this.handleRefresh} />;
 		}
 		return (
-			<ConnectedRouter
-				history={
-					this.includeTracking()
-						? piwik.connectToHistory(this.props.history.getHistory())
-						: this.props.history.getHistory()
-				}
-			>
+			<ConnectedRouter history={piwik.connectToHistory(this.props.history.getHistory())}>
 				<HashRouter>
 					<div
 						style={{
@@ -116,13 +108,7 @@ class AppContainerComponent extends Component {
 	}
 }
 
-const mapStateToProps = (state, props) => {
-	return {
-		hasAcceptedTracking: appSelectors.hasAcceptedTracking(state)
-	};
-};
-
-const AppContainer = connect(mapStateToProps)(AppContainerComponent);
+const AppContainer = connect()(AppContainerComponent);
 
 const App = ({ store, history }) => (
 	<SelfkeyDarkTheme>
