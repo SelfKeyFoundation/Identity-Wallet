90278ede91750f155df5a85212e55dc1fc52ef72
designhorf <designhorf@gmail.com>
2019-03-28 05:46:01 +0100

    fix: button style and deleted unused style snippets

--- src/renderer/marketplace/services/services-list-item.jsx
+++ src/renderer/marketplace/services/services-list-item.jsx
@@ -1,46 +1,13 @@
 import React from 'react';
-import { TableRow, TableCell, Typography, withStyles } from '@material-ui/core';
+import { Button, TableRow, TableCell, Typography, withStyles } from '@material-ui/core';
 import { Tag } from 'selfkey-ui';
 
 const styles = theme => ({
-	// root: {
-	// 	width: '360px',
-	// 	height: '326px',
-	// 	border: 'solid 1px #303c49',
-	// 	borderRadius: '4px',
-	// 	fontFamily: 'Lato, arial, sans-serif'
-	// },
-
-	// svgIcon: {
-	// 	fontSize: '50px',
-	// 	color: '#FFF'
-	// },
-
-	// title: {
-	// 	margin: '20px'
-	// },
-
 	icon: {
-		// marginLeft: '20px'
 		height: '30px',
 		width: '30px'
 	},
 
-	// header: {
-	// 	backgroundColor: '#2a3540'
-	// },
-
-	// body: {
-	// 	width: '320px',
-	// 	textAlign: 'left',
-	// 	margin: '20px',
-	// 	color: '#fff',
-	// 	fontFamily: 'Lato, arial, sans-serif',
-	// 	fontSize: '16px',
-	// 	fontWeight: 400,
-	// 	lineHeight: 1.5,
-	// 	height: '130px'
-	// },
 	noRightPadding: {
 		paddingRight: 0
 	},
@@ -51,11 +18,16 @@ const styles = theme => ({
 
 	footer: {
 		margin: '20px'
+	},
+
+	button: {
+		minWidth: 0,
+		textTransform: 'capitalize'
 	}
 });
 
 export const MarketplaceServicesListItem = withStyles(styles)(
-	({ classes, children, name, description, status, logoUrl, viewAction }) => (
+	({ classes, children, name, logoUrl, viewAction }) => (
 		<TableRow key={name}>
 			<TableCell className={classes.noRightPadding}>
 				<img src={logoUrl} className={classes.icon} />
@@ -81,15 +53,15 @@ export const MarketplaceServicesListItem = withStyles(styles)(
 				<Typography variant="h6">Excluded Residents</Typography>
 			</TableCell>
 			<TableCell>
-				<Typography
-					variant="h6"
-					color="primary"
+				<Button
 					disabled
+					variant="text"
+					color="secondary"
+					className={classes.button}
 					onClick={() => (viewAction ? viewAction(name) : '')}
-					className={classes.link}
 				>
 					Coming Soon
-				</Typography>
+				</Button>
 			</TableCell>
 		</TableRow>
 	)
