3188353e6535bc04962f108c1e0b3d5ff98a9962
Eduardo Santiago Moura <eduardosmoura@gmail.com>
2019-04-16 00:01:00 -0300

    feat(manage-applications): create page structure

--- src/renderer/selfkey-id/main/components/selfkey-id-applications.jsx
+++ src/renderer/selfkey-id/main/components/selfkey-id-applications.jsx
@@ -1,15 +1,309 @@
 import React from 'react';
-import { CardHeader, Card, CardContent, Table, TableBody } from '@material-ui/core';
+import {
+	CardHeader,
+	Card,
+	CardContent,
+	Typography,
+	ExpansionPanel,
+	ExpansionPanelSummary,
+	Grid,
+	Divider,
+	ExpansionPanelDetails,
+	List,
+	ListItem,
+	Button,
+	withStyles
+} from '@material-ui/core';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import { GreenTick, FailedIcon, AttributeAlertIcon, HourGlassIcon, warning } from 'selfkey-ui';
 
-const SelfkeyIdApplications = () => (
-	<Card>
-		<CardHeader title="Marketplace Applications" />
-		<CardContent>
-			<Table>
-				<TableBody />
-			</Table>
-		</CardContent>
-	</Card>
-);
+const styles = theme => ({
+	orange: {
+		width: 'auto',
+		margin: '30px 24px 24px 24px',
+		border: `1px solid ${warning}`,
+		background: 'transparent',
+		color: warning,
+		'&:hover': {
+			border: `1px solid ${warning}`
+		}
+	},
+	statusIcon: {
+		width: '36px !important',
+		height: '36px !important'
+	},
+	type: {
+		paddingRight: '10px'
+	},
+	label: {
+		paddingRight: '20px'
+	}
+});
 
-export default SelfkeyIdApplications;
+const HeaderIcon = withStyles(styles)(({ status }) => {
+	let icon = null;
+	switch (status) {
+		case 'Documents Required':
+			icon = <HourGlassIcon />;
+			break;
+		case 'Documents Submitted':
+			icon = <HourGlassIcon />;
+			break;
+		case 'Denied':
+			icon = <FailedIcon />;
+			break;
+		default:
+			icon = <GreenTick />;
+	}
+
+	return icon;
+});
+
+const StatusComponent = withStyles(styles)(({ status, classes }) => {
+	let icon = null;
+	let message = null;
+	switch (status) {
+		case 'Documents Required':
+			icon = <AttributeAlertIcon className={{ root: classes.statusIcon }} />;
+			message = 'Application started. Missing required documents.';
+			break;
+		case 'Documents Submitted':
+			icon = <AttributeAlertIcon className={{ root: classes.statusIcon }} />;
+			message =
+				'Application started. Documents submitted. Please check your email for further instructions.';
+			break;
+		case 'Denied':
+			icon = <AttributeAlertIcon className={{ root: classes.statusIcon }} />;
+			message = 'Application denied. Please check your email for the reject reason.';
+			break;
+		default:
+			icon = <AttributeAlertIcon className={{ root: classes.statusIcon }} />;
+			message =
+				'Application completed. Please check your email to receive relevant documents and information.';
+	}
+
+	return (
+		<Grid
+			container
+			direction="row"
+			justify="flex-start"
+			alignItems="center"
+			className={classes.orange}
+		>
+			<Grid item>{icon}</Grid>
+			<Grid item>
+				<Typography variant="body1" gutterBottom>
+					Status
+				</Typography>
+				<Typography variant="body2" color="secondary" gutterBottom>
+					{message}
+				</Typography>
+			</Grid>
+			<Grid item justify="flex-end">
+				<Button variant="contained" size="large">
+					Add Documents
+				</Button>
+			</Grid>
+		</Grid>
+	);
+});
+
+export const SelfkeyIdApplications = props => {
+	const { classes } = props;
+	return (
+		<React.Fragment>
+			{props.applications.map((item, index) => (
+				<React.Fragment key={item.contry}>
+					<ExpansionPanel defaultExpanded={index === 0}>
+						<ExpansionPanelSummary expandIcon={<ExpandLessIcon />}>
+							<Grid
+								container
+								direction="row"
+								justify="flex-start"
+								alignItems="baseline"
+							>
+								<Typography variant="h2" className={classes.type}>
+									{item.type}
+								</Typography>
+								<Typography variant="subtitle2" color="secondary" gutterBottom>
+									- {item.country}
+								</Typography>
+							</Grid>
+							<Grid container direction="row" justify="flex-end" alignItems="center">
+								<HeaderIcon status={item.status} />
+								<Typography variant="subtitle2" color="secondary" gutterBottom>
+									{item.status}
+								</Typography>
+							</Grid>
+						</ExpansionPanelSummary>
+						<Divider />
+						<StatusComponent status={item.status} classes={classes} />
+						<ExpansionPanelDetails>
+							<Grid container spacing={32}>
+								<Grid item xs>
+									<Card>
+										<CardHeader title="Application Details" />
+										<Divider />
+										<CardContent>
+											<List>
+												<ListItem key="applicationDate">
+													<Typography
+														variant="body2"
+														color="secondary"
+														gutterBottom
+														className={classes.label}
+													>
+														Application Date
+													</Typography>
+													<Typography
+														variant="body2"
+														align="right"
+														gutterBottom
+													>
+														{item.applicationDate}
+													</Typography>
+												</ListItem>
+												<ListItem key="serviceProvider">
+													<Typography
+														variant="body2"
+														color="secondary"
+														gutterBottom
+														className={classes.label}
+													>
+														Service Provider
+													</Typography>
+													<Typography
+														variant="body2"
+														align="right"
+														gutterBottom
+													>
+														{item.serviceProvider}
+													</Typography>
+												</ListItem>
+												<ListItem key="providerContact">
+													<Typography
+														variant="body2"
+														color="secondary"
+														gutterBottom
+														className={classes.label}
+													>
+														Provider Contact
+													</Typography>
+													<Typography
+														variant="body2"
+														align="right"
+														gutterBottom
+													>
+														{item.providerContact}
+													</Typography>
+												</ListItem>
+												<ListItem key="address">
+													<Typography
+														variant="body2"
+														color="secondary"
+														gutterBottom
+														className={classes.label}
+													>
+														Address
+													</Typography>
+													<Typography
+														variant="body2"
+														align="right"
+														gutterBottom
+													>
+														{item.address}
+													</Typography>
+												</ListItem>
+											</List>
+										</CardContent>
+									</Card>
+								</Grid>
+								<Grid item xs>
+									<Card>
+										<CardHeader title="Payment Details" />
+										<Divider />
+										<CardContent>
+											<List>
+												<ListItem key="transactionId">
+													<Typography
+														variant="body2"
+														color="secondary"
+														gutterBottom
+														className={classes.label}
+													>
+														Transaction ID
+													</Typography>
+													<Typography
+														variant="body2"
+														align="right"
+														gutterBottom
+													>
+														{item.transactionId}
+													</Typography>
+												</ListItem>
+												<ListItem key="transactionDate">
+													<Typography
+														variant="body2"
+														color="secondary"
+														gutterBottom
+														className={classes.label}
+													>
+														Transaction Date
+													</Typography>
+													<Typography
+														variant="body2"
+														align="right"
+														gutterBottom
+													>
+														{item.transactionDate}
+													</Typography>
+												</ListItem>
+												<ListItem key="amount">
+													<Typography
+														variant="body2"
+														color="secondary"
+														gutterBottom
+														className={classes.label}
+													>
+														Amount
+													</Typography>
+													<Typography
+														variant="body2"
+														align="right"
+														gutterBottom
+													>
+														{item.amount}
+													</Typography>
+												</ListItem>
+												<ListItem key="paymentStatus">
+													<Typography
+														variant="body2"
+														color="secondary"
+														gutterBottom
+														className={classes.label}
+													>
+														Payment Status
+													</Typography>
+													<Typography
+														variant="body2"
+														align="right"
+														gutterBottom
+													>
+														{item.paymentStatus}
+													</Typography>
+												</ListItem>
+											</List>
+										</CardContent>
+									</Card>
+								</Grid>
+							</Grid>
+						</ExpansionPanelDetails>
+					</ExpansionPanel>
+					<br />
+				</React.Fragment>
+			))}
+		</React.Fragment>
+	);
+};
+
+export default withStyles(styles)(SelfkeyIdApplications);
--- src/renderer/selfkey-id/main/components/selfkey-id.jsx
+++ src/renderer/selfkey-id/main/components/selfkey-id.jsx
@@ -1,13 +1,67 @@
 import React, { Component } from 'react';
 import { Grid, Typography, Tabs, Tab } from '@material-ui/core';
-import SelfkeyIdOverview from './selfkey-id-overview';
 import { connect } from 'react-redux';
 import { walletSelectors } from 'common/wallet';
 import { push } from 'connected-react-router';
-
-// import SelfkeyIdApplications from './selfkey-id-applications';
+import SelfkeyIdOverview from './selfkey-id-overview';
+import SelfkeyIdApplications from './selfkey-id-applications';
 // import SelfkeyIdCompanies from './selfkey-id-companies';
 // import SelfkeyIdHistory from './selfkey-id-history';
+
+const dummyApplications = [
+	{
+		type: 'Incorporation',
+		country: 'Singapore',
+		status: 'Documents Required',
+		applicationDate: '08 May 2018',
+		serviceProvider: 'Far Horizon Capintal Inc',
+		providerContact: 'support@flagtheory.com',
+		address: '10 Anson Rd Int Plaza #27-15 Singapore 079903',
+		transactionId: 'asdasds21312',
+		transactionDate: '08 May 2018 12:48AM',
+		amount: '746,234,43.00 KEY',
+		paymentStatus: 'Sent KEY'
+	},
+	{
+		type: 'Incorporation',
+		country: 'France',
+		status: 'Documents Submitted',
+		applicationDate: '07 May 2018',
+		serviceProvider: 'Far Horizon Capintal Inc',
+		providerContact: 'support@flagtheory.com',
+		address: '10 Anson Rd Int Plaza #27-15 France 079903',
+		transactionId: 'asdasds21312',
+		transactionDate: '07 May 2018 12:48AM',
+		amount: '746,234,43.00 ETH',
+		paymentStatus: 'Sent ETH'
+	},
+	{
+		type: 'Incorporation',
+		country: 'Malta',
+		status: 'Approved',
+		applicationDate: '09 May 2018',
+		serviceProvider: 'Far Horizon Capintal Inc',
+		providerContact: 'support@flagtheory.com',
+		address: '10 Anson Rd Int Plaza #27-15 Malta 079903',
+		transactionId: 'asdasds2131313',
+		transactionDate: '09 May 2018 12:48AM',
+		amount: '746,234,49.00 KEY',
+		paymentStatus: 'Sent KEY'
+	},
+	{
+		type: 'Incorporation',
+		country: 'Brazil',
+		status: 'Denied',
+		applicationDate: '10 May 2018',
+		serviceProvider: 'Far Horizon Capintal Inc',
+		providerContact: 'support@flagtheory.com',
+		address: '10 Anson Rd Int Plaza #27-15 Brazil 079903',
+		transactionId: 'asdasds2131111',
+		transactionDate: '10 May 2018 12:48AM',
+		amount: '746,234,10.00 KEY',
+		paymentStatus: 'Sent ETH'
+	}
+];
 
 class SelfkeyIdComponent extends Component {
 	state = {
@@ -29,9 +83,9 @@ class SelfkeyIdComponent extends Component {
 	render() {
 		let component = <SelfkeyIdOverview {...this.props} />;
 
-		// if (this.state.tabValue === 1) {
-		// 	component = <SelfkeyIdApplications {...this.props} />;
-		// }
+		if (this.state.tabValue === 1) {
+			component = <SelfkeyIdApplications {...this.props} />;
+		}
 		// } else if (this.state.tabValue === 2) {
 		// 	component = <SelfkeyIdCompanies {...this.props} />;
 		// } else if (this.state.tabValue === 3) {
@@ -46,7 +100,7 @@ class SelfkeyIdComponent extends Component {
 				<Grid item>
 					<Tabs value={this.state.tabValue} onChange={this.handleChange}>
 						<Tab label="Overview" />
-						{/* <Tab label="Marketplace Applications" /> */}
+						<Tab label="Marketplace Applications" />
 						{/* <Tab label="Companies" /> */}
 						{/* <Tab label="History" /> */}
 					</Tabs>
@@ -59,7 +113,8 @@ class SelfkeyIdComponent extends Component {
 
 const mapStateToProps = (state, props) => {
 	return {
-		wallet: walletSelectors.getWallet(state)
+		wallet: walletSelectors.getWallet(state),
+		applications: dummyApplications
 	};
 };
 
