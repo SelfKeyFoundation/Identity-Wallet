c36260be7291a520bb6353067a2487b1ff622d66
Eduardo Santiago Moura <eduardosmoura@gmail.com>
2019-04-18 23:39:01 -0300

    feat(manage-applications): create refresh feature

--- src/renderer/selfkey-id/main/components/selfkey-id-applications.jsx
+++ src/renderer/selfkey-id/main/components/selfkey-id-applications.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
 	Card,
 	CardContent,
@@ -15,6 +15,7 @@ import {
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import { CheckMaIcon, DeniedIcon, HourGlassIcon, StatusInfo } from 'selfkey-ui';
 import config from 'common/config';
+import moment from 'moment';
 
 const styles = theme => ({
 	statusIcon: {
@@ -72,200 +73,237 @@ const HeaderIcon = withStyles(styles)(({ status, classes }) => {
 	return icon;
 });
 
-export const SelfkeyIdApplications = props => {
-	const { classes } = props;
-	return (
-		<React.Fragment>
-			{props.applications.map((item, index) => (
-				<React.Fragment key={item.contry}>
-					<ExpansionPanel defaultExpanded={index === 0}>
-						<ExpansionPanelSummary expandIcon={<ExpandLessIcon />}>
-							<Grid
-								container
-								direction="row"
-								justify="flex-start"
-								alignItems="baseline"
-							>
-								<Typography variant="h2" className={classes.type}>
-									{item.rpName}
-								</Typography>
-								<Typography variant="subtitle2" color="secondary">
-									- {item.scope}
-								</Typography>
-							</Grid>
-							<Grid
-								container
-								direction="row"
-								justify="flex-end"
-								alignItems="center"
-								className={classes.noRightPadding}
-							>
-								<HeaderIcon status={item.currentStatusName} />
-								<Typography variant="subtitle2" color="secondary">
-									{item.currentStatusName}
-								</Typography>
-							</Grid>
-						</ExpansionPanelSummary>
-						<Divider />
-						<Grid
-							className={classes.statusInfoWrap}
-							container
-							direction="row"
-							alignItems="center"
-						>
-							<StatusInfo status={item.currentStatusName} />
-						</Grid>
-						<ExpansionPanelDetails>
-							<Grid container spacing={32}>
-								<Grid item xs>
-									<Card>
-										<Typography variant="h2" className={classes.title}>
-											Application Details
-										</Typography>
-										<Divider variant="middle" />
-										<CardContent>
-											<List className={classes.list}>
-												<ListItem
-													key="applicationDate"
-													className={classes.listItem}
-												>
-													<Typography
-														variant="body2"
-														color="secondary"
-														className={classes.label}
-													>
-														Application Date
-													</Typography>
-													<Typography variant="body2">
-														{item.applicationDate}
-													</Typography>
-												</ListItem>
-												<ListItem
-													key="serviceProvider"
-													className={classes.listItem}
-												>
-													<Typography
-														variant="body2"
-														color="secondary"
-														className={classes.label}
-													>
-														Service Provider
-													</Typography>
-													<Typography variant="body2">
-														{config.applicationsProviderName}
-													</Typography>
-												</ListItem>
-												<ListItem
-													key="providerContact"
-													className={classes.listItem}
-												>
-													<Typography
-														variant="body2"
-														color="secondary"
-														className={classes.label}
-													>
-														Provider Contact
-													</Typography>
-													<Typography variant="body2">
-														{config.appplicationProviderContact}
-													</Typography>
-												</ListItem>
-												<ListItem
-													key="address"
-													className={classes.listItem}
-												>
-													<Typography
-														variant="body2"
-														color="secondary"
-														className={classes.label}
-													>
-														Address
-													</Typography>
-													<Typography variant="body2">
-														{config.applicationsProviderAddress}
-													</Typography>
-												</ListItem>
-											</List>
-										</CardContent>
-									</Card>
-								</Grid>
-								<Grid item xs>
-									<Card>
-										<Typography variant="h2" className={classes.title}>
-											Payment Details
-										</Typography>
-										<Divider variant="middle" />
-										<CardContent>
-											<List className={classes.list}>
-												<ListItem
-													key="transactionId"
-													className={classes.listItem}
-												>
-													<Typography
-														variant="body2"
-														color="secondary"
-														className={classes.label}
-													>
-														Transaction ID
-													</Typography>
-													<Typography variant="body2">
-														{item.payments.transactionHash}
-													</Typography>
-												</ListItem>
-												<ListItem
-													key="transactionDate"
-													className={classes.listItem}
-												>
-													<Typography
-														variant="body2"
-														color="secondary"
-														className={classes.label}
-													>
-														Transaction Date
-													</Typography>
-													<Typography variant="body2">
-														{item.payments.transactionDate}
-													</Typography>
-												</ListItem>
-												<ListItem key="amount" className={classes.listItem}>
-													<Typography
-														variant="body2"
-														color="secondary"
-														className={classes.label}
-													>
-														Amount
-													</Typography>
-													<Typography variant="body2">
-														{item.payments.amountKey}
-													</Typography>
-												</ListItem>
-												<ListItem
-													key="paymentStatus"
-													className={classes.listItem}
-												>
-													<Typography
-														variant="body2"
-														color="secondary"
-														className={classes.label}
-													>
-														Payment Status
-													</Typography>
-													<Typography variant="body2">
-														Sent KEY
-													</Typography>
-												</ListItem>
-											</List>
-										</CardContent>
-									</Card>
-								</Grid>
-							</Grid>
-						</ExpansionPanelDetails>
-					</ExpansionPanel>
-					<br />
-				</React.Fragment>
-			))}
-		</React.Fragment>
-	);
+const getStatusName = status => {
+	let name = '';
+	switch (status) {
+		case 2:
+			name = 'Approved';
+			break;
+		case 3:
+			name = 'Denied';
+			break;
+		case 8:
+			name = 'Denied';
+			break;
+		case 4:
+			name = 'Documents Submitted';
+			break;
+		default:
+			name = 'Documents Required';
+	}
+	return name;
 };
 
-export default withStyles(styles)(SelfkeyIdApplications);
+class SelfkeyIdApplicationsComponent extends Component {
+	render() {
+		const { classes } = this.props;
+		return (
+			<React.Fragment>
+				{this.props.applications.map((item, index) => (
+					<React.Fragment key={item.country}>
+						<ExpansionPanel defaultExpanded={index === 0}>
+							<ExpansionPanelSummary expandIcon={<ExpandLessIcon />}>
+								<Grid
+									container
+									direction="row"
+									justify="flex-start"
+									alignItems="baseline"
+								>
+									<Typography variant="h2" className={classes.type}>
+										{item.rpName.charAt(0).toUpperCase() + item.rpName.slice(1)}
+									</Typography>
+									<Typography variant="subtitle2" color="secondary">
+										- {item.country}
+									</Typography>
+								</Grid>
+								<Grid
+									container
+									direction="row"
+									justify="flex-end"
+									alignItems="center"
+									className={classes.noRightPadding}
+								>
+									<HeaderIcon status={getStatusName(item.currentStatus)} />
+									<Typography variant="subtitle2" color="secondary">
+										{getStatusName(item.currentStatus)}
+									</Typography>
+								</Grid>
+							</ExpansionPanelSummary>
+							<Divider />
+							<Grid
+								className={classes.statusInfoWrap}
+								container
+								direction="row"
+								alignItems="center"
+							>
+								<StatusInfo
+									status={getStatusName(item.currentStatus)}
+									onClick={() => this.props.handleAddDocuments(item.id)}
+									handleRefresh={() => this.props.handleRefresh(item.id)}
+									tooltip={moment(item.updatedAt).format('DD MMM YYYY')}
+								/>
+							</Grid>
+							<ExpansionPanelDetails>
+								<Grid container spacing={32}>
+									<Grid item xs>
+										<Card>
+											<Typography variant="h2" className={classes.title}>
+												Application Details
+											</Typography>
+											<Divider variant="middle" />
+											<CardContent>
+												<List className={classes.list}>
+													<ListItem
+														key="applicationDate"
+														className={classes.listItem}
+													>
+														<Typography
+															variant="body2"
+															color="secondary"
+															className={classes.label}
+														>
+															Application Date
+														</Typography>
+														<Typography variant="body2">
+															{moment(item.applicationDate).format(
+																'DD MMM YYYY'
+															)}
+														</Typography>
+													</ListItem>
+													<ListItem
+														key="serviceProvider"
+														className={classes.listItem}
+													>
+														<Typography
+															variant="body2"
+															color="secondary"
+															className={classes.label}
+														>
+															Service Provider
+														</Typography>
+														<Typography variant="body2">
+															{config.applicationsProviderName}
+														</Typography>
+													</ListItem>
+													<ListItem
+														key="providerContact"
+														className={classes.listItem}
+													>
+														<Typography
+															variant="body2"
+															color="secondary"
+															className={classes.label}
+														>
+															Provider Contact
+														</Typography>
+														<Typography variant="body2">
+															{config.appplicationProviderContact}
+														</Typography>
+													</ListItem>
+													<ListItem
+														key="address"
+														className={classes.listItem}
+													>
+														<Typography
+															variant="body2"
+															color="secondary"
+															className={classes.label}
+														>
+															Address
+														</Typography>
+														<Typography variant="body2">
+															{config.applicationsProviderAddress}
+														</Typography>
+													</ListItem>
+												</List>
+											</CardContent>
+										</Card>
+									</Grid>
+									<Grid item xs>
+										<Card>
+											<Typography variant="h2" className={classes.title}>
+												Payment Details
+											</Typography>
+											<Divider variant="middle" />
+											<CardContent>
+												<List className={classes.list}>
+													<ListItem
+														key="transactionId"
+														className={classes.listItem}
+													>
+														<Typography
+															variant="body2"
+															color="secondary"
+															className={classes.label}
+														>
+															Transaction ID
+														</Typography>
+														<Typography variant="body2">
+															{item.payments.transactionHash}
+														</Typography>
+													</ListItem>
+													<ListItem
+														key="transactionDate"
+														className={classes.listItem}
+													>
+														<Typography
+															variant="body2"
+															color="secondary"
+															className={classes.label}
+														>
+															Transaction Date
+														</Typography>
+														<Typography variant="body2">
+															{moment(
+																item.payments.transactionDate
+															).format('DD MMM YYYY')}
+														</Typography>
+													</ListItem>
+													<ListItem
+														key="amount"
+														className={classes.listItem}
+													>
+														<Typography
+															variant="body2"
+															color="secondary"
+															className={classes.label}
+														>
+															Amount
+														</Typography>
+														<Typography variant="body2">
+															{item.payments.amountKey}
+														</Typography>
+													</ListItem>
+													<ListItem
+														key="paymentStatus"
+														className={classes.listItem}
+													>
+														<Typography
+															variant="body2"
+															color="secondary"
+															className={classes.label}
+														>
+															Payment Status
+														</Typography>
+														<Typography variant="body2">
+															Sent KEY
+														</Typography>
+													</ListItem>
+												</List>
+											</CardContent>
+										</Card>
+									</Grid>
+								</Grid>
+							</ExpansionPanelDetails>
+						</ExpansionPanel>
+						<br />
+					</React.Fragment>
+				))}
+			</React.Fragment>
+		);
+	}
+}
+
+export const SelfkeyIdApplications = withStyles(styles)(SelfkeyIdApplicationsComponent);
+
+export default SelfkeyIdApplications;
--- src/renderer/selfkey-id/main/components/selfkey-id.jsx
+++ src/renderer/selfkey-id/main/components/selfkey-id.jsx
@@ -1,86 +1,132 @@
 import React, { Component } from 'react';
-import { Grid, Typography, Tabs, Tab } from '@material-ui/core';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { Grid, Typography, Tabs, Tab, Button, withStyles } from '@material-ui/core';
 import { connect } from 'react-redux';
 import { walletSelectors } from 'common/wallet';
 import { push } from 'connected-react-router';
+import qs from 'query-string';
+import { incorporationsOperations, incorporationsSelectors } from 'common/incorporations';
+import { kycSelectors, kycOperations } from 'common/kyc';
 import SelfkeyIdOverview from './selfkey-id-overview';
 import SelfkeyIdApplications from './selfkey-id-applications';
+import { Popup } from '../../../common/popup';
 // import SelfkeyIdCompanies from './selfkey-id-companies';
 // import SelfkeyIdHistory from './selfkey-id-history';
 
 const dummyApplications = [
 	{
-		id: '1',
+		id: '5c8a88eacb05f40134eb14a5',
 		owner: 'owner',
-		scope: 'Singapore',
+		country: 'Singapore',
 		rpName: 'incorporations',
 		currentStatus: 1,
 		currentStatusName: 'Documents Required',
-		applicationDate: '08 May 2018',
+		applicationDate: '2019-03-01T17:02:01.123Z',
 		payments: {
 			amount: '123',
 			amountKey: '746,234,43.00 KEY',
 			transactionHash: 'asdasds21312',
-			transactionDate: '08 May 2018 12:48AM'
-		}
+			transactionDate: '2019-03-01T17:02:01.123Z'
+		},
+		updatedAt: '2019-03-01T17:02:01.123Z'
 	},
 	{
 		id: '2',
 		owner: 'owner',
-		scope: 'France',
+		country: 'France',
 		rpName: 'incorporations',
-		currentStatus: 2,
+		currentStatus: 4,
 		currentStatusName: 'Documents Submitted',
-		applicationDate: '07 May 2018',
+		applicationDate: '2019-03-02T17:02:01.123Z',
 		payments: {
 			amount: '123',
 			amountKey: '746,234,40.00 KEY',
 			transactionHash: 'asdasds21311',
-			transactionDate: '07 May 2018 12:48AM'
-		}
+			transactionDate: '2019-03-02T17:02:01.123Z'
+		},
+		updatedAt: '2019-03-02T17:02:01.123Z'
 	},
 	{
 		id: '3',
 		owner: 'owner',
-		scope: 'Malta',
+		country: 'Malta',
 		rpName: 'incorporations',
-		currentStatus: 3,
+		currentStatus: 2,
 		currentStatusName: 'Approved',
-		applicationDate: '06 May 2018',
+		applicationDate: '2019-03-03T17:02:01.123Z',
 		payments: {
 			amount: '123',
 			amountKey: '746,234,41.00 KEY',
 			transactionHash: 'asdasds21313',
-			transactionDate: '06 May 2018 12:48AM'
-		}
+			transactionDate: '2019-03-03T17:02:01.123Z'
+		},
+		updatedAt: '2019-03-03T17:02:01.123Z'
 	},
 	{
 		id: '4',
 		owner: 'owner',
-		scope: 'Brazil',
+		country: 'Brazil',
 		rpName: 'incorporations',
-		currentStatus: 4,
+		currentStatus: 3,
 		currentStatusName: 'Denied',
-		applicationDate: '05 May 2018',
+		applicationDate: '2019-04-01T17:02:01.123Z',
 		payments: {
 			amount: '123',
 			amountKey: '746,234,44.00 KEY',
 			transactionHash: 'asdasds21314',
-			transactionDate: '05 May 2018 12:48AM'
-		}
+			transactionDate: '2019-04-01T17:02:01.123Z'
+		},
+		updatedAt: '2019-04-01T17:02:01.123Z'
 	}
 ];
 
+const styles = theme => ({
+	loading: {
+		marginTop: '5em'
+	}
+});
+
 class SelfkeyIdComponent extends Component {
 	state = {
-		tabValue: 0
+		tabValue: 0,
+		loading: false,
+		applicationId: null,
+		showApplicationRefreshModal: false,
+		messageApplicationRefreshModal: null
 	};
 
 	async componentDidMount() {
-		const { wallet, dispatch } = this.props;
+		const { wallet, dispatch, rpShouldUpdate } = this.props;
+		const tabValue = qs.parse(this.props.location.search, { ignoreQueryPrefix: true }).tabValue;
+		const notAuthenticated = false;
 
 		if (!wallet.isSetupFinished) {
 			await dispatch(push('/selfkeyIdCreate'));
+		}
+
+		if (tabValue) {
+			this.setState({ tabValue: parseInt(tabValue) });
+		}
+
+		if (!this.props.incorporations || !this.props.incorporations.length) {
+			this.props.dispatch(incorporationsOperations.loadIncorporationsOperation());
+		}
+
+		if (rpShouldUpdate) {
+			await this.props.dispatch(
+				kycOperations.loadRelyingParty('incorporations', notAuthenticated)
+			);
+		}
+	}
+
+	componentDidUpdate(prevProps) {
+		if (prevProps.rp !== this.props.rp) {
+			this.setState({ loading: false }, () => {
+				if (this.state.applicationId) {
+					// try to refresh again after loading relying party
+					this.handleApplicationRefresh(this.state.applicationId);
+				}
+			});
 		}
 	}
 
@@ -88,11 +134,134 @@ class SelfkeyIdComponent extends Component {
 		this.setState({ tabValue });
 	};
 
+	handleApplicationAddDocuments = id => {
+		// TODO: add documents
+		console.log(id);
+	};
+
+	handleApplicationRefresh = id => {
+		const { rp } = this.props;
+		const afterAuthRoute = `/main/selfkeyId?tabValue=1`;
+		const cancelRoute = `/main/selfkeyId?tabValue=1`;
+		const authenticated = true;
+
+		// if relying party not loaded, try again
+		if (!rp || !rp.authenticated) {
+			this.setState({ loading: true, applicationId: id }, async () => {
+				await this.props.dispatch(
+					kycOperations.loadRelyingParty(
+						'incorporations',
+						authenticated,
+						afterAuthRoute,
+						cancelRoute
+					)
+				);
+			});
+		} else {
+			// get stored application from local database
+			let application = this.props.applications.find(app => {
+				return app.id === id;
+			});
+			// get current application info from kyc
+			let kycApplication = this.props.rp.applications.find(app => {
+				return app.id === id;
+			});
+
+			let message;
+
+			if (application && kycApplication) {
+				// update stored application
+				application.currentStatus = kycApplication.currentStatus;
+				application.currentStatusName = kycApplication.statusName;
+				application.updatedAt = kycApplication.updatedAt;
+
+				// TODO: update local database
+
+				message = 'Application status updated successfully.';
+			} else {
+				message = 'Could not update your application. Please try again later.';
+			}
+
+			// all done, show modal
+			this.setState({
+				showApplicationRefreshModal: true,
+				messageApplicationRefreshModal: message
+			});
+
+			if (this.state.applicationId) {
+				// reset id
+				this.setState({ applicationId: null });
+			}
+		}
+	};
+
+	renderLoadingScreen = () => (
+		<Grid container justify="center" alignItems="center">
+			<CircularProgress size={50} className={this.props.classes.loading} />
+		</Grid>
+	);
+
+	handleCloseApplicationRefreshModal = evt => {
+		evt && evt.preventDefault();
+		this.setState({ showApplicationRefreshModal: false });
+	};
+
+	renderApplicationRefreshModal() {
+		const { classes } = this.props;
+		return (
+			<Popup
+				open={true}
+				text={'Update Application'}
+				closeAction={this.handleCloseApplicationRefreshModal}
+			>
+				<Grid
+					container
+					className={classes.root}
+					spacing={32}
+					direction="column"
+					justify="flex-start"
+					alignItems="stretch"
+				>
+					<Grid item>
+						<Typography variant="overline">
+							{this.state.messageApplicationRefreshModal}
+						</Typography>
+					</Grid>
+					<Grid item>
+						<Grid container spacing={24}>
+							<Grid item>
+								<Button
+									variant="outlined"
+									size="large"
+									onClick={this.handleCloseApplicationRefreshModal}
+								>
+									Close
+								</Button>
+							</Grid>
+						</Grid>
+					</Grid>
+				</Grid>
+			</Popup>
+		);
+	}
+
 	render() {
+		const { isLoading } = this.props;
+		const { showApplicationRefreshModal } = this.state;
+
 		let component = <SelfkeyIdOverview {...this.props} />;
 
 		if (this.state.tabValue === 1) {
-			component = <SelfkeyIdApplications {...this.props} />;
+			if (isLoading || this.state.loading) {
+				return this.renderLoadingScreen();
+			}
+			component = (
+				<SelfkeyIdApplications
+					{...this.props}
+					handleAddDocuments={this.handleApplicationAddDocuments}
+					handleRefresh={this.handleApplicationRefresh}
+				/>
+			);
 		}
 		// } else if (this.state.tabValue === 2) {
 		// 	component = <SelfkeyIdCompanies {...this.props} />;
@@ -114,18 +283,29 @@ class SelfkeyIdComponent extends Component {
 					</Tabs>
 				</Grid>
 				<Grid item>{component}</Grid>
+				{this.state.tabValue === 1 &&
+					showApplicationRefreshModal &&
+					this.renderApplicationRefreshModal()}
 			</Grid>
 		);
 	}
 }
 
 const mapStateToProps = (state, props) => {
+	const notAuthenticated = false;
 	return {
 		wallet: walletSelectors.getWallet(state),
+		isLoading: incorporationsSelectors.getLoading(state),
+		rp: kycSelectors.relyingPartySelector(state, 'incorporations'),
+		rpShouldUpdate: kycSelectors.relyingPartyShouldUpdateSelector(
+			state,
+			'incorporations',
+			notAuthenticated
+		),
 		applications: dummyApplications
 	};
 };
 
-export const SelfkeyId = connect(mapStateToProps)(SelfkeyIdComponent);
+export const SelfkeyId = connect(mapStateToProps)(withStyles(styles)(SelfkeyIdComponent));
 
 export default SelfkeyId;
