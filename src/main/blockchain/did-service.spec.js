1c9a2654809f1d38d1dd2ef9924f53ace437e290
rodrigopavezi <rodrigopavezi@gmail.com>
2019-05-08 14:00:32 -0300

    feat(create-did): add test

--- src/main/blockchain/did-service.spec.js
+++ src/main/blockchain/did-service.spec.js
@@ -1,28 +1,55 @@
-/* 
 import DIDService from './did-service';
 import { setGlobalContext } from 'common/context';
 
 describe('DIDService', () => {
 	let service = null;
+	const ledger = {
+		methods: {
+			createDID: () => {
+				return {
+					send: () => {
+						return { events: { CreatedDID: { returnValues: { id: 'id' } } } };
+					}
+				};
+			},
+			getController: () => {
+				return {
+					call: () => 'controller'
+				};
+			}
+		}
+	};
+	const web3Service = {
+		web3: {
+			eth: {
+				Contract: () => ledger
+			},
+			utils: {
+				hexToBytes: () => {}
+			}
+		}
+	};
+	setGlobalContext({ web3Service });
+
 	beforeEach(() => {
 		service = new DIDService();
-		const ledger = {
-			methods: {
-				createDID: () => {}
-			}
-		};
-
-		const web3Service = {
-			web3: {
-				eth: {
-					Contract: () => ledger
-				}
-			}
-		};
-		setGlobalContext({ web3Service });
 	});
 
-	xit('createDID', async () => {});
-	xit('getControllerAddress', async () => {});
+	it('createDID', async () => {
+		const contractSpy = jest.spyOn(web3Service.web3.eth, 'Contract');
+		const createDIDSpy = jest.spyOn(ledger.methods, 'createDID');
+		const transaction = await service.createDID();
+		expect(contractSpy).toHaveBeenCalled();
+		expect(createDIDSpy).toHaveBeenCalled();
+		expect(transaction.events.CreatedDID.returnValues.id).toEqual('id');
+	});
+
+	it('getControllerAddress', async () => {
+		const contractSpy = jest.spyOn(web3Service.web3.eth, 'Contract');
+		const getControllerSpy = jest.spyOn(ledger.methods, 'getController');
+		const controllerAddress = await service.getControllerAddress('did');
+		expect(contractSpy).toHaveBeenCalled();
+		expect(getControllerSpy).toHaveBeenCalled();
+		expect(controllerAddress).toEqual('controller');
+	});
 });
-*/
