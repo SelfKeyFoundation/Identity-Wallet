9d17ad8935d225967a05cefcaaa53de664d77b77
Maxim Kovalov <max.kovalov@gmail.com>
2019-05-17 22:00:42 +0300

    feat(storybook): init storybook

---
+++ stories/common.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { storiesOf } from '@storybook/react';
+import { Popup } from '../src/renderer/common/popup';
+
+storiesOf('Common', module).add('Popup', () => (
+	<Popup closeAction={action('popup close action click')} open text="Test Popup">
+		Storybook popup content
+	</Popup>
+));
