9a953d20f8ca3037c253c22bd2f8b4171582fd48
Maxim Kovalov <max.kovalov@gmail.com>
2019-05-21 12:45:35 +0300

    feat(banking-offers): offers page ui components

---
+++ src/renderer/marketplace/banking/account-type-tabs.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { withStyles, Tabs, Tab, Typography } from '@material-ui/core';
+const styles = theme => ({
+	tabContent: {
+		marginTop: '15px',
+		marginBottom: '15px'
+	}
+});
+
+export const BankingAccountTypeTabs = withStyles(styles)(
+	({ classes, accountType, onAccountTypeChange }) => {
+		return (
+			<React.Fragment>
+				<Tabs value={accountType} onChange={(evt, value) => onAccountTypeChange(value)}>
+					<Tab value="personal" label="Personal Accounts" />
+					<Tab value="corporate" label="Corporate Accounts" />
+					<Tab value="private" label="Private Banking" />
+				</Tabs>
+				{accountType === 'personal' && (
+					<Typography variant="body2" color="secondary" className={classes.tabContent}>
+						Personal account refers to the account owned by an individual or a couple if
+						it's a joint-account. That type of account is intended to reflect the
+						person's banking needs and obligations and is not transferrable.
+					</Typography>
+				)}
+				{accountType === 'corporate' && (
+					<Typography variant="body2" color="secondary" className={classes.tabContent}>
+						Corporate Accounts
+					</Typography>
+				)}
+				{accountType === 'private' && (
+					<Typography variant="body2" color="secondary" className={classes.tabContent}>
+						Private Banking
+					</Typography>
+				)}
+			</React.Fragment>
+		);
+	}
+);
+
+export default BankingAccountTypeTabs;
