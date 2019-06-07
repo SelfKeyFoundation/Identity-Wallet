240d08b68965f9c23140ce60c57eed04f47bc4c8
rodrigopavezi <rodrigopavezi@gmail.com>
2019-06-07 12:58:35 -0300

    fix: merge

--- src/common/config.js
+++ src/common/config.js
@@ -120,7 +120,8 @@ const dev = {
 		primaryToken: PRIMARY_TOKEN || 'KI'
 	},
 	matomoSite: 2,
-	ledgerAddress: '0x27332286A2CEaE458b82A1235f7E2a3Aa8945cAB'
+	ledgerAddress: '0x27332286A2CEaE458b82A1235f7E2a3Aa8945cAB',
+	paymentSplitterAddress: '0xb91FF8627f30494d27b91Aac1cB3c7465BE58fF5'
 };
 
 const prod = {
