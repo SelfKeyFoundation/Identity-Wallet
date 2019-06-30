9f53b96407b11993e076f23e6c7fad3134193d28
Maycon Mello <maycon.mellos@gmail.com>
2019-06-30 13:11:31 -0300

    refactor: removing debugger

--- src/renderer/selfkey-id/main/components/selfkey-id.jsx
+++ src/renderer/selfkey-id/main/components/selfkey-id.jsx
@@ -285,9 +285,6 @@ class SelfkeyIdComponent extends Component {
 		let loading = isLoading || processing || this.state.loading;
 		let component = <SelfkeyIdOverview {...this.props} onRef={ref => (this.overview = ref)} />;
 
-		// eslint-disable-next-line no-debugger
-		debugger;
-
 		if (this.state.tabValue === 1) {
 			if (loading) {
 				return this.renderLoadingScreen();
