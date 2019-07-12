5a7c9c1beb60114471a19fe99cfa392b890ab64f
Andre Goncalves <andre@andregoncalves.com>
2019-07-12 09:38:21 +0100

    feat: add support for env variables to toggle feature flags

--- src/common/feature-flags.js
+++ src/common/feature-flags.js
@@ -1,5 +1,10 @@
 import config from './config';
 
-const featureIsEnabled = featureName => !!config.features[featureName];
+const featureIsEnabled = featureName => {
+	if (process.env.hasOwnProperty('FEATURE_PAYMENT_CONTRACT')) {
+		return !!process.env.FEATURE_PAYMENT_CONTRACT;
+	}
+	return !!config.features[featureName];
+};
 
 export { featureIsEnabled };
