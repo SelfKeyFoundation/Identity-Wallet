b74c38b075194f7701677747a8e8ef604062c9eb
Maxim Kovalov <max.kovalov@gmail.com>
2019-08-26 14:34:58 +0300

    feat(identity): identity model

---
+++ src/main/identity/identity.js
@@ -0,0 +1,57 @@
+import { Model } from 'objection';
+import BaseModel from '../common/base-model';
+const TABLE_NAME = 'identities';
+
+export class Identity extends BaseModel {
+	static tableName = TABLE_NAME;
+	static idColumn = 'id';
+	static jsonSchema = {
+		type: 'object',
+		properties: {
+			id: { type: 'integer' },
+			walletId: { type: 'integer' },
+			parentId: { type: ['integer', null] },
+			rootIdentity: { type: 'boolean', default: true },
+			name: { type: 'string' },
+			type: { type: 'string' },
+			profilePicture: { type: 'binary' },
+			did: { type: 'string' },
+			isSetupFinished: { type: 'boolean' }
+		},
+		required: ['walletId', 'type']
+	};
+
+	static get relationMappings() {
+		const Wallet = require('../wallet/wallet').default;
+		return {
+			wallet: {
+				relation: Model.BelongsToOneRelation,
+				modelClass: Wallet,
+				join: {
+					from: `${this.tableName}.walletId`,
+					to: `${Wallet.tableName}.id`
+				}
+			}
+		};
+	}
+
+	static findAllByWalletId(walletId) {
+		return this.query()
+			.select()
+			.where({ walletId });
+	}
+
+	static async findById(id) {
+		return this.query().findById(id);
+	}
+
+	static create(itm, tx) {
+		return this.query(tx).insertAndFetch(itm);
+	}
+
+	static delete(id, tx) {
+		return this.query(tx).deleteById(id);
+	}
+}
+
+export default Identity;
--- src/main/migrations/20190819133653_identities.js
+++ src/main/migrations/20190819133653_identities.js
@@ -1,103 +1,109 @@
 /* istanbul ignore file */
 exports.up = async (knex, Promise) => {
-	await knex.schema.createTable('identities', t => {
-		t.increments('id');
-		t.integer('walletId');
-		t.integer('parentId');
-		t.boolean('rootIdentity');
-		t.binary('profilePicture');
-		t.string('type');
-		t.string('name');
-		t.integer('did');
-		t.boolean('isSetupFinished').defaultTo(false);
-		t.integer('createdAt').notNullable();
-		t.integer('updatedAt');
-	});
-	await knex.schema.renameTable('wallets', 'wallets_old');
-	await knex.schema.createTable('wallets', t => {
-		t.increments('id');
-		t.string('name');
-		t.string('address')
-			.unique()
-			.notNullable();
-		t.string('keystoreFilePath');
-		t.string('type')
-			.notNullable()
-			.defaultTo('local');
-		t.integer('createdAt').notNullable();
-		t.integer('updatedAt');
-	});
+	try {
+		await knex.schema.createTable('identities', t => {
+			t.increments('id');
+			t.integer('walletId');
+			t.integer('parentId');
+			t.boolean('rootIdentity');
+			t.binary('profilePicture');
+			t.string('type');
+			t.string('name');
+			t.string('did');
+			t.boolean('isSetupFinished').defaultTo(false);
+			t.integer('createdAt').notNullable();
+			t.integer('updatedAt');
+		});
+		await knex.schema.renameTable('wallets', 'wallets_old');
+		await knex.schema.createTable('wallets', t => {
+			t.increments('id');
+			t.string('name');
+			t.string('address')
+				.unique()
+				.notNullable();
+			t.string('keystoreFilePath');
+			t.string('type')
+				.notNullable()
+				.defaultTo('local');
+			t.integer('createdAt').notNullable();
+			t.integer('updatedAt');
+		});
 
-	let walletsOld = await knex('wallets_old').select();
+		let walletsOld = await knex('wallets_old').select();
 
-	let { wallets, identities } = walletsOld.reduce(
-		(acc, curr) => {
-			acc.wallets.push({
-				id: curr.id,
-				name: curr.name,
-				address: curr.publicKey,
-				keystoreFilePath: curr.keystoreFilePath,
-				type: curr.profile,
-				createdAt: curr.createdAt,
-				updatedAt: Date.now()
-			});
-			acc.identities.push({
-				name: curr.name,
-				walletId: curr.id,
-				parentId: null,
-				rootIdentity: true,
-				profilePicture: curr.profilePicture,
-				type: 'individual',
-				did: curr.did,
-				isSetupFinished: !!curr.isSetupFinished,
-				createdAt: Date.now(),
-				updatedAt: Date.now()
-			});
+		let { wallets, identities } = walletsOld.reduce(
+			(acc, curr) => {
+				acc.wallets.push({
+					id: curr.id,
+					name: curr.name,
+					address: curr.publicKey,
+					keystoreFilePath: curr.keystoreFilePath,
+					type: curr.profile,
+					createdAt: curr.createdAt,
+					updatedAt: Date.now()
+				});
+				acc.identities.push({
+					name: curr.name,
+					walletId: curr.id,
+					parentId: null,
+					rootIdentity: true,
+					profilePicture: curr.profilePicture,
+					type: 'individual',
+					did: curr.did,
+					isSetupFinished: !!curr.isSetupFinished,
+					createdAt: Date.now(),
+					updatedAt: Date.now()
+				});
+				return acc;
+			},
+			{ wallets: [], identities: [] }
+		);
+		if (!wallets.length) {
+			return;
+		}
+		await knex('wallets').insert(wallets);
+		await knex('identities').insert(identities);
+		await knex.schema.renameTable('id_attributes', 'id_attributes_old');
+
+		let attributesOld = await knex('id_attributes_old').select();
+
+		await knex.schema.createTable('id_attributes', t => {
+			t.increments('id');
+			t.integer('identityId')
+				.notNullable()
+				.references('identities.id');
+			t.string('name');
+			t.integer('typeId')
+				.notNullable()
+				.references('id_attribute_types.id');
+			t.text('data')
+				.notNullable()
+				.defaultTo('{}');
+			t.integer('createdAt').notNullable();
+			t.integer('updatedAt');
+		});
+
+		identities = await knex('identities').select();
+
+		let walletIdentities = identities.reduce((acc, curr) => {
+			acc[curr.walletId] = curr.id;
 			return acc;
-		},
-		{ wallets: [], identities: [] }
-	);
+		}, {});
 
-	await knex('wallets').insert(wallets);
-	await knex('identities').insert(identities);
-
-	await knex.schema.renameTable('id_attributes', 'id_attributes_old');
-
-	let attributesOld = await knex('id_attributes_old').select();
-
-	await knex.schema.createTable('id_attributes', t => {
-		t.increments('id');
-		t.integer('identityId')
-			.notNullable()
-			.references('identities.id');
-		t.string('name');
-		t.integer('typeId')
-			.notNullable()
-			.references('id_attribute_types.id');
-		t.text('data')
-			.notNullable()
-			.defaultTo('{}');
-		t.integer('createdAt').notNullable();
-		t.integer('updatedAt');
-	});
-
-	identities = await knex('identities').select();
-
-	let walletIdentities = identities.reduce((acc, curr) => {
-		acc[curr.walletId] = curr.id;
-		return acc;
-	}, {});
-
-	const attributes = attributesOld.map(attr => ({
-		id: attr.id,
-		identityId: walletIdentities[attr.walletId],
-		name: attr.name,
-		typeId: attr.typeId,
-		data: attr.data,
-		createdAt: attr.createdAt,
-		updatedAt: Date.now()
-	}));
-	await knex('id_attributes').insert(attributes);
+		const attributes = attributesOld.map(attr => ({
+			id: attr.id,
+			identityId: walletIdentities[attr.walletId],
+			name: attr.name,
+			typeId: attr.typeId,
+			data: attr.data,
+			createdAt: attr.createdAt,
+			updatedAt: Date.now()
+		}));
+		await knex('id_attributes').insert(attributes);
+	} catch (error) {
+		console.error(error.stack);
+		throw error;
+	}
 };
 
 exports.down = async (knex, Promise) => {
