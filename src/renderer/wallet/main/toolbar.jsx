910bc30dbdd184439eeac102fb2c4d8a00ac98f3
designhorf <designhorf@gmail.com>
2019-08-26 11:31:55 +0200

    feat: added basic switching profile to toolbar

--- src/renderer/wallet/main/toolbar.jsx
+++ src/renderer/wallet/main/toolbar.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { withStyles, Grid } from '@material-ui/core';
-import { MenuNewIcon } from 'selfkey-ui';
+import { withStyles, Grid, Typography } from '@material-ui/core';
+import { MenuNewIcon, DropdownIcon, PersonIcon } from 'selfkey-ui';
 import PriceBox from '../../price-box';
 import Sidebar from './sidebar';
 import config from 'common/config';
@@ -41,6 +41,24 @@ const styles = theme => ({
 	},
 	closedDrawer: {
 		left: '77px'
+	},
+	nameRole: {
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'space-around',
+		margin: '0 20px 0 15px',
+		maxWidth: '118px'
+	},
+	sepVertContainer: {
+		display: 'flex',
+		justifyContent: 'space-around',
+		marginRight: '20px'
+	},
+	sepVert: {
+		background: 'linear-gradient(180deg, rgba(94, 11, 128, 0) 0%, #475768 100%)',
+		height: '59px',
+		marginTop: '16px',
+		width: '1px'
 	}
 });
 
@@ -74,13 +92,42 @@ class Toolbar extends Component {
 					alignItems="center"
 					className={classes.wrapper}
 				>
-					<Grid item xs={12} style={{ paddingRight: '60px' }}>
+					<Grid item xs={10} style={{ paddingRight: '60px' }}>
 						<Grid container direction="row" justify="flex-end" alignItems="center">
 							<Grid item>
 								<PriceBox cryptoCurrency={config.constants.primaryToken} />
 							</Grid>
 							<Grid item>
 								<PriceBox cryptoCurrency="ETH" />
+							</Grid>
+						</Grid>
+						{/* <Grid item className={classes.sepVertContainer}>
+							<div className={classes.sepVert} />
+						</Grid> */}
+					</Grid>
+					<Grid item xs={2}>
+						<Grid
+							container
+							direction="row"
+							justify="flex-start"
+							alignItems="center"
+							spacing={0}
+						>
+							<Grid item style={{ width: '222px' }}>
+								<Grid container wrap="nowrap">
+									<Grid item>
+										<PersonIcon />
+									</Grid>
+									<Grid item className={classes.nameRole}>
+										<Typography variant="h6">Name</Typography>
+										<Typography variant="subtitle1" color="secondary">
+											Personal Profile
+										</Typography>
+									</Grid>
+									<Grid item style={{ marginTop: '18px', paddingRight: '15px' }}>
+										<DropdownIcon />
+									</Grid>
+								</Grid>
 							</Grid>
 						</Grid>
 					</Grid>
