15d2c2fc7cb6b5c75dd60dddaedf332501bda03d
designhorf <designhorf@gmail.com>
2019-08-28 06:59:48 +0200

    fix: width of Bank Accounts screen

--- src/renderer/marketplace/bank-accounts/list/offers-page.jsx
+++ src/renderer/marketplace/bank-accounts/list/offers-page.jsx
@@ -7,8 +7,13 @@ import { BankIcon } from 'selfkey-ui';
 
 const styles = theme => ({
 	pageContent: {
-		width: '1140px',
+		width: '1080px',
 		margin: '0 auto'
+	},
+	'@media screen and (min-width: 1230px)': {
+		pageContent: {
+			width: '1140px'
+		}
 	},
 	header: {
 		borderBottom: 'solid 1px #475768',
@@ -27,7 +32,7 @@ const styles = theme => ({
 		width: '36px'
 	},
 	backButtonContainer: {
-		left: '15px',
+		left: '75px',
 		position: 'absolute'
 	},
 	tabs: {
