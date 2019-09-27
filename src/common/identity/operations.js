8de6ad138f4c7484d1dba642ff99634d8d359626
Andre Goncalves <andre@andregoncalves.com>
2019-09-27 16:09:29 +0100

    fix: select identity only when new

--- src/common/identity/operations.js
+++ src/common/identity/operations.js
@@ -150,8 +150,8 @@ const createCorporateProfileOperation = data => async (dispatch, getState) => {
 		identity = await dispatch(
 			identityOperations.createIdentityOperation(wallet.id, 'corporate')
 		);
+		await dispatch(identityOperations.setCurrentIdentityAction(identity.id));
 	}
-	await dispatch(identityOperations.setCurrentIdentityAction(identity.id));
 	const getTypeId = url => {
 		return idAttributeTypes.find(idAttributeType => idAttributeType.url === url).id;
 	};
