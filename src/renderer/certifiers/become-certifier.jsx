94a4af712a31e852725b8358326a2d2a88c5d216
designhorf <designhorf@gmail.com>
2019-10-07 10:56:18 +0200

    fix: removed Grid

--- src/renderer/certifiers/become-certifier.jsx
+++ src/renderer/certifiers/become-certifier.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Typography, Button, Divider, withStyles, IconButton } from '@material-ui/core';
+import { Typography, Button, Divider, withStyles, IconButton } from '@material-ui/core';
 import { CloseButtonIcon, KeyTooltip, TooltipArrow, InfoTooltip } from 'selfkey-ui';
 
 const styles = theme => ({
@@ -10,7 +10,10 @@ const styles = theme => ({
 		width: '100%'
 	},
 	containerHeader: {
+		alignItems: 'flex-start',
 		background: '#2A3540',
+		display: 'flex',
+		justify: 'flex-start',
 		padding: '25px 30px',
 		'& div': {
 			display: 'inline-block',
@@ -32,14 +35,24 @@ const styles = theme => ({
 		marginBottom: '20px'
 	},
 	whyText: {
+		display: 'flex',
+		flexDirection: 'column',
 		marginBottom: 35
 	},
-	serviceCostBox: {
-		marginBottom: '20px'
+	steps: {
+		alignItems: 'center',
+		display: 'flex',
+		justifyContent: 'space-between'
+	},
+	applyBtn: {
+		marginRight: '20px'
 	}
 });
 
 const serviceCostStyle = theme => ({
+	serviceCostBox: {
+		marginBottom: '20px'
+	},
 	serviceCost: {
 		paddingBottom: '30px',
 		width: '100%'
@@ -70,12 +83,13 @@ const serviceCostStyle = theme => ({
 			color: theme.palette.secondary.main
 		}
 	},
+	priceRowBox: {
+		alignItems: 'center',
+		display: 'flex',
+		justifyContent: 'flex-start'
+	},
 	bold: {
 		fontWeight: 600
-	},
-	rowSeparator: {
-		border: '1px solid #475768',
-		margin: '30px 0'
 	}
 });
 
@@ -114,46 +128,42 @@ const stepItemStyle = theme => ({
 });
 
 export const SimpleServiceCost = withStyles(serviceCostStyle)(({ classes }) => (
-	<div className={classes.serviceCost}>
-		<Typography variant="h2">Service Costs</Typography>
-		<div className={classes.priceTable}>
-			<div className={classes.priceRow}>
-				<Grid
-					container
-					direction="row"
-					justify="flex-start"
-					alignItems="center"
-					spacing={0}
-				>
-					<div className="rowItem">
-						<Typography variant="body2" color="secondary">
-							Network Transaction Fee
-						</Typography>
+	<div className={classes.serviceCostBox}>
+		<div className={classes.serviceCost}>
+			<Typography variant="h2">Service Costs</Typography>
+			<div className={classes.priceTable}>
+				<div className={classes.priceRow}>
+					<div className={classes.priceRowBox}>
+						<div className="rowItem">
+							<Typography variant="body2" color="secondary">
+								Network Transaction Fee
+							</Typography>
+						</div>
+						<div className="rowItem time" />
+						<div className="rowItem price">
+							<Typography className={classes.bold} variant="body2" color="primary">
+								$0,01
+							</Typography>
+							<Typography variant="subtitle2" color="secondary">
+								0,3237484 ETH
+								<KeyTooltip
+									interactive
+									placement="top-start"
+									title={
+										<React.Fragment>
+											<span>...</span>
+											<TooltipArrow />
+										</React.Fragment>
+									}
+								>
+									<IconButton aria-label="Info">
+										<InfoTooltip />
+									</IconButton>
+								</KeyTooltip>
+							</Typography>
+						</div>
 					</div>
-					<div className="rowItem time" />
-					<div className="rowItem price">
-						<Typography className={classes.bold} variant="body2" color="primary">
-							$0,01
-						</Typography>
-						<Typography variant="subtitle2" color="secondary">
-							0,3237484 ETH
-							<KeyTooltip
-								interactive
-								placement="top-start"
-								title={
-									<React.Fragment>
-										<span>...</span>
-										<TooltipArrow />
-									</React.Fragment>
-								}
-							>
-								<IconButton aria-label="Info">
-									<InfoTooltip />
-								</IconButton>
-							</KeyTooltip>
-						</Typography>
-					</div>
-				</Grid>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -186,101 +196,75 @@ class BecomeCertifierComponent extends Component {
 		return (
 			<div className={classes.container}>
 				<CloseButtonIcon onClick={onBackClick} className={classes.closeIcon} />
-				<Grid
-					container
-					justify="flex-start"
-					alignItems="flex-start"
-					className={classes.containerHeader}
-				>
+				<div className={classes.containerHeader}>
 					<Typography variant="h2" className="region">
 						Become a Certifier
 					</Typography>
-				</Grid>
+				</div>
 				<div className={classes.contentContainer}>
-					<Grid container className={classes.whyText}>
-						<Grid item>
-							<Typography variant="h2" gutterBottom>
-								Why you get
-							</Typography>
-						</Grid>
-						<Grid item>
-							<Typography variant="body2">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id
-								nulla purus. Pellentesque nisl metus, finibus a lectus quis, tempus
-								hendrerit lectus. Praesent nunc magna, porttitor id justo non,
-								vulputate laoreet massa. Aenean commodo in justo vel consectetur.
-								Curabitur finibus sem vitae urna cursus condimentum. Fusce enim
-								arcu, pharetra in ornare a, congue vitae nibh. Cras tincidunt diam
-								quis sem ultrices pulvinar. Maecenas sed velit tincidunt, egestas
-								magna ac, commodo ante. Duis molestie libero vitae lorem volutpat, a
-								tempor erat fringilla. <br />
-								<br />
-								Nam eu consequat diam. In hac habitasse platea dictumst. Vivamus
-								lacinia, erat non lobortis vestibulum, ligula enim hendrerit lectus,
-								non maximus mi ex ac dolor. Curabitur sit amet gravida augue. In
-								vitae felis bibendum, feugiat mi a, condimentum tellus. Cras
-								porttitor laoreet mauris eget fermentum. Nunc pellentesque non quam
-								eu fermentum. Mauris nec metus non massa viverra vestibulum eu vel
-								arcu. Vestibulum ante ipsum primis in faucibus orci luctus et
-								ultrices posuere cubilia Curae.
-							</Typography>
-						</Grid>
-					</Grid>
+					<div className={classes.whyText}>
+						<Typography variant="h2" gutterBottom>
+							Why you get
+						</Typography>
+						<Typography variant="body2">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id nulla
+							purus. Pellentesque nisl metus, finibus a lectus quis, tempus hendrerit
+							lectus. Praesent nunc magna, porttitor id justo non, vulputate laoreet
+							massa. Aenean commodo in justo vel consectetur. Curabitur finibus sem
+							vitae urna cursus condimentum. Fusce enim arcu, pharetra in ornare a,
+							congue vitae nibh. Cras tincidunt diam quis sem ultrices pulvinar.
+							Maecenas sed velit tincidunt, egestas magna ac, commodo ante. Duis
+							molestie libero vitae lorem volutpat, a tempor erat fringilla. <br />
+							<br />
+							Nam eu consequat diam. In hac habitasse platea dictumst. Vivamus
+							lacinia, erat non lobortis vestibulum, ligula enim hendrerit lectus, non
+							maximus mi ex ac dolor. Curabitur sit amet gravida augue. In vitae felis
+							bibendum, feugiat mi a, condimentum tellus. Cras porttitor laoreet
+							mauris eget fermentum. Nunc pellentesque non quam eu fermentum. Mauris
+							nec metus non massa viverra vestibulum eu vel arcu. Vestibulum ante
+							ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.
+						</Typography>
+					</div>
 					<Divider className={classes.divider} />
 					<Typography variant="h2" gutterBottom>
 						How the process works
 					</Typography>
-					<Grid item>
-						<Grid
-							container
-							direction="row"
-							justify="space-between"
-							alignItems="center"
-							spacing={0}
-						>
-							<StepItem
-								number={1}
-								title="Join the maketplace"
-								description="Joining our marketplace is FREE, you are paying only once for the Network Transaction Fee "
-								link="(what’s this?)."
-							/>
-							<StepItem
-								number={2}
-								title="Pick the certifications requests you want"
-								description="You get to see all the certifications requests from our users, and you can pick to work on whatever you wish."
-							/>
-							<StepItem
-								number={3}
-								title="Earn Money"
-								description="For each certification process you complete you earn money, as KEY tokens. You can swap the token for any currency you need."
-							/>
-						</Grid>
-					</Grid>
+					<div className={classes.steps}>
+						<StepItem
+							number={1}
+							title="Join the maketplace"
+							description="Joining our marketplace is FREE, you are paying only once for the Network Transaction Fee "
+							link="(what’s this?)."
+						/>
+						<StepItem
+							number={2}
+							title="Pick the certifications requests you want"
+							description="You get to see all the certifications requests from our users, and you can pick to work on whatever you wish."
+						/>
+						<StepItem
+							number={3}
+							title="Earn Money"
+							description="For each certification process you complete you earn money, as KEY tokens. You can swap the token for any currency you need."
+						/>
+					</div>
 
 					<Divider className={classes.divider} />
 
-					<Grid item className={classes.serviceCostBox}>
-						<SimpleServiceCost />
-					</Grid>
+					<SimpleServiceCost />
 
-					<Grid item>
-						<Grid container direction="row" spacing={24}>
-							<Grid item>
-								<Button
-									variant="contained"
-									size="large"
-									onClick={this.onStartClick}
-								>
-									Apply as a Certifier
-								</Button>
-							</Grid>
-							<Grid item>
-								<Button variant="outlined" size="large" onClick={onBackClick}>
-									Cancel
-								</Button>
-							</Grid>
-						</Grid>
-					</Grid>
+					<div>
+						<Button
+							className={classes.applyBtn}
+							variant="contained"
+							size="large"
+							onClick={this.onStartClick}
+						>
+							Apply as a Certifier
+						</Button>
+						<Button variant="outlined" size="large" onClick={onBackClick}>
+							Cancel
+						</Button>
+					</div>
 				</div>
 			</div>
 		);
