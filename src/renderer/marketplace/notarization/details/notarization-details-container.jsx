c3a6a1061038c0cea3883dd7d15aba97408c0769
Ferenc Horvath <designhorf@gmail.com>
2019-10-24 13:54:21 +0200

    feat: route to request notarization and handle no DID or ID cases

--- src/renderer/marketplace/notarization/details/notarization-details-container.jsx
+++ src/renderer/marketplace/notarization/details/notarization-details-container.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { push } from 'connected-react-router';
 import { pricesSelectors } from 'common/prices';
+import { identitySelectors } from 'common/identity';
+// import { kycSelectors } from 'common/kyc';
 import { withStyles } from '@material-ui/core/styles';
 import { marketplaceSelectors } from 'common/marketplace';
 import { MarketplaceNotariesComponent } from '../common/marketplace-notaries-component';
@@ -12,7 +14,8 @@ const styles = theme => ({});
 
 class NotarizationDetailsContainer extends MarketplaceNotariesComponent {
 	state = {
-		tab: 'types'
+		tab: 'types',
+		loading: false
 	};
 
 	componentDidMount() {
@@ -23,34 +26,57 @@ class NotarizationDetailsContainer extends MarketplaceNotariesComponent {
 
 	onTabChange = tab => this.setState({ tab });
 
-	render() {
-		const { isLoading, keyRate } = this.props;
+	onApplyClick = () => {
+		const { identity } = this.props;
+		const selfkeyIdRequiredRoute = '/main/marketplace/selfkey-id-required';
+		const selfkeyDIDRequiredRoute = '/main/marketplace/selfkey-did-required';
+		const requestNotarizationRoute = '/main/marketplace/notaries/process';
 
+		// When clicking the start process,
+		// we check if an authenticated kyc-chain session exists
+		// If it doesn't we trigger a new authenticated rp session
+		// and redirect to checkout route
+		// The loading state is used to disable the button while data is being loaded
+		console.log(`${identity}`);
+		this.setState({ loading: true }, async () => {
+			if (!identity.isSetupFinished) {
+				return this.props.dispatch(push(selfkeyIdRequiredRoute));
+			}
+			if (!identity.did) {
+				return this.props.dispatch(push(selfkeyDIDRequiredRoute));
+			} else {
+				await this.props.dispatch(push(requestNotarizationRoute));
+			}
+		});
+	};
+
+	render() {
+		const { keyRate, kycRequirements, templateId } = this.props;
 		return (
 			<NotarizationDetailsPage
 				onBackClick={this.onBackClick}
 				keyRate={keyRate}
-				loading={isLoading}
+				loading={this.state.loading || this.props.isLoading}
 				tab={this.state.tab}
+				kycRequirements={kycRequirements}
+				templateId={templateId}
 				onTabChange={this.onTabChange}
+				startNotarize={this.onApplyClick}
 			/>
 		);
 	}
 }
 
 NotarizationDetailsContainer.propTypes = {
-	// inventory: PropTypes.array,
-	// vendors: PropTypes.array,
 	isLoading: PropTypes.bool,
 	keyRate: PropTypes.number
 };
 
 const mapStateToProps = (state, props) => {
 	return {
-		// vendors: marketplaceSelectors.selectVendorsForCategory(state, 'banking'),
-		// inventory: marketplaceSelectors.selectBanks(state),
 		isLoading: marketplaceSelectors.isLoading(state),
-		keyRate: pricesSelectors.getRate(state, 'KEY', 'USD')
+		keyRate: pricesSelectors.getRate(state, 'KEY', 'USD'),
+		identity: identitySelectors.selectCurrentIdentity(state)
 	};
 };
 
