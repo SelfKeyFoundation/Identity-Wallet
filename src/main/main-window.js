1a2cab408a0ccb1e2288bcf37fd1a9e6af80a713
Maxim Kovalov <max.kovalov@gmail.com>
2020-01-23 17:30:56 +0200

    feat(window-controls): add env vars to controll windows size

--- src/main/main-window.js
+++ src/main/main-window.js
@@ -13,15 +13,16 @@ export const createMainWindow = async () => {
 	const windowOptions = {
 		id: 'main-window',
 		title: electron.app.getName(),
-		width: 1170,
-		height: 800,
-		minWidth: 1170,
-		minHeight: 800,
+		width: +process.env.WINDOW_WIDTH || 1170,
+		height: +process.env.WINDOW_HEIGHT || 800,
+		minWidth: +process.env.WINDOW_MIN_WIDTH || 1170,
+		minHeight: +process.env.WINDOW_MIN_HEIGHT || 800,
 		webPreferences: {
 			nodeIntegration: true,
 			webSecurity: true,
 			disableBlinkFeatures: 'Auxclick',
-			preload: path.resolve(__dirname, 'preload.js')
+			preload: path.resolve(__dirname, 'preload.js'),
+			zoomFactor: +process.env.WINDOW_ZOOM_FACTOR || 1
 		},
 		icon: __static + '/assets/icons/png/newlogo-256x256.png'
 	};
@@ -32,8 +33,8 @@ export const createMainWindow = async () => {
 		delay: 0,
 		minVisible: 1500,
 		splashScreenOpts: {
-			height: 800,
-			width: 1170,
+			height: process.env.WINDOW_HEIGHT || 800,
+			width: process.env.WINDOW_WIDTH || 1170,
 			transparent: true
 		}
 	});
