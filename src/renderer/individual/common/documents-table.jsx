3b3c365082c0273fa8ef6d1ebbe5c8add712bba3
Andre Goncalves <andre@andregoncalves.com>
2020-01-27 13:34:46 +0000

    refactor: move selfkey components to individual entity folder (#1976)
    * refactor: move selfkey components to individual entity folder
    
    * refactor: individual applications
    
    * feat: reload relying party, fix storybook
    
    * feat: storybook for individual profile components
    
    Co-authored-by: Kovalov Maxim <max.kovalov@gmail.com>

---
+++ src/renderer/individual/common/documents-table.jsx
@@ -0,0 +1,158 @@
+import React from 'react';
+import moment from 'moment';
+import {
+	Table,
+	TableBody,
+	TableRow,
+	IconButton,
+	TableHead,
+	Typography,
+	withStyles
+} from '@material-ui/core';
+import {
+	FilePdfIcon,
+	FileImageIcon,
+	FileDefaultIcon,
+	FileMultipleIcon,
+	FileAudioIcon,
+	FileLinkWithModal,
+	EditTransparentIcon,
+	DeleteIcon,
+	SmallTableHeadRow,
+	SmallTableCell
+} from 'selfkey-ui';
+
+const styles = theme => ({
+	labelCell: {
+		whiteSpace: 'normal',
+		wordBreak: 'break-all',
+		'& > div': {
+			alignItems: 'center'
+		}
+	},
+	documentColumn: {
+		display: 'flex',
+		alignItems: 'center',
+		'& .file-icon': {
+			marginRight: '15px'
+		}
+	},
+	ellipsis: {
+		overflow: 'hidden',
+		textOverflow: 'ellipsis',
+		whiteSpace: 'nowrap',
+		maxWidth: '222px'
+	}
+});
+
+const lastUpdateDate = ({ updatedAt }) => moment(updatedAt).format('DD MMM YYYY, hh:mm a');
+
+const DocumentExpiryDate = ({ doc }) => {
+	let date;
+	if (!doc || !doc.data || !doc.data.value || !doc.data.value.expires) {
+		date = '-';
+	} else {
+		date = moment(doc.data.value.expires).format('DD MMM YYYY');
+	}
+	return <span>{date}</span>;
+};
+
+const DocumentName = ({ entry, classes }) => {
+	let fileType = null;
+	let fileName = null;
+	let hasOneDocument = false;
+	let FileIcon = FileDefaultIcon;
+
+	if (entry.documents.length === 1) {
+		fileType = entry.documents[0].mimeType;
+		hasOneDocument = true;
+		if (fileType) {
+			if (fileType === 'application/pdf') FileIcon = FilePdfIcon;
+			else if (fileType.startsWith('audio')) FileIcon = FileAudioIcon;
+			else if (fileType.startsWith('image')) FileIcon = FileImageIcon;
+		}
+	} else if (entry.documents.length > 1) {
+		fileName = `${entry.documents.length} files`;
+		FileIcon = FileMultipleIcon;
+	}
+
+	return (
+		<div className={classes.documentColumn}>
+			<div className="file-icon">
+				<FileIcon />
+			</div>
+			<div>
+				<Typography variant="h6">{entry.name}</Typography>
+				{fileName && (
+					<Typography
+						variant="subtitle1"
+						color="secondary"
+						className={classes.ellipsis}
+						title={fileName}
+					>
+						{fileName}
+					</Typography>
+				)}
+				{hasOneDocument && (
+					<FileLinkWithModal
+						file={entry.documents[0]}
+						small
+						onPDFOpen={file => window.openPDF(file.content || file.url)}
+					/>
+				)}
+			</div>
+		</div>
+	);
+};
+
+export const DocumentsTable = withStyles(styles)(
+	({ classes, documents = [], onEditAttribute, onDeleteAttribute }) => (
+		<Table>
+			<TableHead>
+				<SmallTableHeadRow>
+					<SmallTableCell>
+						<Typography variant="overline">Type</Typography>
+					</SmallTableCell>
+					<SmallTableCell>
+						<Typography variant="overline">Label</Typography>
+					</SmallTableCell>
+					<SmallTableCell>
+						<Typography variant="overline">Expiry Date</Typography>
+					</SmallTableCell>
+					<SmallTableCell>
+						<Typography variant="overline">Last Edited</Typography>
+					</SmallTableCell>
+					<SmallTableCell align="right">
+						<Typography variant="overline">Actions</Typography>
+					</SmallTableCell>
+				</SmallTableHeadRow>
+			</TableHead>
+			<TableBody>
+				{documents.map(entry => (
+					<TableRow key={entry.id}>
+						<SmallTableCell>
+							<Typography variant="h6">{entry.type.content.title}</Typography>
+						</SmallTableCell>
+						<SmallTableCell className={classes.labelCell}>
+							<DocumentName entry={entry} classes={classes} />
+						</SmallTableCell>
+						<SmallTableCell>
+							<DocumentExpiryDate doc={entry} />
+						</SmallTableCell>
+						<SmallTableCell>
+							<Typography variant="h6">{lastUpdateDate(entry)}</Typography>
+						</SmallTableCell>
+						<SmallTableCell align="right">
+							<IconButton onClick={() => onEditAttribute(entry)}>
+								<EditTransparentIcon />
+							</IconButton>
+							<IconButton onClick={() => onDeleteAttribute(entry)}>
+								<DeleteIcon />
+							</IconButton>
+						</SmallTableCell>
+					</TableRow>
+				))}
+			</TableBody>
+		</Table>
+	)
+);
---
+++ stories/individual.stories.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from '@storybook/react';
+
+import { RegisterDidCard } from '../src/renderer/did/register-did-card';
+
+import { IndividualDashboardPage } from '../src/renderer/individual/dashboard/dashboard-page';
+import { IndividualDashboardTabs } from '../src/renderer/individual/dashboard/dashboard-tabs';
+import { IndividualOverviewTab } from '../src/renderer/individual/dashboard/overview-tab';
+import { IndividualApplicationsTab } from '../src/renderer/individual/dashboard/applications-tab';
+import { AttributesTable } from '../src/renderer/individual/common/attributes-table';
+import { DocumentsTable } from '../src/renderer/individual/common/documents-table';
+
+import { dummyProfile, dummyProfileWithoutDid } from './individual-data';
+
+storiesOf('Individual Profile', module).add('Dashboard', () => (
+	<div style={{ width: '1140px' }}>
+		<IndividualDashboardPage />
+	</div>
+));
+
+storiesOf('Individual Profile/Dashboard Tabs', module)
+	.add('default', () => (
+		<IndividualDashboardTabs
+			profile={dummyProfile}
+			didComponent={
+				<RegisterDidCard
+					onRegisterDidClick={action('register did click')}
+					onAssociateDidClick={action('associate did click')}
+				/>
+			}
+			onMarketplaceClick={action('on marketplace')}
+			onAvatarClick={action('on profile picture')}
+			onEditAttribute={action('on edit attribute')}
+			onDeleteAttribute={action('on delete attribute')}
+			onAddAttribute={action('on add attribute')}
+			onAddDocument={action('on add document')}
+			onTabChange={linkTo('Individual Identity/Dashboard Tabs', tab => tab)}
+		/>
+	))
+	.add('overview', () => (
+		<IndividualDashboardTabs
+			tab="overview"
+			profile={dummyProfile}
+			didComponent={
+				<RegisterDidCard
+					onRegisterDidClick={action('register did click')}
+					onAssociateDidClick={action('associate did click')}
+				/>
+			}
+			onMarketplaceClick={action('on marketplace')}
+			onAvatarClick={action('on profile picture')}
+			onEditAttribute={action('on edit attribute')}
+			onDeleteAttribute={action('on delete attribute')}
+			onAddAttribute={action('on add attribute')}
+			onAddDocument={action('on add document')}
+			onTabChange={linkTo('Individual Identity/Dashboard Tabs', tab => tab)}
+		/>
+	))
+	.add('applications', () => (
+		<IndividualDashboardTabs
+			tab="applications"
+			profile={dummyProfile}
+			didComponent={
+				<RegisterDidCard
+					onRegisterDidClick={action('register did click')}
+					onAssociateDidClick={action('associate did click')}
+				/>
+			}
+			onMarketplaceClick={action('on marketplace')}
+			onAvatarClick={action('on profile picture')}
+			onEditAttribute={action('on edit attribute')}
+			onDeleteAttribute={action('on delete attribute')}
+			onAddAttribute={action('on add attribute')}
+			onAddDocument={action('on add document')}
+			onTabChange={linkTo('Individual Identity/Dashboard Tabs', tab => tab)}
+		/>
+	))
+
+	.add('overview tab', () => (
+		<IndividualOverviewTab
+			profile={dummyProfile}
+			onMarketplaceClick={action('on marketplace')}
+			onAvatarClick={action('on profile picture')}
+			onEditAttribute={action('on edit attribute')}
+			onDeleteAttribute={action('on delete attribute')}
+			onAddAttribute={action('on add attribute')}
+			onAddDocument={action('on add document')}
+		/>
+	))
+	.add('overview tab without DID', () => (
+		<IndividualOverviewTab
+			profile={dummyProfileWithoutDid}
+			didComponent={
+				<RegisterDidCard
+					onRegisterDidClick={action('register did click')}
+					onAssociateDidClick={action('associate did click')}
+				/>
+			}
+			onMarketplaceClick={action('on marketplace')}
+			onAvatarClick={action('on profile picture')}
+			onEditAttribute={action('on edit attribute')}
+			onDeleteAttribute={action('on delete attribute')}
+			onAddAttribute={action('on add attribute')}
+			onAddDocument={action('on add document')}
+		/>
+	))
+	.add('applications tab', () => <IndividualApplicationsTab applications={[]} />)
+	.add('empty applications tab', () => <IndividualApplicationsTab />);
+
+storiesOf('Individual Profile/Components', module)
+	.add('Documents Table', () => (
+		<DocumentsTable
+			documents={dummyProfile.documents}
+			onEditAttribute={action('on edit attribute')}
+			onDeleteAttribute={action('on delete attribute')}
+		/>
+	))
+	.add('Attributes Table', () => (
+		<AttributesTable
+			attributes={dummyProfile.allAttributes}
+			onEditAttribute={action('on edit attribute')}
+			onDeleteAttribute={action('on delete attribute')}
+		/>
+	));
