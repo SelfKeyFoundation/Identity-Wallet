d7cc990fbe49f6c5c23f4c5fb7d5d6845fd0c91d
Ferenc Horvath <designhorf@gmail.com>
2020-02-10 09:40:59 +0100

    fix: alignment and style of advanced transaction

--- src/renderer/transaction/send/advanced-transaction.jsx
+++ src/renderer/transaction/send/advanced-transaction.jsx
@@ -9,36 +9,17 @@ import { getLocale } from 'common/locale/selectors';
 import { getFiatCurrency } from 'common/fiatCurrency/selectors';
 import { getTokens } from 'common/wallet-tokens/selectors';
 import { withStyles } from '@material-ui/core/styles';
-import { Grid, Divider } from '@material-ui/core';
+import { Grid, MenuItem, Select, Input, Button, Typography, Divider } from '@material-ui/core';
 import { appOperations, appSelectors } from 'common/app';
 import { push } from 'connected-react-router';
 import { debounce, over } from 'lodash';
+import { KeyboardArrowDown } from '@material-ui/icons';
+import { InputTitle } from '../../common/input-title';
 
 const styles = theme => ({
 	container: {
 		fontFamily: 'Lato, arial, sans-serif'
 	},
-
-	button: {
-		boxSizing: 'border-box',
-		height: '45px',
-		width: '201px',
-		border: '1px solid #0FB8D0',
-		borderRadius: '4px',
-		background: 'linear-gradient(0deg, #09A8BA 0%, #0ABBD0 100%)',
-		color: '#FFFFFF',
-		fontSize: '16px',
-		fontWeight: 600,
-		letterSpacing: '0.67px',
-		lineHeight: '20px',
-		textAlign: 'center',
-		cursor: 'pointer',
-		'&:disabled': {
-			cursor: 'default',
-			opacity: 0.7
-		}
-	},
-
 	selectAllAmountBtn: {
 		cursor: 'pointer',
 		fontSize: '13px',
@@ -55,23 +36,11 @@ const styles = theme => ({
 	actionButtonsContainer: {
 		paddingTop: '50px'
 	},
-	input: {
-		outlineWidth: 0,
-		backgroundColor: '#262f39',
-		paddingBottom: '20px',
-		marginBottom: '20px',
-		width: '100%',
-		border: 'none',
-		borderBottom: '2px solid #93b0c1',
-		fontSize: '20px',
-		color: '#a9c5d6',
-		'&::-webkit-input-placeholder': {
-			fontSize: '20px',
-			color: '#a9c5d6'
-		}
-	},
-	inputError: {
-		borderBottom: '2px solid #FE4B61 !important'
+	errorColor: {
+		backgroundColor: 'rgba(255,46,99,0.09) !important',
+		border: '2px solid #FE4B61 !important',
+		boxShadow: 'none !important',
+		color: '#FE4B61 !important'
 	},
 	amountContainer: {
 		paddingTop: '25px',
@@ -104,7 +73,6 @@ const styles = theme => ({
 		margin: '0px',
 		padding: '0px'
 	},
-
 	addressErrorText: {
 		height: '19px',
 		width: '242px',
@@ -113,36 +81,51 @@ const styles = theme => ({
 		fontSize: '13px',
 		lineHeight: '19px'
 	},
-
 	addressErrorColor: {
 		color: '#FE4B61',
 		borderBottom: '2px solid #FE4B61'
 	},
-
 	divider: {
-		backgroundColor: 'transparent',
-		borderBottom: '2px solid #93b0c1',
-		paddingTop: '10px'
+		margin: '40px 0'
 	},
-
 	cryptoSelect: {
-		height: '40px',
-		width: '300px',
-		color: '#FFFFFF',
-		fontFamily: 'Lato',
-		fontSize: '20px',
-		lineHeight: '36px',
-		backgroundColor: '#1E262E',
-		border: '1px solid #384656',
-		borderRadius: '30px',
-		paddingLeft: '10px',
-		paddingBottom: '10px'
+		width: '100%'
 	},
-
 	selectItem: {
 		border: 0,
 		backgroundColor: '#1E262E',
 		color: '#FFFFFF'
+	},
+	tokenBottomSpace: {
+		marginBottom: '30px'
+	},
+	flexColumn: {
+		flexDirection: 'column'
+	},
+	tokenMax: {
+		display: 'flex',
+		flexWrap: 'nowrap',
+		marginBottom: '5px'
+	},
+	bottomSpace: {
+		marginBottom: '20px'
+	},
+	fiatPrice: {
+		display: 'flex'
+	},
+	amount: {
+		marginRight: '20px'
+	},
+	errorText: {
+		height: '19px',
+		width: '242px',
+		color: '#FE4B61',
+		fontFamily: 'Lato',
+		fontSize: '13px',
+		lineHeight: '19px'
+	},
+	lofasz: {
+		padding: '28px 100px'
 	}
 });
 
@@ -267,9 +250,11 @@ class TransactionSendBoxContainer extends PureComponent {
 
 		return activeTokens.map(token => {
 			return (
-				<option key={token.symbol} value={token.symbol} className={classes.selectItem}>{`${
-					token.name
-				} - ${token.balance} ${token.symbol}`}</option>
+				<MenuItem
+					key={token.symbol}
+					value={token.symbol}
+					className={classes.selectItem}
+				>{`${token.symbol} - ${token.name}`}</MenuItem>
 			);
 		});
 	}
@@ -290,14 +275,14 @@ class TransactionSendBoxContainer extends PureComponent {
 					spacing={24}
 				>
 					<Grid item>
-						<button className={classes.button} onClick={this.handleConfirm}>
+						<Button variant="contained" size="large" onClick={this.handleConfirm}>
 							CONFIRM
-						</button>
+						</Button>
 					</Grid>
 					<Grid item>
-						<button className={classes.button} onClick={this.handleCancel}>
+						<Button variant="outlined" size="large" onClick={this.handleCancel}>
 							CANCEL
-						</button>
+						</Button>
 					</Grid>
 				</Grid>
 			);
@@ -311,13 +296,15 @@ class TransactionSendBoxContainer extends PureComponent {
 					className={classes.actionButtonsContainer}
 				>
 					<Grid item>
-						<button
+						<Button
 							disabled={!sendBtnIsEnabled}
 							className={classes.button}
 							onClick={this.handleSend}
+							variant="contained"
+							size="large"
 						>
 							SEND
-						</button>
+						</Button>
 					</Grid>
 				</Grid>
 			);
@@ -331,102 +318,98 @@ class TransactionSendBoxContainer extends PureComponent {
 
 	render() {
 		const { classes, addressError, amountUsd, locale, fiatCurrency } = this.props;
+		const labelInputClass = `${addressError ? classes.errorColor : ''}`;
 		let { cryptoCurrency } = this.state;
-		let sendAmountClass = `${classes.input} ${classes.amountInput}`;
-		let addressInputClass = `${classes.input} ${addressError ? classes.addressErrorColor : ''}`;
 		const title = this.getTitle(cryptoCurrency);
+		console.log(addressError);
 		return (
 			<TransactionBox
 				cryptoCurrency={cryptoCurrency}
 				closeAction={this.handleCancelAction}
 				title={title}
 			>
-				<input
-					type="text"
-					onChange={e => this.handleAddressChange(e)}
-					value={this.state.address}
-					className={addressInputClass}
-					placeholder="Send to Address"
-				/>
+				<div className={classes.tokenBottomSpace}>
+					<InputTitle title="Token" />
+					{this.state.isCustomView && (
+						<Select
+							className={classes.cryptoSelect}
+							value={this.state.cryptoCurrency}
+							onChange={e => this.handleCryptoCurrencyChange(e)}
+							name="cryptoCurrency"
+							disableUnderline
+							IconComponent={KeyboardArrowDown}
+							input={<Input disableUnderline />}
+						>
+							{this.renderSelectTokenItems()}
+						</Select>
+					)}
+				</div>
+				<div className={classes.bottomSpace}>Available: {this.state.amount}</div>
+				<div className={classes.tokenBottomSpace}>
+					<InputTitle title="Amount" />
+					<div className={classes.tokenMax}>
+						<Input
+							type="text"
+							onChange={e => this.handleAmountChange(e)}
+							value={this.state.amount}
+							placeholder="0.00"
+							className={classes.amount}
+							fullWidth
+						/>
+						<Button
+							onClick={() => this.handleAllAmountClick()}
+							variant="outlined"
+							size="large"
+						>
+							Max
+						</Button>
+					</div>
+					<div className={classes.fiatPrice}>
+						<Typography
+							variant="subtitle2"
+							color="secondary"
+							style={{ marginRight: '3px' }}
+						>
+							<NumberFormat
+								locale={locale}
+								priceStyle="currency"
+								currency={fiatCurrency}
+								value={amountUsd}
+								fractionDigits={15}
+							/>
+						</Typography>
+						<Typography variant="subtitle2" color="secondary">
+							USD
+						</Typography>
+					</div>
+				</div>
+
+				<div>
+					<InputTitle title="Send to" />
+					<div className={`${classes.tokenMax} ${classes.flexColumn}`}>
+						<Input
+							type="text"
+							onChange={e => this.handleAddressChange(e)}
+							value={this.state.address}
+							placeholder="0x"
+							className={labelInputClass}
+							fullWidth
+						/>
+						{addressError && (
+							<span id="labelError" className={classes.errorText}>
+								Invalid address. Please check and try again.
+							</span>
+						)}
+					</div>
+				</div>
+
+				<Divider className={classes.divider} />
+
 				{addressError && (
 					<span className={classes.addressErrorText}>
 						Invalid address. Please check and try again.
 					</span>
 				)}
-				<Grid
-					container
-					direction="row"
-					className={classes.amountContainer}
-					alignItems="center"
-					justify="space-between"
-				>
-					<Grid item xs>
-						<Grid
-							container
-							direction="row"
-							justify="flex-start"
-							alignItems="center"
-							spacing={16}
-						>
-							<Grid item>
-								<button
-									onClick={() => this.handleAllAmountClick()}
-									className={classes.selectAllAmountBtn}
-								>
-									ALL
-								</button>
-							</Grid>
-							<Grid item xs>
-								<input
-									type="text"
-									onChange={e => this.handleAmountChange(e)}
-									value={this.state.amount}
-									className={sendAmountClass}
-									placeholder="0.00"
-								/>
-							</Grid>
-						</Grid>
-					</Grid>
-					<Grid item>
-						{this.state.isCustomView && (
-							<select
-								value={this.state.cryptoCurrency}
-								onChange={e => this.handleCryptoCurrencyChange(e)}
-								name="cryptoCurrency"
-								className={classes.cryptoSelect}
-							>
-								<option
-									value="custom"
-									disabled
-									selected
-									className={classes.selectItem}
-								>
-									Custom Token
-								</option>
-								{this.renderSelectTokenItems()}
-							</select>
-						)}
-					</Grid>
-				</Grid>
-				<Divider className={classes.divider} />
-				<Grid
-					container
-					direction="row"
-					justify="space-between"
-					alignItems="center"
-					className={classes.usdAmoutContainer}
-				>
-					<span>
-						<NumberFormat
-							locale={locale}
-							style="currency"
-							currency={fiatCurrency}
-							value={amountUsd}
-							fractionDigits={15}
-						/>
-					</span>
-					<span> USD </span>
-				</Grid>
 				{this.renderFeeBox()}
 				{this.renderButtons()}
 			</TransactionBox>
