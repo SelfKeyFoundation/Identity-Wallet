7deeb4a4fbc8d5fcf0e3a397c8b7637d608dbf19
Ferenc Horvath <designhorf@gmail.com>
2020-02-17 10:26:08 +0100

    Feat/1970 marketplace more information (#2049)
    * feat: refactored what you get from payment checkout
    
    * feat: refactored Service works form payment checkout
    
    * feat: implemented refactored components
    
    * feat: new tab for incorporation
    
    * feat: changed default tab and params to details page
    
    * feat: added What you get to banking
    
    * feat: refactored What you get tab

--- src/renderer/marketplace/common/payment-checkout.jsx
+++ src/renderer/marketplace/common/payment-checkout.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { Grid, Typography, Button } from '@material-ui/core';
 import { CloseButtonIcon } from 'selfkey-ui';
-import { sanitize, FlagCountryName } from '.';
+import { FlagCountryName } from '.';
+import { WhatYouGet } from './marketplace-what-you-get';
+import { HowServiceWorks } from './marketplace-how-service-works';
 
 const styles = theme => ({
 	container: {
@@ -36,83 +38,8 @@ const styles = theme => ({
 		borderRadius: '4px',
 		padding: '30px'
 	},
-	whatYouGet: {
-		paddingBottom: '30px',
-		marginBottom: '30px',
-		borderBottom: '2px solid #475768'
-	},
-	description: {
-		margin: '1em 1em 1em 0',
-		fontFamily: 'Lato, arial',
-		color: '#FFF',
-		width: '60%',
-		borderRight: '1px solid #475768',
-		lineHeight: '1.4em',
-		fontSize: '14px',
-		'& p': {
-			marginBottom: '1.5em',
-			lineHeight: '1.4em',
-			maxWidth: '90%'
-		},
-		'& strong': {
-			fontWeight: 'bold',
-			color: theme.palette.secondary.main,
-			display: 'block',
-			padding: '0',
-			borderBottom: '1px solid #435160',
-			marginBottom: '0.5em',
-			marginTop: '0em'
-		},
-		'& ul': {
-			listStyle: 'outside',
-			lineHeight: '1.4em',
-			marginLeft: '1.5em',
-			marginBottom: '1.5em'
-		},
-		'& ul li': {
-			lineHeight: '1.4em',
-			marginBottom: '0.5em'
-		},
-		'& a': {
-			color: theme.palette.secondary.main
-		}
-	},
-	descriptionHelp: {
-		width: '35%',
-		color: theme.palette.secondary.main,
-		fontFamily: 'Lato, arial',
-		fontSize: '12px',
-		lineHeight: '1.5em',
-		'& p': {
-			marginBottom: '1em'
-		}
-	},
-	howItWorks: {
-		paddingBottom: '30px',
-		marginBottom: '30px',
-		borderBottom: '2px solid #475768'
-	},
-	howItWorksBox: {
-		width: '26%',
-		padding: '2em 3%',
-		margin: '2em 0',
-		color: '#FFF',
-		background: '#313D49',
-		'& header h4': {
-			display: 'inline-block',
-			marginLeft: '0.5em',
-			fontSize: '16px'
-		},
-		'& header span': {
-			color: '#00C0D9',
-			fontWeight: 'bold',
-			fontSize: '20px'
-		},
-		'& h3': {
-			fontSize: '13px'
-		}
-	},
 	serviceCost: {
+		borderBottom: '2px solid #475768',
 		width: '100%',
 		paddingBottom: '30px',
 		marginBottom: '30px'
@@ -198,88 +125,25 @@ const PaymentCheckout = withStyles(styles)(
 					alignItems="center"
 					className={classes.content}
 				>
-					<div className={classes.whatYouGet}>
-						<Typography variant="body2" gutterBottom>
-							What you get
-						</Typography>
-						<Grid
-							container
-							direction="row"
-							justify="space-between"
-							alignItems="center"
-							spacing={0}
-						>
-							<div
-								className={classes.description}
-								dangerouslySetInnerHTML={{
-									__html: sanitize(description)
-								}}
-							/>
-							<div className={classes.descriptionHelp}>
-								<p>Time to form: {timeToForm} week(s).</p>
-								<p>
-									All our incorporation services include a yearly consulting
-									session, a dedicated account manager and access to our global
-									network of trusted business services, including introductions to
-									accountants, financial, tax and legal advisors at no cost.
-								</p>
-							</div>
-						</Grid>
-					</div>
-					<div className={classes.howItWorks}>
-						<Typography variant="body2" gutterBottom>
-							How the service works
-						</Typography>
-						<Grid
-							container
-							direction="row"
-							justify="space-between"
-							alignItems="center"
-							spacing={0}
-						>
-							<div className={classes.howItWorksBox}>
-								<header>
-									<span>1</span>
-									<Typography variant="h4" gutterBottom>
-										Provide initial documents
-									</Typography>
-								</header>
-								<div>
-									<Typography variant="h3" gutterBottom>
-										{initialDocsText}
-									</Typography>
-								</div>
-							</div>
-							<div className={classes.howItWorksBox}>
-								<header>
-									<span>2</span>
-									<Typography variant="h4" gutterBottom>
-										KYC Process
-									</Typography>
-								</header>
-								<div>
-									<Typography variant="h3" gutterBottom>
-										{kycProcessText}
-									</Typography>
-								</div>
-							</div>
-							<div className={classes.howItWorksBox}>
-								<header>
-									<span>3</span>
-									<Typography variant="h4" gutterBottom>
-										Get final documents
-									</Typography>
-								</header>
-								<div>
-									<Typography variant="h3" gutterBottom>
-										{getFinalDocsText}
-									</Typography>
-								</div>
-							</div>
-						</Grid>
-					</div>
 					<div className={classes.serviceCost}>
-						<Typography variant="body2" gutterBottom>
+						<WhatYouGet
+							classes={classes}
+							description={description}
+							timeToForm={timeToForm}
+							initialDocsText={initialDocsText}
+							kycProcessText={kycProcessText}
+							getFinalDocsText={getFinalDocsText}
+						/>
+
+						<HowServiceWorks
+							classes={classes}
+							initialDocsText={initialDocsText}
+							kycProcessText={kycProcessText}
+							getFinalDocsText={getFinalDocsText}
+						/>
+					</div>
+					<div style={{ width: '100%' }}>
+						<Typography variant="h2" gutterBottom>
 							Service Costs
 						</Typography>
 
