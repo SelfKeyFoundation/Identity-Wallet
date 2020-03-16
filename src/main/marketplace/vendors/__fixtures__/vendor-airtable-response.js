554e309e63e3de041781cb87b73bbfadf15c2fb7
Andre Goncalves <andre@andregoncalves.com>
2020-03-16 10:22:27 +0000

    fix: tests and entities.data bug

--- src/main/marketplace/vendors/__fixtures__/vendor-airtable-response.js
+++ src/main/marketplace/vendors/__fixtures__/vendor-airtable-response.js
@@ -2,7 +2,7 @@ export default {
 	entities: [
 		{
 			data: {
-				vendor_id: 'wandx',
+				vendorId: 'wandx',
 				name: 'WandX',
 				description:
 					'WandX is a decentralized exchange that enables seamless creation and trade of any ERC20 Token or ERC20 Token baskets. Create and trade a basket of ERC20 Tokens with a single transaction. Token basket themes such as decentralized identity or decentralized exchanges can be created. The WAND Token can be used to get a 75% discount on the exchange fee.',
@@ -14,7 +14,7 @@ export default {
 		},
 		{
 			data: {
-				vendor_id: 'xstar',
+				vendorId: 'xstar',
 				name: 'XStar.io',
 				status: 'inactive',
 				categories: ['exchanges'],
--- src/main/marketplace/vendors/vendor-service.js
+++ src/main/marketplace/vendors/vendor-service.js
@@ -15,7 +15,7 @@ export class VendorService {
 	async fetchVendors() {
 		const fetched = await request.get({ url: VENDOR_API_ENDPOINT, json: true });
 		return fetched.entities
-			.filter(entity => !!entity.vendorId)
+			.filter(entity => !!entity.data.vendorId)
 			.map(entity => {
 				let vendor = _.mapKeys(entity.data, (value, key) => _.camelCase(key));
 				if (vendor.relyingPartyConfig) {
