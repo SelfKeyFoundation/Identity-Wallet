cef79bb2704f26242531aa4d09e876f6c9dbd1a9
Ferenc Horvath <designhorf@gmail.com>
2020-03-18 14:37:51 +0100

    Fix/2021 standardize popups (#2089)
    * fix: standardized trezor modals
    
    * fix: deleted unnecessary classname
    
    * fix: standardized unlock wallet modals
    
    * fix: added Circular progress  option into popup header
    
    * fix: standardized address book popups
    
    * fix: standardized No Connection popup
    
    * fix: standardized Terms popups
    
    * fix: standardized Create and Unlock Wallet popups
    
    * fix: standardized Transaction unlock popup
    
    * fix: added closecomponent prop and fixed shadows
    
    * fix: standardized about and disclaimer modals
    
    * fix: standardized transaction send, receive popups
    
    * fix: removed unnecessary sign in dashboard
    
    * feat: added close button class as an option in popup
    
    * fix: standardized Associate Did popup
    
    * fix: standardized Payment complete and checkout popups
    
    * fix: standardized Marketplace process started popup
    
    * fix: standardized Global error popup

--- src/renderer/marketplace/common/payment-checkout.jsx
+++ src/renderer/marketplace/common/payment-checkout.jsx
@@ -2,21 +2,14 @@ import React from 'react';
 
 import { withStyles } from '@material-ui/core/styles';
 import { Grid, Typography, Button } from '@material-ui/core';
-import { CloseButtonIcon, ModalWrap, ModalHeader, ModalBody } from 'selfkey-ui';
 import { FlagCountryName } from '.';
 import { WhatYouGet } from './marketplace-what-you-get';
 import { HowServiceWorks } from './marketplace-how-service-works';
+import { Popup } from '../../common';
 
 const styles = theme => ({
-	container: {
-		position: 'relative',
-		width: '100%',
-		margin: '0 auto',
-		maxWidth: '960px'
-	},
-	modalHeader: {
-		display: 'flex',
-		height: '76px'
+	closeButton: {
+		marginLeft: '180px'
 	},
 	containerHeader: {
 		'& div': {
@@ -27,26 +20,15 @@ const styles = theme => ({
 			marginLeft: '16px'
 		}
 	},
-	closeIcon: {
-		position: 'absolute',
-		right: '-20px',
-		top: '-20px'
+	modalWrap: {
+		left: 'calc(50% - 452px)',
+		width: '960px'
 	},
-	contentContainer: {
-		border: '1px solid #303C49',
-		borderRadius: '4px',
-		padding: '30px'
-	},
-	serviceCost: {
-		borderBottom: '2px solid #475768',
+	payButton: {
 		width: '100%',
-		paddingBottom: '30px',
-		marginBottom: '30px'
-	},
-	priceTable: {
-		padding: '2em',
-		margin: '2em 0',
-		background: '#313D49'
+		'& button': {
+			marginRight: '30px'
+		}
 	},
 	priceRow: {
 		padding: '10px 0',
@@ -70,19 +52,20 @@ const styles = theme => ({
 			color: theme.palette.secondary.main
 		}
 	},
+	priceTable: {
+		padding: '2em',
+		margin: '2em 0',
+		background: '#313D49'
+	},
 	rowSeparator: {
 		border: '1px solid #475768',
 		margin: '30px 0'
 	},
-	payButton: {
+	serviceCost: {
+		borderBottom: '2px solid #475768',
 		width: '100%',
-		'& button': {
-			marginRight: '30px'
-		}
-	},
-	modalWrap: {
-		left: 'calc(50% - 452px)',
-		width: '960px'
+		paddingBottom: '30px',
+		marginBottom: '30px'
 	}
 });
 
@@ -106,9 +89,12 @@ const PaymentCheckout = withStyles(styles)(
 		onStartClick,
 		startButtonText
 	}) => (
-		<ModalWrap className={classes.modalWrap}>
-			<CloseButtonIcon onClick={onBackClick} className={classes.closeIcon} />
-			<ModalHeader className={classes.modalHeader}>
+		<Popup
+			closeAction={onBackClick}
+			open
+			popupClass={classes.modalWrap}
+			closeButtonClass={classes.closeButton}
+			text={
 				<Grid
 					container
 					justify="flex-start"
@@ -120,106 +106,100 @@ const PaymentCheckout = withStyles(styles)(
 					</div>
 					<Typography variant="h2">{title}</Typography>
 				</Grid>
-			</ModalHeader>
-			<ModalBody>
-				<Grid
-					container
-					justify="flex-start"
-					alignItems="center"
-					className={classes.content}
-				>
-					<div className={classes.serviceCost}>
-						<WhatYouGet
-							classes={classes}
-							description={description}
-							timeToForm={timeToForm}
-							initialDocsText={initialDocsText}
-							kycProcessText={kycProcessText}
-							getFinalDocsText={getFinalDocsText}
-						/>
+			}
+		>
+			<Grid container justify="flex-start" alignItems="center" className={classes.content}>
+				<div className={classes.serviceCost}>
+					<WhatYouGet
+						classes={classes}
+						description={description}
+						timeToForm={timeToForm}
+						initialDocsText={initialDocsText}
+						kycProcessText={kycProcessText}
+						getFinalDocsText={getFinalDocsText}
+					/>
 
-						<HowServiceWorks
-							classes={classes}
-							initialDocsText={initialDocsText}
-							kycProcessText={kycProcessText}
-							getFinalDocsText={getFinalDocsText}
-						/>
-					</div>
-					<div style={{ width: '100%' }}>
-						<Typography variant="h2" gutterBottom>
-							Service Costs
-						</Typography>
+					<HowServiceWorks
+						classes={classes}
+						initialDocsText={initialDocsText}
+						kycProcessText={kycProcessText}
+						getFinalDocsText={getFinalDocsText}
+					/>
+				</div>
+				<div style={{ width: '100%' }}>
+					<Typography variant="h2" gutterBottom>
+						Service Costs
+					</Typography>
 
-						<div className={classes.priceTable}>
-							{options.map(option => (
-								<div key={option.id} className={classes.priceRow}>
-									<Grid
-										container
-										direction="row"
-										justify="flex-start"
-										alignItems="center"
-										spacing={0}
-									>
-										<div className="rowItem">{option.description}</div>
-										<div className="rowItem time">{option.notes}</div>
-										<div className="rowItem price">
-											{option.price && (
-												<React.Fragment>
-													${option.price.toLocaleString()}
-												</React.Fragment>
-											)}
-										</div>
-									</Grid>
+					<div className={classes.priceTable}>
+						{options.map(option => (
+							<div key={option.id} className={classes.priceRow}>
+								<Grid
+									container
+									direction="row"
+									justify="flex-start"
+									alignItems="center"
+									spacing={0}
+								>
+									<div className="rowItem">{option.description}</div>
+									<div className="rowItem time">{option.notes}</div>
+									<div className="rowItem price">
+										{option.price && (
+											<React.Fragment>
+												${option.price.toLocaleString()}
+											</React.Fragment>
+										)}
+									</div>
+								</Grid>
+							</div>
+						))}
+						<div className={classes.rowSeparator} />
+						<div className={classes.priceRow}>
+							<Grid
+								container
+								direction="row"
+								justify="flex-start"
+								alignItems="center"
+								spacing={0}
+							>
+								<div className="rowItem">Cost</div>
+								<div className="rowItem time" />
+								<div className="rowItem price">
+									Total: ${price.toLocaleString()}
+									<div className="time">{keyAmount.toLocaleString()} KEY</div>
 								</div>
-							))}
-							<div className={classes.rowSeparator} />
-							<div className={classes.priceRow}>
-								<Grid
-									container
-									direction="row"
-									justify="flex-start"
-									alignItems="center"
-									spacing={0}
-								>
-									<div className="rowItem">Cost</div>
-									<div className="rowItem time" />
-									<div className="rowItem price">
-										Total: ${price.toLocaleString()}
-										<div className="time">{keyAmount.toLocaleString()} KEY</div>
-									</div>
-								</Grid>
-							</div>
-							<div className={classes.priceRow}>
-								<Grid
-									container
-									direction="row"
-									justify="flex-start"
-									alignItems="center"
-									spacing={0}
-								>
-									<div className="rowItem transactionFee">
-										Network Transaction Fee
-									</div>
-									<div className="rowItem time" />
-									<div className="rowItem price">
-										${usdFee.toLocaleString()}
-										<div className="time">{ethFee.toLocaleString()} ETH</div>
-									</div>
-								</Grid>
-							</div>
+							</Grid>
+						</div>
+						<div className={classes.priceRow}>
+							<Grid
+								container
+								direction="row"
+								justify="flex-start"
+								alignItems="center"
+								spacing={0}
+							>
+								<div className="rowItem transactionFee">
+									Network Transaction Fee
+								</div>
+								<div className="rowItem time" />
+								<div className="rowItem price">
+									${usdFee.toLocaleString()}
+									<div className="time">{ethFee.toLocaleString()} ETH</div>
+								</div>
+							</Grid>
 						</div>
 					</div>
-					<div className={classes.payButton}>
-						<Button variant="contained" size="large" onClick={onStartClick}>
-							{startButtonText}
-						</Button>
-						<Button variant="outlined" size="large" onClick={onBackClick}>
-							Cancel
-						</Button>
-					</div>
-				</Grid>
-			</ModalBody>
-		</ModalWrap>
+				</div>
+				<div className={classes.payButton}>
+					<Button variant="contained" size="large" onClick={onStartClick}>
+						{startButtonText}
+					</Button>
+					<Button variant="outlined" size="large" onClick={onBackClick}>
+						Cancel
+					</Button>
+				</div>
+			</Grid>
+		</Popup>
 	)
 );
 
