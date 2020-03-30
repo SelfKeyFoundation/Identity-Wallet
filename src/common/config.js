139f3ff5d8879bf0b19297ee445f8137050b2197
Andre Goncalves <andre@andregoncalves.com>
2020-03-30 12:35:31 +0100

    style: remove extra comma

--- src/common/config.js
+++ src/common/config.js
@@ -162,7 +162,7 @@ const dev = {
 		walletExport: true,
 		transactionsListFilter: true,
 		loansMarketplace: true,
-		swapTokens: true,
+		swapTokens: true
 	},
 	testWalletAddress: '0x23d233933c86f93b74705cf0d236b39f474249f8',
 	testDidAddress: '0xee10a3335f48e10b444e299cf017d57879109c1e32cec3e31103ceca7718d0ec',
