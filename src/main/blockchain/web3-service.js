adcf275bf7677fe460c32a8fe0a0152f04f6ae16
Maxim Kovalov <max.kovalov@gmail.com>
2020-04-01 17:19:11 +0300

    fix(balance): fix balance

--- src/main/blockchain/web3-service.js
+++ src/main/blockchain/web3-service.js
@@ -62,13 +62,17 @@ export class Web3Service {
 		const engine = new ProviderEngine();
 
 		engine.addProvider(this.getWalletEthTxSubprovider());
+		const subscriptionSubprovider = new SubscriptionSubprovider();
+		engine.addProvider(subscriptionSubprovider);
 		engine.addProvider(new WebsocketProvider({ rpcUrl: SELECTED_SERVER_URL }));
 		engine.on('error', error => {
 			log.error('Web3Service provider error %s', error);
 		});
-		engine.start();
-
 		this.web3 = new Web3(engine);
+		engine.on('start', async () => {
+			await this.web3.eth.getBlockNumber();
+		});
+		engine.start();
 		this.web3.transactionConfirmationBlocks = 1;
 	}
 
@@ -96,9 +100,11 @@ export class Web3Service {
 		engine.on('error', error => {
 			log.error('Web3Service provider error %s', error);
 		});
-		engine.start();
-
 		this.web3 = new Web3(engine);
+		engine.on('start', async () => {
+			await this.web3.eth.getBlockNumber();
+		});
+		engine.start();
 		this.web3.transactionConfirmationBlocks = 1;
 	}
 
@@ -149,8 +155,13 @@ export class Web3Service {
 		engine.addProvider(this.trezorWalletSubProvider);
 		engine.addProvider(subscriptionSubprovider);
 		engine.addProvider(new WebsocketProvider({ rpcUrl: SELECTED_SERVER_URL }));
-		engine.start();
 		this.web3 = new Web3(engine);
+		engine.on('start', async () => {
+			// block tracking does not work if not manually started for some reason
+			// causing incorrect balances to be returned on getBalance call
+			await this.web3.eth.getBlockNumber();
+		});
+		engine.start();
 		this.web3.transactionConfirmationBlocks = 1;
 	}
 
