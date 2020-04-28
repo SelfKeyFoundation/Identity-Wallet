2e4e4a73097894a75236dcd8c3db5ec0ccdc1b47
Andre Goncalves <andre@andregoncalves.com>
2020-04-28 10:20:42 +0100

    Feat/loans listing filters (#2184)
    * feat: add range component
    
    * feat: initial routing to details component
    
    * feat: refactor filters, select max, min and rates based on loanType

--- src/renderer/marketplace/loans/common/marketplace-loans-component.jsx
+++ src/renderer/marketplace/loans/common/marketplace-loans-component.jsx
@@ -3,7 +3,9 @@ import { MarketplaceComponent } from '../../common/marketplace-component';
 const MARKETPLACE_LOANS_ROOT_PATH = '/main/marketplace/loans';
 
 export default class MarketplaceLoansComponent extends MarketplaceComponent {
+	rootPath = () => MARKETPLACE_LOANS_ROOT_PATH;
 	listRoute = () => MARKETPLACE_LOANS_ROOT_PATH;
+	detailsRoute = vendorId => `${MARKETPLACE_LOANS_ROOT_PATH}/details/${vendorId}`;
 }
 
 export { MarketplaceLoansComponent };
