6ce6a806f6afb356505a47276945899334255a57
Andre Goncalves <andre@andregoncalves.com>
2020-05-18 11:18:30 +0100

    fix: text alignment in requirements section and remove unnecessary grid

--- src/renderer/marketplace/loans/details/details-requirements.jsx
+++ src/renderer/marketplace/loans/details/details-requirements.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import { Typography, Grid } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 import { withStyles } from '@material-ui/styles';
 
 const styles = theme => ({
 	bodyText: {
-		marginTop: '1em	'
+		marginTop: '1em',
+		textAlign: 'left'
 	}
 });
 
 const LoansDetailsRequirements = withStyles(styles)(({ classes, item }) => (
-	<Grid container direction="column" justify="flex-start" alignItems="flex-start" spacing={2}>
-		<Grid item>
-			<Typography variant="h5">Requirements for Loans:</Typography>
-			<Typography variant="body2" className={classes.bodyText}>
-				{item.data.requirements}
-			</Typography>
-		</Grid>
-	</Grid>
+	<React.Fragment>
+		<Typography variant="h5">Requirements for Loans:</Typography>
+		<Typography variant="body2" className={classes.bodyText}>
+			{item.data.requirements}
+		</Typography>
+	</React.Fragment>
 ));
 
 export { LoansDetailsRequirements };
