d5747b4d280f340c039a980381255b683140efa6
Andre Goncalves <andre@andregoncalves.com>
2020-05-27 11:23:00 +0100

    fix: interest only payment formula

--- src/renderer/marketplace/loans/calculator/index.jsx
+++ src/renderer/marketplace/loans/calculator/index.jsx
@@ -110,9 +110,9 @@ const calculateSimpleInterest = ({ amount, months, apr }) => {
 	const principal = parseFloat(amount);
 	const interest = parseFloat(apr) / 100 / 12;
 
-	const monthly = 0;
 	const totalInterest = principal * interest * months;
 	const total = principal + totalInterest;
+	const monthly = totalInterest / months;
 
 	return { monthly, total, totalInterest };
 };
