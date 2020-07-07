584d59176a94701438866315766f8fe19479e3b1
Maxim Kovalov <max.kovalov@gmail.com>
2020-07-07 18:01:52 +0300

    feat: add matomo goals

--- src/common/matomo.js
+++ src/common/matomo.js
@@ -6,6 +6,20 @@ export const matomoGoalTracking = goal => {
 };
 
 export const matomoGoals = {
-	CreateSelfKeyId: config.matomoSite === 1 ? 1 : 5
+	CreateSelfKeyId: config.matomoSite === 1 ? 1 : 5,
 	// TODO: implement more goals in feature/910
+	CreateIndividualDID: 11,
+	MarketplaceVisitIndividualExchange: 12,
+	MarketplaceVisitIndividualIncorporations: 13,
+	MarketplaceVisitIndividualPassports: 14,
+	MarketplaceVisitIndividualBankAccounts: 15,
+	MarketplaceVisitIndividualNotaries: 16,
+	MarketplaceVisitIndividualLoans: 17,
+	MarketplaceVisitCorporateExchange: 18,
+	MarketplaceVisitCorporateIncorporations: 19,
+	MarketplaceVisitCorporatePassports: 20,
+	MarketplaceVisitCorporateBankAccounts: 21,
+	MarketplaceVisitCorporateNotaries: 22,
+	MarketplaceVisitCorporateLoans: 23,
+	CreateCorporateDID: 24
 };
