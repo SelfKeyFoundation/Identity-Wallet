9b35c0aadb80423962e18c96cbe9480b8d7b45ca
Andre Goncalves <andre@andregoncalves.com>
2020-08-18 09:58:21 +0100

    fix: remove old certifiers code

--- src/common/notaries/index.js
+++
@@ -1,8 +0,0 @@
-/* istanbul ignore file */
-import reducer from './reducers';
-
-export { notariesOperations } from './operations';
-export { notariesSelectors } from './selectors';
-export { notariesTypes } from './types';
-
-export default reducer;
