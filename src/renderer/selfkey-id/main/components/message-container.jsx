fcae5620d7a16c6b415380b3fa9df91ed4953a92
Andre Goncalves <andre@andregoncalves.com>
2020-08-18 10:03:30 +0100

    fix: cleanup comments

--- src/renderer/selfkey-id/main/components/message-container.jsx
+++ src/renderer/selfkey-id/main/components/message-container.jsx
@@ -20,20 +20,3 @@ const styledComponent = withStyles(styles)(MessageContainer);
 const connectedComponent = connect(mapStateToProps)(styledComponent);
 export default connectedComponent;
 export { connectedComponent as MessageContainer };
-
-// 	{
-// 		id: 809598767582156,
-// 		name: 'John Paul',
-// 		type: 'person',
-// 		date: 1569504593,
-// 		message:
-// 			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum ullamcorper purus sit amet convallis. Quisque congue augue quam, dignissim aliquam lorem dignissim ac.'
-// 	},
-// 	{
-// 		id: 2348767582156,
-// 		name: 'Smith Jhonson Certifier',
-// 		type: 'certifier',
-// 		date: 2234104593,
-// 		message:
-// 			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum ullamcorper purus sit amet convallis. Quisque congue augue quam, dignissim aliquam lorem dignissim ac.  '
-// 	},
