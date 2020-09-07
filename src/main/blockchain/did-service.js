85066147d995b06f3e0a5a2555c599e7ca39c1f3
Maxim Kovalov <max.kovalov@gmail.com>
2020-09-07 21:52:16 +0300

    fix: did timeout

--- src/main/blockchain/did-service.js
+++ src/main/blockchain/did-service.js
@@ -1,18 +1,20 @@
 import { getGlobalContext } from 'common/context';
 import { abi as ledgerABI } from 'main/assets/data/DIDLedger.json';
 import config from 'common/config';
+import { Logger } from 'common/logger';
+
+const log = new Logger('DIDService');
 
 export class DIDService {
 	constructor() {
 		this.web3Service = getGlobalContext().web3Service;
-		this.zero = this.web3Service.web3.utils.hexToBytes(
-			'0x0000000000000000000000000000000000000000000000000000000000000000'
-		);
+		this.zero = '0x0000000000000000000000000000000000000000000000000000000000000000';
 	}
 
 	createDID(walletAddress, gas) {
 		const ledger = new this.web3Service.web3.eth.Contract(ledgerABI, config.ledgerAddress);
-		this.web3Service.web3.transactionConfirmationBlocks = 2;
+		ledger.transactionConfirmationBlocks = 2;
+		ledger.transactionBlockTimeout = 750;
 		return ledger.methods.createDID(this.zero).send({ from: walletAddress, gas });
 	}
 
@@ -24,8 +26,13 @@ export class DIDService {
 	async getGasLimit(from) {
 		const ledger = new this.web3Service.web3.eth.Contract(ledgerABI, config.ledgerAddress);
 		const MAX_GAS = 4500000;
-		const estimate = await ledger.methods.createDID(this.zero).estimateGas({ from });
-		return Math.round(Math.min(estimate * 1.1, MAX_GAS));
+		try {
+			const estimate = await ledger.methods.createDID(this.zero).estimateGas({ from });
+			return Math.round(Math.min(estimate * 1.1, MAX_GAS));
+		} catch (error) {
+			log.error(error);
+			throw error;
+		}
 	}
 }
 
