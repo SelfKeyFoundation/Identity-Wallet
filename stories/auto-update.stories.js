a5deb4eb1f288f31d6bfcf7cbef5a4c0e5d0e449
Ferenc Horvath <designhorf@gmail.com>
2020-09-08 18:45:37 +0200

    fix: changed release notes in auto update story

--- stories/auto-update.stories.js
+++ stories/auto-update.stories.js
@@ -6,7 +6,8 @@ import AutoUpdateProgress from '../src/renderer/auto-update/auto-update-progress
 const info = {
 	releaseName: '1.0.1-beta',
 	releaseDate: 'April 08, 2020',
-	releaseNotes: '<h4>Hello</h4><ul>\n<li>UX fixes</li>\n<li>Bug fixes</li>\n</ul>'
+	releaseNotes:
+		'<p>SelfKey  Identity Wallet 1.0.0 beta.</p><h4>Release Date:</h4><p>April 08, 2020</p><h4>Changes:</h4><ul>\n<li>UX fixes</li>\n<li>Bug fixes</li>\n</ul>'
 };
 
 const progress = {
