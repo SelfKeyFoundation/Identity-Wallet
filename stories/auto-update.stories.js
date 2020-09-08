44c7f2252349ef7187a2b62c69cb0b8f1b73f072
Maxim Kovalov <max.kovalov@gmail.com>
2020-09-08 16:16:50 +0300

    fix: release notes

--- stories/auto-update.stories.js
+++ stories/auto-update.stories.js
@@ -6,7 +6,7 @@ import AutoUpdateProgress from '../src/renderer/auto-update/auto-update-progress
 const info = {
 	releaseName: '1.0.1-beta',
 	releaseDate: 'April 08, 2020',
-	releaseNotes: '<ul>\n<li>UX fixes</li>\n<li>Bug fixes</li>\n</ul>'
+	releaseNotes: '<h4>Hello</h4><ul>\n<li>UX fixes</li>\n<li>Bug fixes</li>\n</ul>'
 };
 
 const progress = {
