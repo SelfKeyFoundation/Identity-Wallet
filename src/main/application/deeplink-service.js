e45f7b23d498249fb7e3808b708d2dc91e9c4ccf
Andre Goncalves <andre@andregoncalves.com>
2020-12-09 10:12:47 +0000

    fix: revert url patching for wc

--- src/main/application/deeplink-service.js
+++ src/main/application/deeplink-service.js
@@ -20,16 +20,7 @@ export class DeepLinksService {
 
 	handleOpenLink = async url => {
 		try {
-			// selfkey://wc?uri=wc:b18792a6-2b02-4479-83ab-d8dd4d2ef9c6@1?bridge=https%3A%2F%2Fbridge.walletconnect.org&key=c28407a557a89e96be04a0fc44f1ad9a1c4c180d46af6ec27dfb03d917674089
 			log.info('incoming deep link %s', url);
-			if (url.indexOf('selfkey://wc?uri=') === 0) {
-				const uri = url.replace('selfkey://wc?uri=', '');
-				await this.handlers['wallet-connect'].handleUrlCommand(uri);
-			} else {
-				log.info('invalid deep link format');
-				return;
-			}
-			/*
 			const urlRegexp = /^[^:/]*:\/\/([^/]*)\/(.*)$/;
 			const match = (url || '').match(urlRegexp);
 			if (!match || match.length !== 3) {
@@ -42,7 +33,6 @@ export class DeepLinksService {
 			}
 
 			await this.handlers[match[1]].handleUrlCommand(match[2]);
-			*/
 		} catch (error) {
 			log.error(error);
 		}
