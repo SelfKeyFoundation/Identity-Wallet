a636bdcd3f9d98e5990b236b5845fe8d182f1bf4
Maxim Kovalov <max.kovalov@gmail.com>
2020-12-14 20:20:40 +0200

    feat: add moonpay option to buy key

--- src/renderer/dashboard/buy-key-widget.jsx
+++ src/renderer/dashboard/buy-key-widget.jsx
@@ -1,12 +1,10 @@
-import React, { PureComponent } from 'react';
-import { featureIsEnabled } from 'common/feature-flags';
+import React from 'react';
 import { Grid, Typography, Button } from '@material-ui/core';
-import { withStyles } from '@material-ui/styles';
+import { makeStyles } from '@material-ui/styles';
 import { CustomIcon, CoinsIcon, ExchangeSmallIcon } from 'selfkey-ui';
-import BuyKeyPopup from './buy-key-popup-container';
-import TokenSwap from '../transaction/swap';
+import { PropTypes } from 'prop-types';
 
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
 	bgIcon: {
 		top: '-60px',
 		marginTop: '-112px',
@@ -55,78 +53,47 @@ const styles = theme => ({
 			right: '-269px'
 		}
 	}
-});
+}));
 
-class BuyKeyWidget extends PureComponent {
-	state = {
-		popup: null
-	};
+export const BuyKeyWidget = props => {
+	const { onBuyClick, onSwapClick } = props;
+	const classes = useStyles();
+	return (
+		<Grid item className={classes.buyKey}>
+			<Typography variant="h1" className={classes.title}>
+				Buy KEY tokens, to use in the SelfKey Marketplace.
+			</Typography>
+			<Button
+				variant="outlined"
+				size="large"
+				className={classes.ctabutton}
+				onClick={onBuyClick}
+			>
+				<CustomIcon width="24px" height="24px" />
+				<span>Buy KEY</span>
+			</Button>
 
-	handleBuyPopup = () => {
-		this.setState({ popup: 'buy' });
-	};
+			{onSwapClick && (
+				<Button
+					variant="outlined"
+					size="large"
+					className={classes.ctabutton}
+					onClick={onSwapClick}
+				>
+					<ExchangeSmallIcon width="24px" height="24px" />
+					<span>Swap Tokens</span>
+				</Button>
+			)}
+			<div className={classes.bgIcon}>
+				<CoinsIcon width="76px" height="79px" fill="#313B49" />
+			</div>
+		</Grid>
+	);
+};
 
-	handleSwapPopup = () => {
-		this.setState({ popup: 'swap' });
-	};
+BuyKeyWidget.propTypes = {
+	onBuyClick: PropTypes.func.isRequired,
+	onSwapClick: PropTypes.func
+};
 
-	handlePopupClose = () => {
-		this.setState({ popup: null });
-	};
-
-	handleExternalLink = e => {
-		window.openExternal(
-			e,
-			'https://help.selfkey.org/article/128-how-to-pay-for-marketplace-products-services-with-key'
-		);
-	};
-
-	render() {
-		const { classes } = this.props;
-		const { popup } = this.state;
-		return (
-			<React.Fragment>
-				{popup !== null && popup === 'buy' && (
-					<BuyKeyPopup
-						closeAction={this.handlePopupClose}
-						externalLink={this.handleExternalLink}
-					/>
-				)}
-				{popup !== null && popup === 'swap' && (
-					<TokenSwap closeAction={this.handlePopupClose} />
-				)}
-				<Grid item className={classes.buyKey}>
-					<Typography variant="h1" className={classes.title}>
-						Buy KEY tokens, to use in the SelfKey Marketplace.
-					</Typography>
-					<Button
-						variant="outlined"
-						size="large"
-						className={classes.ctabutton}
-						onClick={this.handleBuyPopup}
-					>
-						<CustomIcon width="24px" height="24px" />
-						<span>Buy KEY</span>
-					</Button>
-
-					{featureIsEnabled('swapTokens') && (
-						<Button
-							variant="outlined"
-							size="large"
-							className={classes.ctabutton}
-							onClick={this.handleSwapPopup}
-						>
-							<ExchangeSmallIcon width="24px" height="24px" />
-							<span>Swap Tokens</span>
-						</Button>
-					)}
-					<div className={classes.bgIcon}>
-						<CoinsIcon width="76px" height="79px" fill="#313B49" />
-					</div>
-				</Grid>
-			</React.Fragment>
-		);
-	}
-}
-
-export default withStyles(styles)(BuyKeyWidget);
+export default BuyKeyWidget;
---
+++ stories/buy-key.stories.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import BuyKeyWidget from '../src/renderer/dashboard/buy-key-widget';
+import BuyKeyModal from '../src/renderer/dashboard/buy-key-popup-modal';
+
+storiesOf('Buy Key/Dashboard Widget', module)
+	.add('default', () => (
+		<div style={{ width: 400 }}>
+			<BuyKeyWidget onBuyClick={action('buy key')} />
+		</div>
+	))
+	.add('swap-enabled', () => (
+		<div style={{ width: 400 }}>
+			<BuyKeyWidget onBuyClick={action('buy key')} onSwapClick={action('swap key')} />
+		</div>
+	));
+
+const exchanges = [
+	{ id: 'binance', name: 'Binance', url: 'https://binance.com' },
+	{ id: 'binance2', name: 'Binance2', trade_url: 'https://binance.com' }
+];
+
+storiesOf('Buy Key/Buy Key Popup', module)
+	.add('default', () => (
+		<BuyKeyModal
+			address="0xsdasdsadasdasd"
+			exchanges={exchanges}
+			onCloseClick={action('close')}
+		/>
+	))
+	.add('moonpay enabled', () => (
+		<BuyKeyModal
+			onMoonpayClick={() => {
+				action('moonpay');
+			}}
+			address="0xsdasdsadasdasd"
+			exchanges={exchanges}
+			onCloseClick={action('close')}
+		/>
+	));
