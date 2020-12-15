938917264c52c6da6480fcea2131a1dbbe54c87a
Kovalov Maxim <max.kovalov@gmail.com>
2020-12-15 12:21:27 +0200

    feat: add moonpay option to buy key (#2524)

--- src/common/config.js
+++ src/common/config.js
@@ -162,7 +162,8 @@ const common = {
 		rewardToken: false,
 		keyfi: true,
 		hdWallet: true,
-		deepLinks: true
+		deepLinks: true,
+		moonpay: false
 	}
 };
 
@@ -195,13 +196,14 @@ const dev = {
 		walletExport: true,
 		transactionsListFilter: true,
 		loansMarketplace: true,
-		swapTokens: true,
+		swapTokens: false,
 		contract: true,
 		exchangesMarketplace: false,
 		rewardToken: true,
 		keyfi: true,
 		hdWallet: true,
-		deepLinks: true
+		deepLinks: true,
+		moonpay: true
 	},
 	testWalletAddress: '0x23d233933c86f93b74705cf0d236b39f474249f8',
 	testDidAddress: '0xee10a3335f48e10b444e299cf017d57879109c1e32cec3e31103ceca7718d0ec',
@@ -243,7 +245,8 @@ const prod = {
 		rewardToken: false,
 		keyfi: true,
 		hdWallet: true,
-		deepLinks: true
+		deepLinks: true,
+		moonpay: false
 	},
 	attributeTypeSource: ATTRIBUTE_TYPE_SOURCE_OVERRIDE || 'production'
 };
