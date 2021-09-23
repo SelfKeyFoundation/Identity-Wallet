df389ec6e1fe64c7f59b6f943b5d6b9d02fa2818
Andre Goncalves <andre@andregoncalves.com>
2021-09-23 08:51:12 +0100

    fix: correct gas fee estimate url

--- src/main/blockchain/eth-gas-station-service.js
+++ src/main/blockchain/eth-gas-station-service.js
@@ -1,5 +1,5 @@
 'use strict';
-import 'common/config';
+import config from 'common/config';
 import { featureIsEnabled } from 'common/feature-flags';
 import request from 'request';
 import { Logger } from 'common/logger';
@@ -7,8 +7,10 @@ import { Logger } from 'common/logger';
 const log = new Logger('eth-gas-station-service');
 
 // TODO: move URLs to config
+const chainId = config.chainId || 3;
 const URL = 'https://ethgasstation.info/json/ethgasAPI.json';
-const EIP1559_URL = 'https://gas-api.metaswap.codefi.network/networks/3/suggestedGasFees';
+const EIP1559_URL =
+	'https://gas-api.metaswap.codefi.network/networks/' + chainId + '/suggestedGasFees';
 
 export class EthGasStationService {
 	getInfo() {
