b7a82987959aaa27e168a0058d7ee8cd5e98eea7
Andre Goncalves <andre@andregoncalves.com>
2021-09-24 18:05:19 +0100

    fix: replace suggested max priority fee

--- src/renderer/marketplace/passports/checkout/passports-checkout-container.jsx
+++ src/renderer/marketplace/passports/checkout/passports-checkout-container.jsx
@@ -39,7 +39,7 @@ class PassportsCheckoutContainerComponent extends MarketplacePassportsComponent 
 		const gasPrice = ethGasStationInfo ? ethGasStationInfo.average : 50;
 		const maxPriorityFee =
 			ethGasStationInfo && ethGasStationInfo.fees && ethGasStationInfo.fees.medium
-				? parseFloat(ethGasStationInfo.fees.medium.suggestedMaxFeePerGas)
+				? parseFloat(ethGasStationInfo.fees.medium.suggestedMaxPriorityFeePerGas)
 				: 1;
 
 		const price = program.price;
