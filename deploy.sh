653ae01684d5637b481184e04f72d51855547ce1
mzmsk <mzmcode2@gmail.com>
2022-09-27 08:03:37 -0300

    feat: design updates

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ then
 else
     VERSION=$(cat /tmp/mac/package.json | jq .version)
     gsutil cp /tmp/mac/dist/*.{zip,dmg,pkg} gs://sk-builds/$CIRCLE_BRANCH/$TIMESTAMP/
-    curl -i -X POST -H "Content-Type: application/json" -d "{\"text\": \"Mac build has been deployed for \n$COMMIT_MESSAGE \n[(see artifacts)](https://console.cloud.google.com/storage/browser/sk-builds/$CIRCLE_BRANCH/$VERSION/$TIMESTAMP/?project=selfkey2)\", \"channel\": \"poi-builds\" }" $MATTERMOST_TOKEN
+    curl -i -X POST -H "Content-Type: application/json" -d "{\"text\": \"Mac build has been deployed for \n$COMMIT_MESSAGE \n[(see artifacts)](https://console.cloud.google.com/storage/browser/sk-builds/$CIRCLE_BRANCH/$VERSION/$TIMESTAMP/?project=selfkey2)\", \"channel\": \"poi-builds\" }" https://mattermost.kyc-chain.com/hooks/$MATTERMOST_TOKEN
 fi
 
